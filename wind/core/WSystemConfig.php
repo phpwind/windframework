42e4acf37b5ea460f7a27cd18b209c535caadcf8
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-06 09:43:00 +0000

    framework develop configparser
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@22 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/WSystemConfig.php
+++ wind/core/WSystemConfig.php
@@ -7,63 +7,65 @@
  */
 
 /**
+ * 配置信息解析
  * the last known user to change this file in the repository  <$LastChangedBy$>
- * @author Qiong Wu <papa0924@gmail.com>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
  * @version $Id$ 
- * @package 
+ * @package
  */
-class WSystemConfig extends WConfig implements WContext {
-	private $systemConfig;
-	private $config;
+class WSystemConfig extends WConfig implements WContext{
+	private $system = array();
+	private $custom = array();
+	private $config = array();
 	
 	/**
-	 * 
-	 * @param array $sytemConfig	//框架的默认配置
-	 * @param array $config			//应用配置
+	 * 争对数组格式的解析
+	 * @param array $configSystem
+	 * @param array $configCustom
 	 */
-	function parse($sytemConfig, $config = NULL) {
+	public function parse($configSystem,$configCustom = array()){
+		if(!is_array($configSystem) || !is_array($configCustom))
+			throw new WSystemException("配置信息格式不对",5);
+		if(empty($configSystem))
+			throw new WSystemException("系统配置不能为空",5);
+		$this->config = array_merge($configSystem,$configCustom);
+		$this->system  = $configSystem;
+		$this->custom  = $configCustom;
+	}
+	
+	/**
+	 * 争对xml格式配置的解析
+	 * @param xml $configSystem
+	 * @param xml $configCustom
+	 */
+	public function parseXML($configSystem,$configCustom = null){
 		
-		$args = func_get_args();
-		$obj = isset($args[2]) ? $args[2] : $this;
-		if (empty($args[2])) {
-			$systemConfig = $this->getSystemConfig();
-			$config = array_merge($systemConfig, $config);
+	}
+	
+	/**
+	 * 根据配置名取得相应的配置
+	 * @param string $configName
+	 * @return string
+	 */
+	public function getConfig($configName){
+		if($configName && isset($this->config[$configName])){
+			return $this->config[$configName];
+		}else{
+			throw new WSystemException("{$configName}配置信息不存在",5);
 		}
-		foreach ($config as $key => $value) {
-			if ($ifRecursion && is_array($value)) {
-				$obj->{$key} = new stdClass();
-				$obj->parse($value, $ifRecursion, $this->{$key});
-			} else {
-				$obj->{$key} = $value;
-			}
+	}
+	
+	public function getRouterConfig(){
+		if(isset($this->config['router'])){
+			return $this->config['router'];
+		}else{
+			throw new WSystemException("路由配置信息不存在",5);
 		}
 	}
 	
-	/**
-	 * xml 格式配置解析
-	 */
-	function parse1() {
-
-	}
-	
-	function getRouterConfig() {
-		return '';
-	}
-	
-	/**
-	 * 
-	 */
-	function getConfig($configName) {
-		return '';
-	}
-	
-	/**
-	 * @return WSystemConfig
-	 */
-	public static function getInstance() {
+	public static function getInstance(){
 		return W::getInstance(__CLASS__);
 	}
-
 }
 
 ?>
