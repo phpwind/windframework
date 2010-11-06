2a76c4cdd3a503d0fecee7a8e03f300f2eae676b
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-06 08:05:13 +0000

    framework develop configparser
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@14 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/WConfigParser.php
+++ wind/base/WConfigParser.php
@@ -14,33 +14,32 @@ WBasic::import('base.WModel');
  * @package
  */
 class WConfigParser {
-	
-	public function __construct($config = array(),$ifRecursion = false){
-		$this->parse($config,$ifRecursion);
+
+	public function parse($config = array(),$ifGlobal = true,$ifSelf = true){
+		$config = $config ? $config : $this->getAppConfig();
+		$config = $ifGlobal ? array_merge($this->getSystemConfig(),$config) : $config;
+		$obj = $ifSelf ? $this : new stdClass();
+		foreach($config as $key=>$value) $obj->{$key} = $value;
+		return  $obj;
 	}
-	
-	private function parse($config = array(),$ifRecursion = false){
-		$args = func_get_args();
-		$obj = isset($args[2]) ? $args[2] : $this;
-		if(empty($args[2])){
-			$systemConfig = $this->getSystemConfig();
-			$config = array_merge($systemConfig,$config);
-		}
-		foreach($config as $key=>$value){
-			if($ifRecursion && is_array($value)){
-				$obj->{$key} = new stdClass();
-				$obj->parse($value,$ifRecursion,$this->{$key});
-			}else{
-				$obj->{$key} = $value;
-			}
-		}
-	}
-	
 	
 	
 	private function getSystemConfig(){
 		return include 'config.php';
 	}
+	
+	private function getAppConfig(){
+		$path = WIND_PATH;
+		$pos = strrpos($path,DIRECTORY_SEPARATOR);
+		if($post+1 == strlen($path)) {
+			$path = rtrim($path,DIRECTORY_SEPARATOR);
+			$pos = strrpos($path,DIRECTORY_SEPARATOR);
+		}
+		$path = substr($path,0,$pos+1);
+		$appConfigFile = $path.'config.php';
+		return is_file($appConfigFile) ? include $appConfigFile : array();
+	}
+	
 }
 
 ?>
