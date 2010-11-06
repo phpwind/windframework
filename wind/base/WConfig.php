3090803d1eabf41141b25dcef2fab5b55f5464bc
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-06 08:49:51 +0000

    framework develop Exception/message
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@20 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/WConfig.php
+++ wind/base/WConfig.php
@@ -20,19 +20,19 @@ abstract class WConfig extends WModule {
 	 * @param array $configSystem
 	 * @param array $configCustom
 	 */
-	public function parse($configSystem, $configCustom) {}
+	public function parse($configSystem, $configCustom);
 	
 	/**
 	 * 配置信息解析方法
 	 * @param xml $configSystem
 	 * @param xml $configCustom
 	 */
-	public function parseXML($configSystem, $configCustom) {}
+	public function parseXML($configSystem, $configCustom);
 	
 	/**
 	 * 根据配置名称获得配置信息
 	 * @param string $configName
 	 */
-	public function getConfig($configName) {}
+	public function getConfig($configName);
 
 }
