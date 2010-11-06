73963a5efe2a5853bdcc3fe1a10273a775e80d80
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-06 08:33:54 +0000

    配置部署 -> 基类调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@18 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/WSystemConfig.php
+++ wind/core/WSystemConfig.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * @author Qian Su <aoxue.1988.su.qian@163.com> 2010-11-3
+ * @author Qiong Wu <papa0924@gmail.com> 2010-11-6
  * @link http://www.phpwind.com
  * @copyright Copyright &copy; 2003-2110 phpwind.com
  * @license 
  */
 
 /**
- * 对配置文件的解析
  * the last known user to change this file in the repository  <$LastChangedBy$>
- * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @author Qiong Wu <papa0924@gmail.com>
  * @version $Id$ 
- * @package
+ * @package 
  */
 class WSystemConfig extends WModule implements WContext {
 	private $systemConfig;
