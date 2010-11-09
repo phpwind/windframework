b0d11c3da47c8f378be2c68d1165a04bdef20fb2
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-09 03:28:37 +0000

    删除调试代码
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@43 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/WWebApplicationController.php
+++ wind/core/WWebApplicationController.php
@@ -16,10 +16,6 @@ class WWebApplicationController implements WApplicationController {
 	private $router = NULL;
 	
 	function processRequest($request) {
-		$c = $request->getGet('c');
-		$a = $request->getGet('a');
-		$obj = new $c();
-		$obj->$a($request);
 	}
 	
 	function initApplicationController() {
