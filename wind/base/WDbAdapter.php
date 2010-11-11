89646e86ac4b6be185c915eda7f242b0f8771f2d
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-11 06:03:13 +0000

    框架DB部署
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@67 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/WDbAdapter.php
+++ wind/base/WDbAdapter.php
@@ -5,3 +5,59 @@
  * @copyright Copyright &copy; 2003-2110 phpwind.com
  * @license 
  */
+
+
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+abstract class WDbAdapter{
+	protected $linked = array();
+	protected $linking = null;
+	protected $queryId = '';
+	protected $last_sql = '';
+	protected $last_errstr = '';
+	protected $last_errcode = 0;
+	protected $sqlBuilder = null;
+	protected $is_conntected = 0;
+	
+	
+	protected $transCounter = 0;
+	public $enableSavePoint = 0;
+	protected $savepoint = array();
+	protected function __construct(){
+		
+	}
+	
+	private function parseConfig(){
+		
+	}
+	private function parseDSN();
+	protected function connect();
+	public function addConnect();
+	public function switchConnect();
+	public function switchDataBase();
+	public function query();
+	public function exceute();
+	public function insert();
+	public function update();
+	public function select();
+	public function delete();
+	public function getAll();
+	
+	protected function close();
+	protected function dispose();
+	public function savePoint();
+	public function beginTrans();
+	public function rollbackTrans();
+	public function getAffectedRows();
+	public function getInsertId();
+	public function getLastSql(){
+		return $this->sql;
+	}
+	
+	
+
+}
