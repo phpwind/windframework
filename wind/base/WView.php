cf7b1181ef0bfb75d696606a0861df49c276da9f
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-12 03:39:40 +0000

    视图试实现
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@75 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/WView.php
+++ wind/base/WView.php
@@ -6,64 +6,107 @@
  * @license 
  */
 class WView {
-	private static $configs = array();
-	private static $instance;
-	private $viewContents = '';
-	private $var;//模板变量输出
+	private static $configs = array ();
+	private static $instance = null;
+	private $viewContents = ''; //输出的内容
+	private $var; //模板变量输出
 	
+
 	//TODO 试图配置信息解析
 	public function getInstance($config = NULL) {
-		$config = array('engine' => 'php', 'cache_path' => '');
+		if (self::$instance == null) {
+			$this->_initView ( $config );
+			self::$instance = new WView ();
+		}
+		return self::$instance;
 	}
-	
+	//TODO
+	private function _initView($config) {
+		if (! $config)
+			$config = array ('engine' => 'php', 'cache_path' => '', 'tmpExt' => '.phtml' );
+		$this->config = $config;
+	}
 	public function assign($val, $value) {
-		if (is_object($val)) {
-			$this->var[$val] = get_object_vals($val);
+		if (is_object ( $val )) {
+			$this->var [$val] = get_object_vals ( $val );
 		} else {
-			$this->var[$val] = $value;
+			$this->var [$val] = $value;
 		}
 	}
 	public function __set($val, $value) {
-		$this->assign($val, $value);
+		$this->assign ( $val, $value );
 	}
 	public function __get($val) {
-		if (isset($this->val[$val])) return $this->val[$val];
+		if (isset ( $this->val [$val] ))
+			return $this->val [$val];
 		return null;
 	}
 	
 	//TODO 有效性判断，此处是否可以将具体的实现转移至工具类中，以便其它地方调用
-	public function redirect($url, $params=array(), $delayTime=0, $msg='') {
-		$url = str_replace(array("\n", "\r"), '', $url);
+	public function redirect($url, $params = array(), $delayTime = 0, $msg = '') {
+		$url = str_replace ( array ("\n", "\r" ), '', $url );
 		$parse = '';
-		foreach ((array)$params as $key => $value) {
-			if ($value != '') $parse .= "{$key}={$value}&";
+		foreach ( ( array ) $params as $key => $value ) {
+			if ($value != '')
+				$parse .= "{$key}={$value}&";
 		}
-		(strpos($url, '?') === false) ? $url .= "?{$parse}" : "&{$parse}";
+		(strpos ( $url, '?' ) === false) ? $url .= "?{$parse}" : "&{$parse}";
 		if ($msg == '') {
 			$msg = "系统将在{$delayTime}秒之后，自动跳转到!";
 		}
-		$delayTime = intval($delayTime);
-		if (!headers_sent()) {
+		$delayTime = intval ( $delayTime );
+		if (! headers_sent ()) {
 			if ($delayTime === 0) {
-				header('Location:' . $url);
-				exit();
+				header ( 'Location:' . $url );
+				exit ();
 			} else {
-				header("refresh:{$delayTime};url={$url}");
-            	exit($msg);
+				header ( "refresh:{$delayTime};url={$url}" );
+				exit ( $msg );
 			}
 		}
 		$jumpStr = "<meta http-equiv='Refresh' content='{$delayTime};URL={$url}'>";
 		($delayTime > 0) && $jumpStr .= $msg;
-		exit($jumpStr);
+		exit ( $jumpStr );
 	}
 	
-	public function display($templateFile='', $charset='', $contentType='') {
-		
+	/**
+	 * 
+	 * @param string $templateFile 模板名称
+	 * @param string $charset 输出的字符集（默认为系统的）
+	 * @param string $contentType 输出的类型
+	 */
+	public function display($templateFile = '', $charset = '', $contentType = '') {
+		$this->fetch ( $templateFile, $charset, $contentType, false );
 	}
 	
+	/**
+	 * @param string $templateFile 模板名称
+	 * @param string $charset 输出的字符集（默认为系统的）
+	 * @param string $contentType 输出的类型
+	 * @param boolean $return 是否返回还是立即显示
+	 */
 	//TODO 获得模板内容
-	public function fetch($templateFile='', $charset='', $contentType='text/html', $return=true){
-		
+	public function fetch($templateFile = '', $charset = '', $contentType = 'text/html', $return = true) {
+		$templateFile = $this->config ['cache_path'] . $templateFile . $this->config ['tmpExt'];
+		if (! file_exists ( $templateFile ))
+			return;
+		(! $charset) && $charset = $this->config ['charset'];
+		(! $contentType) && $contentType = 'text/html';
+		header ( "Content-Type:" . $contentType . "; charset=" . $charset );
+		header ( "Cache-control: private" ); //支持页面回跳
+		if (extension_loaded ( 'zlib' ))
+			ob_start ( 'ob_gzhandler' ); //开启缓存输出
+		else
+			ob_start ();
+		extract ( $this->val, EXTR_OVERWRITE );
+		include $templateFile;
+		$this->viewContent = ob_get_contents ();
+		if ($return) {
+			ob_end_clean();
+			return $this->viewContent;
+		} else {
+			ob_end_flush();
+		}
 	}
 	//TODO  cache方面，同时能调用解析不同模板引擎
 }
