db6c925c3a2e59306544683764ff1988ec3a08b3
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-18 02:37:47 +0000

    配置文件视图模块，添加模板的编译路径和缓存路径的配置
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@137 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/config.php
+++ wind/config.php
@@ -3,56 +3,58 @@
  * @author Qiong Wu <papa0924@gmail.com> 2010-11-6
  * @link http://www.phpwind.com
  * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
+ * @license
  */
 
 /*
  * 框架核心配置文件 <路由配置，应用配置，过滤器配置...>
- * 
+ *
  * */
 $sysConfig = array(
 	/* 保存过滤路径 */
 	'filters' => array(),
-	
+
 	/* 应用路径配置 */
 	'modules' => array(
 		'default' => 'actionControllers'
 	),
-	
+
 	/* 模板相关配置信息 */
 	'view' => array(
 		'viewPath' => 'template',  //模板文件路径
 		'tpl' => 'index',  //默认的模板文件
 		'engine' => 'default',  //default,smarty
-		'ext' => 'htm' //模板文件后缀名
-	), 
-	
+		'ext' => 'htm', //模板文件后缀名
+		'cacheDir' => 'cache',//模板文件的缓存路径 
+		'compileDir' => 'compile',//模板编译路径
+	),
+
 	/* 模板引擎配置信息 */
 	'viewEngine' => array(
-		'default' => 'WIND:core.WViewer', 
-		'pw' => 'WIND:core.WPWViewer', 
-		'smarty' => 'core.WSmartyViewer'
+		'default' => 'WIND:core.WViewer',
+		'pw' => 'WIND:core.WPWViewer',
+		'smarty' => 'libs.WSmarty',
 	),
-	
+
 	/*
 	 * 这个配置选项有两个值，配置操作结构是基于WActionController或者基于WAction
 	 * 基于WAction：则目录结构是actionControllers/controller/action.php
 	 * 基于WActionController：则目录结构是actionControllers/controller.php
 	 * */
 	'controller' => 'WActionController',
-	
+
 	/* 路由策略配置 */
 	'router' => array(
 		'parser' => 'url'
-	), 
-	
+	),
+
 	/* URL路由规则配置  */
 	'urlRule' => array(
-		'action' => 'run', 
-		'controller' => 'index', 
+		'action' => 'run',
+		'controller' => 'index',
 		'module' => ''
 	),
-	 
+
 	/* 路由解析器配置 */
 	'routerParser' => array(
 		'url' => 'WIND:core.WUrlRouter'
