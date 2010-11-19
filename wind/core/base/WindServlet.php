cf9cbeb1a10482fe0d66c6f20f5ae81e9f4e911f
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-19 12:14:31 +0000

    命名方式调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@186 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/base/WindServlet.php
+++ wind/core/base/WindServlet.php
@@ -6,6 +6,9 @@
  * @license 
  */
 L::import('WIND:component.exception.WindException');
+L::import('WIND:component.request.WindHttpRequest');
+L::import('WIND:component.request.WindHttpResponse');
+
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
@@ -26,16 +29,16 @@ abstract class WindServlet {
 	
 	protected function __construct() {
 		try {
-			$this->reuqest = W::getInstance('WHttpRequest');
+			$this->reuqest = WindHttpRequest::getInstance();
 			$this->response = $this->reuqest->getResponse();
 		
 		} catch (Exception $exception) {
-			throw new WException('init action servlet failed!!');
+			throw new WindException('init action servlet failed!!');
 		}
 	}
 	
 	public function run() {
-		if ($this->reuqest === null || $this->response === null) throw new WException('init action servlet failed!!');
+		if ($this->reuqest === null || $this->response === null) throw new WindException('init action servlet failed!!');
 		
 		$this->service($this->reuqest, $this->response);
 		
@@ -50,11 +53,11 @@ abstract class WindServlet {
 	 * them to the <code>do</code><i>XXX</i> methods defined in 
 	 * this class.There's no need to override this method.
 	 * 
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
-	 * @throws WException
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
+	 * @throws WindException
 	 */
-	protected function service(WHttpRequest $request, WHttpResponse $response) {
+	protected function service(WindHttpRequest $request, WindHttpResponse $response) {
 		$method = $request->getRequestMethod();
 		
 		if (strcasecmp($method, self::METHOD_GET) == 0) {
@@ -77,78 +80,78 @@ abstract class WindServlet {
 
 		} else {
 			$errMsg = 'your request method is not supported!!!';
-			$response->sendError(WHttpResponse::SC_METHOD_NOT_ALLOWED, $errMsg);
+			$response->sendError(WindHttpResponse::SC_METHOD_NOT_ALLOWED, $errMsg);
 		}
 	}
 	
 	/**
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
-	 * @throws WException
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
+	 * @throws WindException
 	 */
-	protected function doPost(WHttpRequest $request, WHttpResponse $response) {
+	protected function doPost(WindHttpRequest $request, WindHttpResponse $response) {
 		$protocol = $request->getProtocol();
 		$msg = "The method post is not supported.";
 		if (!$protocol || (strpos($protocol, '1.1')) !== false) {
-			$response->sendError(WHttpResponse::SC_METHOD_NOT_ALLOWED, $msg);
+			$response->sendError(WindHttpResponse::SC_METHOD_NOT_ALLOWED, $msg);
 		} else
 			$this->process($request, $response);
 	}
 	
 	/**
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
-	 * @throws WException
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
+	 * @throws WindException
 	 */
-	protected function doGet(WHttpRequest $request, WHttpResponse $response) {
+	protected function doGet(WindHttpRequest $request, WindHttpResponse $response) {
 		$protocol = $request->getProtocol();
 		$msg = "The method get is not supported.";
 		if (!$protocol || (strpos($protocol, '1.1')) !== false) {
-			$response->sendError(WHttpResponse::SC_METHOD_NOT_ALLOWED, $msg);
+			$response->sendError(WindHttpResponse::SC_METHOD_NOT_ALLOWED, $msg);
 		} else
 			$this->process($request, $response);
 	}
 	
 	/**
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
-	 * @throws WException
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
+	 * @throws WindException
 	 */
-	protected function doPut(WHttpRequest $request, WHttpResponse $response) {
+	protected function doPut(WindHttpRequest $request, WindHttpResponse $response) {
 		$this->process($request, $response);
 	}
 	
 	/**
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
-	 * @throws WException
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
+	 * @throws WindException
 	 */
-	protected function doDelete(WHttpRequest $request, WHttpResponse $response) {
+	protected function doDelete(WindHttpRequest $request, WindHttpResponse $response) {
 		$this->process($request, $response);
 	}
 	
 	/**
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
 	 */
-	protected function doTrace(WHttpRequest $request, WHttpResponse $response) {
+	protected function doTrace(WindHttpRequest $request, WindHttpResponse $response) {
 
 	}
 	
 	/**
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
 	 */
-	protected function doOptions(WHttpRequest $request, WHttpResponse $response) {
+	protected function doOptions(WindHttpRequest $request, WindHttpResponse $response) {
 
 	}
 	
 	/**
-	 * @param WHttpRequest $request
-	 * @param WHttpResponse $response
-	 * @throws WException
+	 * @param WindHttpRequest $request
+	 * @param WindHttpResponse $response
+	 * @throws WindException
 	 */
-	protected function doHead(WHttpRequest $request, WHttpResponse $response) {
+	protected function doHead(WindHttpRequest $request, WindHttpResponse $response) {
 
 	}
 
