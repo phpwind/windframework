a5144051994a8720fc3ef860fe957f32657c9df2
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-22 02:34:41 +0000

    配置文件的接口
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@191 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/base/impl/WindConfig.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author xiaoxia xu <x_824@sina.com> 2010-11-19
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+interface WindConfig {
+	const app = 'app';
+	const appName = 'appName';
+	const appPath = 'appPath';
+	const appConfig = 'appConfig';
+	
+	const isOpen = 'isOpen';
+	const describe = 'describe';
+	
+	const filters = 'filters';
+	const filter = 'filter';
+	const filterName = 'filterName';
+	const filterPath = 'filterPath';
+	
+	const template = 'template';
+	const templateDir = 'templateDir';
+	const compileDir = 'compileDir';
+	const cacheDir = 'cacheDir';
+	const templateExt = 'templateExt';
+	const engine = 'engine';
+	
+	const urlRule = 'urlRule';
+	const routerPase = 'routerPase';
+}
