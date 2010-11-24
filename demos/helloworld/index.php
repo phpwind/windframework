6801d689de07a6edb8921e9a4a00a6d1d15900a4
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-24 06:51:49 +0000

    代码调整测试
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@238 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/helloworld/index.php
+++ demos/helloworld/index.php
@@ -3,15 +3,17 @@
  * @author Qiong Wu <papa0924@gmail.com> 2010-11-6
  * @link http://www.phpwind.com
  * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
+ * @license
  */
 header("Content-type: text/html; charset=gbk");
-define('R_P', dirname(__FILE__));
+define('R_P', dirname ( __FILE__ ));
 define('F_P', R_P . '/../../wind/');
 define('C_P', R_P . '/wind/');
+define('CONFIG_CACHE_PATH', R_P . '/data');//设置缓存文件位置
 
-require_once (R_P . '/data/config.php');
 require_once (F_P . '/wind.php');
 
-W::setApps('TEST', array('rootPath' => R_P), true);
+//W::setApps('TEST', array('rootPath' => R_P), true);
+//W::setCurrentApp('helloworld');//设置当前应用的名字
+
 WindFrontController::getInstance()->run();
