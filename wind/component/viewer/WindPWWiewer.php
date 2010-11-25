c7f6661e5734170098f08e6a44bece2c009becab
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-25 09:19:05 +0000

    布局方式
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@251 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/viewer/WindPWWiewer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-11-25
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+/**
+ * PW模板引擎支持
+ * 
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindPWWiewer extends WindViewer {
+
+}
