f66dfa780bb3a57892060b05969c8d55dd6dc8a0
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-26 04:14:09 +0000

    工具类
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@290 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/utility/Common.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @author xiaoxia xu <x_824@sina.com> 2010-11-26
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+class Common {
+
+	/**
+	 * 变量导出为字符串
+	 *
+	 * @param mixed $input 变量
+	 * @param string $indent 缩进
+	 * @return string
+	 */
+	public function varExport($input, $indent = '') {
+		switch (gettype($input)) {
+			case 'string':
+				return "'" . str_replace(array("\\", "'"), array("\\\\", "\'"), $input) . "'";
+			case 'array':
+				$output = "array(\r\n";
+				foreach ($input as $key => $value) {
+					$output .= $indent . "\t" . Common::varExport($key, $indent . "\t") . ' => ' . Common::varExport($value, $indent . "\t");
+					$output .= ",\r\n";
+				}
+				$output .= $indent . ')';
+				return $output;
+			case 'boolean':
+				return $input ? 'true' : 'false';
+			case 'NULL':
+				return 'NULL';
+			case 'integer':
+			case 'double':
+			case 'float':
+				return "'" . (string) $input . "'";
+		}
+		return 'NULL';
+	}
+	
+	/**
+	 * 写文件
+	 *
+	 * @param string $fileName 文件绝对路径
+	 * @param string $data 数据
+	 * @param string $method 读写模式
+	 * @param bool $ifLock 是否锁文件
+	 * @param bool $ifCheckPath 是否检查文件名中的“..”
+	 * @param bool $ifChmod 是否将文件属性改为可读写
+	 * @return bool 是否写入成功
+	 */
+	public function writeover($fileName, $data, $method = 'rb+', $ifLock = true, $ifCheckPath = true, $ifChmod = true) {
+		$tmpname = strtolower($fileName);
+		$tmparray = array(':\/\/', "\0");
+		$tmparray[] = '..';
+		if (str_replace($tmparray, '', $tmpname) != $tmpname) return false;
+		
+		@touch($fileName);
+		if (!$handle = @fopen($fileName, $method)) return false;
+		$ifLock && flock($handle, LOCK_EX);
+		$writeCheck = fwrite($handle, $data);
+		$method == 'rb+' && ftruncate($handle, strlen($data));
+		fclose($handle);
+		$ifChmod && @chmod($fileName, 0777);
+		return $writeCheck;
+	}
+}
