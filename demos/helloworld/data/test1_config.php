87751234ecd3898371c41e6bb0feb8ca2aec2778
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-26 04:14:14 +0000

    命名方式调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@291 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/helloworld/data/test1_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @author Qiong Wu <papa0924@gmail.com> 2010-11-22
- * @link http://www.phpwind.com
- * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
- */
--- demos/helloworld/data/test_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @author Qiong Wu <papa0924@gmail.com> 2010-11-19
- * @link http://www.phpwind.com
- * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
- */
-$testConfig = array();
