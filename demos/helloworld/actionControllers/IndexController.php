23938284e4061a9613f0eb1996605ba7cd7ca177
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-11-30 07:03:29 +0000

    代码调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@353 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/helloworld/actionControllers/IndexController.php
+++ demos/helloworld/actionControllers/IndexController.php
@@ -3,21 +3,26 @@
  * @author Qiong Wu <papa0924@gmail.com> 2010-11-9
  * @link http://www.phpwind.com
  * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
+ * @license
  */
 
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
- * @version $Id$ 
- * @package 
+ * @version $Id$
+ * @package
  */
 class IndexController extends WindController {
-	
+
 	public function run() {
-		WindErrorMessage::getInstance()->showError();
+		$userForm = WindFormFactory::getInstance()->getFormHandle();
+		echo $userForm;
+		echo "mmmmm";
+		$this->setViewData(array('test'=>'xxxxxxxxxx'));
+//		$this->getModelAndView()->setAction('run','otherControllers.otherController');
+		//WindErrorMessage::getInstance()->showError();
 //		$this->setViewData(array('test' => 'xxxxxxxxxx'));
-		$this->getModelAndView()->setAction('run','otherControllers.otherController');
+		//this->getModelAndView()->setAction('run','otherControllers.otherController');
 	}
-	
+
 }
