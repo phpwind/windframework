5ce01388fc070dc26b5a001d2060c53e1c3999d9
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-03 03:37:45 +0000

    delete log框架DB部署
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@396 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/db/dbtest.php
+++ demos/db/dbtest.php
@@ -31,11 +31,11 @@ $dsn ="mssql://username:password@localhost:3306/dbname/gbk/1/1/";
 $config = array ('phpwind' => $phpwind, 'beta' => $phpwind_beta,'tt'=>$config );
 $manager = WindDbManager::getInstance ( $config );
 $db = $manager->dbDriverFactory ();
-$option ['table'] = 'pw_members a';
-$option ['where'] = array ('a =223 and b=33' );
-$option ['where'] = array('a = :a AND B=:b',array(':a'=>'sss',':b'=>'sss'));
-$option ['field'] = array ('a.uid' => 'ids', 'a.username' );
-$option ['join'] = array ('pw_posts' => array ('left', 'a.uid=b.authorid', 'b' ) );
+$option ''table'' = 'pw_members a';
+$option ''where'' = array ('a =223 and b=33' );
+$option ''where'' = array('a = :a AND B=:b',array(':a'=>'sss',':b'=>'sss'));
+$option ''field'' = array ('a.uid' => 'ids', 'a.username' );
+$option ''join'' = array ('pw_posts' => array ('left', 'a.uid=b.authorid', 'b' ) );
 //不指定db连接
 $db->select ("SELECT * from pw_members");
 //指定db连接
@@ -47,19 +47,19 @@ print_r($result);
 //$result = $mysql->getAllResult ();
 //更新数据
 /*
-$option ['set'] = array ('username' => "test" );
-$option ['table'] = 'pw_members';
-$option ['where'] = array ('lt' => array ('uid', 1 ) );
+$option ''set'' = array ('username' => "test" );
+$option ''table'' = 'pw_members';
+$option ''where'' = array ('lt' => array ('uid', 1 ) );
 $mysql->update ( $option, 'phpwind' );
 //新增数据
-$option ['where'] = array ('eq' => array ('uid', 1 ) );
-$option ['data'] = array ("asfafafafaf" );
-$option ['table'] = 'pw_members';
-$option ['field'] = array ('username' );
+$option ''where'' = array ('eq' => array ('uid', 1 ) );
+$option ''data'' = array ("asfafafafaf" );
+$option ''table'' = 'pw_members';
+$option ''field'' = array ('username' );
 $mysql->insert ( $option, 'phpwind' );
 //删除数据
-$option ['table'] = 'pw_members';
-$option ['where'] = array ('eq' => array ('uid', 22 ) );
+$option ''table'' = 'pw_members';
+$option ''where'' = array ('eq' => array ('uid', 22 ) );
 $mysql->delete ( $option, 'phpwind' );
 print_r($result);
 
@@ -72,10 +72,10 @@ $phpwind_beta = array ('charset' => 'gb2312', 'dbtype' => 'mssql', 'dbname' => '
 $config = array ('betat' => $phpwind_beta );
 //$mssql = new WindMsSql ( $config );
 /**
-$option ['table'] = 'pw_members a';
-$option ['where'] = array ('lt' => array ('a.uid', 10 ) );
-$option ['field'] = array ('a.uid' => 'ids', 'a.username' );
-$option ['join'] = array ('pw_posts' => array ('left', 'a.uid=b.authorid', 'b' ) );
+$option ''table'' = 'pw_members a';
+$option ''where'' = array ('lt' => array ('a.uid', 10 ) );
+$option ''field'' = array ('a.uid' => 'ids', 'a.username' );
+$option ''join'' = array ('pw_posts' => array ('left', 'a.uid=b.authorid', 'b' ) );
 $mssql->select ( $option );
 $result = $mssql->getAllResult ();
 //print_r($result);
@@ -111,6 +111,59 @@ $config = array (
 	)
 );
 
+$config = Array
+(
+    'phpwind' => Array
+        (
+            'dbtype' => 'mysql',
+            'dbhost' => 'localhost',
+            'dbuser' => 'root',
+            'dbpass' => 'suqian0512h',
+            'dbport' => '3306',
+            'dbname' => 'phpwind',
+            'path' => 'WIND:component.db.WindMySql',
+            'className' => 'WindMySql',
+        ),
+
+    'user' => Array
+        (
+            'dbtype' => 'mssql',
+            'dbhost' => 'localhost',
+            'dbuser' => 'sa',
+            'dbpass' => '151@suqian',
+            'dbname' => 'phpwind',
+            'path' => 'WIND:component.db.WindMsSql',
+            'className' => 'WindMsSql'
+        )
+
+);
+$dbConfig = array(
+		'phpwind' => array(
+			'dbtype' => 'mysql',
+			'dbhost' => 'localhost',
+			'dbuser' => 'root',
+			'dbpass' => 'suqian0512h',
+			'dbport' => '3306',
+			'dbname' => 'phpwind',
+		),
+		'user' => array(
+			'dbtype' => 'mssql',
+			'dbhost' => 'localhost',
+			'dbuser' => 'sa',
+			'dbpass' => '151@suqian',
+			'dbname' => 'phpwind',
+		),
+	);
+	$dbDriver =  array(
+		'mysql' => array(
+			'path' => 'WIND:component.db.WindMySql',
+			'className' => 'WindMySql',
+		),
+		'mssql' => array(
+			'path' => 'WIND:component.db.WindMsSql',
+			'className' => 'WindMsSql',
+		),
+	);
 $dsn ="mssql://username:password@localhost:3306/dbname/gbk/1/1/";
 	
 //print_r ( $mssql->parseDSN($dsn));
