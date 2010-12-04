8b1f8235f0ae6227d1d8ad732cb7ad1f51ebc864
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-04 04:17:51 +0000

    删除测试文件
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@415 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/formtest/compile/config.php
+++
@@ -1,9 +0,0 @@
-<?php
- return array(
-	'FormTest' => array(
-		'name' => 'FormTest',
-		'configPath' => 'D:\\PHPAPP\\phpwindframework\\trunk\\demos\\formtest\\compile\\FormTest_config.php',
-		'rootPath' => 'D:\\PHPAPP\\phpwindframework\\trunk\\demos\\formtest',
-	),
-);
-?>
