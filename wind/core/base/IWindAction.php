a881691a1baa38c35377cfab1b0e971dcc62de8f
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-04 05:01:49 +0000

    bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@418 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/base/IWindAction.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-12-3
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+interface IWindAction {
+	const INPUT_TYPE_GET = 'get';
+	const INPUT_TYPE_POST = 'post';
+	const INPUT_TYPE_COOKIE = 'cookie';
+}
