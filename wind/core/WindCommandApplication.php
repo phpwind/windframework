510a1d7508bba064f02d6a5099a7793d6154d3fb
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-06 13:04:20 +0000

    多应用支持
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@454 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/WindCommandApplication.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-12-6
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+L::import('WIND:core.base.IWindApplication');
+
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindCommandApplication implements IWindApplication {
+	
+	/**
+	 * 
+	 */
+	public function init() {
+		
+	}
+
+	/**
+	 * @param unknown_type $request
+	 * @param unknown_type $response
+	 */
+	public function processRequest($request, $response) {
+		
+	}
+
+	/**
+	 * 
+	 */
+	public function destory() {
+		
+	}
+
+
+}
