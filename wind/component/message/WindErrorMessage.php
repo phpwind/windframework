71c26f915bfaa11aa27181952ec46c93384b7600
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-08 07:30:11 +0000

    代码调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@474 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/message/WindErrorMessage.php
+++ wind/component/message/WindErrorMessage.php
@@ -3,56 +3,55 @@
  * @author Qiong Wu <papa0924@gmail.com> 2010-11-25
  * @link http://www.phpwind.com
  * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
+ * @license
  */
 
 L::import('WIND:component.message.WindMessage');
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
- * @version $Id$ 
- * @package 
+ * @version $Id$
+ * @package
  */
 class WindErrorMessage extends WindMessage {
 	private $errorAction = '';
 	private static $instance = null;
-	
+
 	private function __construct() {}
-	
+
 	private $forward = null;
-	
+
 	/**
 	 * 添加错误信息
-	 * 
+	 *
 	 * @param string $message
 	 * @param string $key
 	 */
 	public function addError($message, $key = '') {
 		parent::addMessage($message, $key);
 	}
-	
+
 	/**
 	 * 返回错误信息
-	 * 
+	 *
 	 * @param string $key
 	 * @return Ambigous <string, multitype:>
 	 */
 	public function getError($key = '') {
 		return parent::getMessage($key);
 	}
-	
+
 	/**
 	 * 设置错误处理操作
 	 */
 	public function setErrorAction($action = '') {
 		$action && $this->errorAction = $action;
 	}
-	
+
 	/**
 	 * 重定向错误处理
 	 */
 	public function sendError() {
-		print_r($this->getError());
 		if (count($this->getError()) == 0) return;
 		if ($this->errorAction === '') {
 			$this->errorAction = C::getErrorMessage(IWindConfig::ERRORMESSAGE_ERRORACTION);
@@ -65,7 +64,7 @@ class WindErrorMessage extends WindMessage {
 		$this->clear();
 		exit();
 	}
-	
+
 	/**
 	 * @return WindErrorMessage
 	 */
