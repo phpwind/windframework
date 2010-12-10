5fa89fd89eec4cd79205c8256b6cefd5795291ef
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-10 10:26:21 +0000

    FRAMEWORD UNIT TEST
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@553 18ba2127-5a84-46d4-baec-3457e417f034

--- test/component/db/drivers/mysql/WindMySqlBuilderTest.php
+++ test/component/db/drivers/mysql/WindMySqlBuilderTest.php
@@ -5,7 +5,9 @@
  * @copyright Copyright &copy; 2003-2110 phpwind.com
  * @license 
  */
-
+require_once('../../../../BaseTestCase.php');
+L::import(WIND_PATH . '/component/exception/WindException.php');
+L::import(WIND_PATH . '/component/db/drivers/mysql/WindMySqlBuilder.php');
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qian Su <aoxue.1988.su.qian@163.com>
@@ -13,5 +15,141 @@
  * @package 
  */
 class WindMySqlBuilderTest extends BaseTestCase{
+
+	private $config = '';
+	private $WindMySqlBuilder = null;
 	
+	
+	public function init(){
+		
+		if($this->WindMySqlBuilder == null){
+			$this->config = C::getDataBaseConnection('phpwind_8');
+			$this->WindMySqlBuilder = new WindMySqlBuilder($this->config);
+		}
+	}
+	
+	public static function provider(){
+		return array(
+			array('pw_posts','','','',''),
+			array('pw_posts','a.uid=pw_posts.authorid','','',''),
+			array('pw_posts','a.uid=b.authorid','b','',''),
+			array('pw_posts','a.uid=b.authorid','b','subject,pid',''),
+			array('pw_posts','a.uid=b.authorid','b','b.subject,b.pid',''),
+			array('pw_posts','a.uid=b.authorid','b',array('subject','pid'),'phpwind_8'),
+		);
+	}
+	
+	public function setUp() {
+		$this->init();
+	}
+	
+	public function tearDown(){
+		$this->WindMySqlBuilder->reset();
+	}
+	
+	/**
+     * @dataProvider provider
+     */
+
+	public function testFrom($table,$joinWhere,$table_alias,$fields,$schema){
+
+		$builder = $this->WindMySqlBuilder->from($table,$table_alias,$fields,$schema);
+		$from = $this->WindMySqlBuilder->getSql(WindSqlBuilder::FROM);
+		$field = $fields ? $this->WindMySqlBuilder->getSql(WindSqlBuilder::FIELD):true;
+		$this->assertTrue($from && $field && ($builder instanceof WindSqlBuilder));
+	}
+	
+	public function testDistinct(){
+		$builder = $this->WindMySqlBuilder->distinct(true);
+		$distinct = $this->WindMySqlBuilder->getSql(WindSqlBuilder::DISTINCT);
+		$this->assertEquals(WindSqlBuilder::SQL_DISTINCT,$distinct);
+		$this->assertTrue(($builder instanceof WindSqlBuilder));
+	}
+	
+	public function testField(){
+		$builder = $this->WindMySqlBuilder->field('username,uid');
+		$field = $this->WindMySqlBuilder->getSql(WindSqlBuilder::FIELD);
+		$this->assertTrue($field && ($builder instanceof WindSqlBuilder));
+	}
+	
+	/**
+     * @dataProvider provider
+     */
+	public function testJoin($table,$joinWhere,$table_alias,$fields,$schema){
+		$this->assertTrue($this->_join(WindSqlBuilder::INNER,$table,$joinWhere,$table_alias,$fields,$schema));
+	}
+	
+	
+	/**
+     * @dataProvider provider
+     */
+	public function testLeftJoin(){
+		$this->assertTrue($this->_join(WindSqlBuilder::LEFT,$table,$joinWhere,$table_alias,$fields,$schema));
+	}
+	
+	/**
+     * @dataProvider provider
+     */
+	public function testRightJoin(){
+		$this->assertTrue($this->_join(WindSqlBuilder::RIGHT,$table,$joinWhere,$table_alias,$fields,$schema));
+	}
+	
+	/**
+     * @dataProvider provider
+     */
+	public function testFullJoin(){
+		$this->assertTrue($this->_join(WindSqlBuilder::FULL,$table,$joinWhere,$table_alias,$fields,$schema));
+	}
+	
+	public function testInnerJoin(){
+		$this->assertTrue($this->_join(WindSqlBuilder::INNER,$table,$joinWhere,$table_alias,$fields,$schema));
+	}
+	
+	public function testCrossJoin(){
+		
+	}
+	
+	public function testWhere(){
+		
+	}
+	
+	public function testOrWhere(){
+		
+	}
+	
+	public function testGroup(){
+		
+	}
+	
+	public function testHaving(){
+		
+	}
+	
+	public function testOrHaving(){
+		
+	}
+	
+	public function testOrder(){
+		
+	}
+	
+	public function testLimit(){
+		
+	}
+	
+	public function testData(){
+		
+	}
+	
+	public function testSet(){
+		
+	}
+	
+	private function _join($joinType,$table,$joinWhere,$table_alias,$fields,$schema){
+		$joinMethod = $joinType.'Join';
+		$builder = $this->WindMySqlBuilder->$joinMethod($table,$joinWhere,$table_alias,$fields,$schema);
+		$join = $this->WindMySqlBuilder->getSql(WindSqlBuilder::JOIN);
+		$field = $fields ? $this->WindMySqlBuilder->getSql(WindSqlBuilder::FIELD):true;
+		return $join && $field && ($builder instanceof WindSqlBuilder);
+	}
 }
--- test/component/db/drivers/mysql/WindMySqlTest.php
+++ test/component/db/drivers/mysql/WindMySqlTest.php
@@ -6,6 +6,7 @@
  * @license 
  */
 require_once('../../../../BaseTestCase.php');
+L::import(WIND_PATH . '/component/exception/WindException.php');
 L::import(WIND_PATH . '/component/db/drivers/mysql/WindMySql.php');
 L::import(WIND_PATH . '/component/db/drivers/mysql/WindMySqlBuilder.php');
 
@@ -17,21 +18,106 @@ L::import(WIND_PATH . '/component/db/drivers/mysql/WindMySqlBuilder.php');
  */
 class WindMySqlTest extends  BaseTestCase{
 	private $WindMySql = null;
+	private $table = 'pw_actions';
+	private $selectSql = 'SELECT images,descrip,name FROM pw_actions';
+	private $insertSql = "INSERT pw_actions ( images,descrip,name )VALUES ( 'a' ,'b','c' ) ";
+	private $updateSql = "UPDATE pw_actions SET name= 'suqian'  WHERE id = 1";
+	private $deleteSql = "DELETE FROM pw_actions WHERE id = 2";
+	private $config = array();
 	
-	public function setUp() {
-		if($this->WindMySql == null){
-			$config = C::getDataBaseConnection('phpwind_8');
-			$this->WindMySql = new WindMySql($config);
-		}
+	public function __construct(){
+		parent::__construct();
+		$this->config = C::getDataBaseConnection('phpwind_8');
+		$this->WindMySql = new WindMySql($this->config);
 	}
+	
 	
 	public function testGetSqlBuilder(){
-		$this->assertTrue($this->WindMySql instanceof WindMySqlBuilder);
+		$this->assertTrue($this->WindMySql->getSqlBuilder() instanceof WindMySqlBuilder);
 	}
 	
+	public function testGetLastSql(){
+		$this->testQuery();
+		$this->assertEquals($this->selectSql,$this->WindMySql->getLastSql());
+	}
+	
+	public function testQuery(){
+		$this->assertTrue($this->WindMySql->query($this->selectSql));
+	}
+	
+	public function testGetAffectedRows(){
+		$this->assertTrue(is_int($this->WindMySql->getAffectedRows()));
+	}
+	
+	public function testGetLastInsertId(){
+		$this->assertTrue(is_int($this->WindMySql->getLastInsertId()));
+	}
+	
+	public function testGetMetaTables(){
+		$this->assertTrue(is_array($this->WindMySql->getMetaTables()));
+	}
+	
+	public function testGetMetaTablesBySchema(){
+		$this->assertTrue(is_array($this->WindMySql->getMetaTables($this->config[IWindDbConfig::CONN_NAME])));
+	}
+	
+	public function testGetMetaColumns(){
+		$this->assertTrue(is_array($this->WindMySql->getMetaColumns($this->table)));
+	}
+	
+	public function testGetAllRow(){
+		$this->testQuery();
+		$this->assertTrue(is_array($this->WindMySql->getAllRow(MYSQL_ASSOC)));
+	}
+	
+	public function testGetRow(){
+		$this->testQuery();
+		$this->assertTrue(is_array($this->WindMySql->getRow(MYSQL_ASSOC)));
+	}
+	
+	public function testInsert(){
+		$this->assertTrue($this->WindMySql->insert($this->insertSql));
+	}
+	
+	public function testUpdate(){
+		$this->assertTrue($this->WindMySql->update($this->updateSql));
+	}
+	
+	public function testDelete(){
+		$this->assertTrue($this->WindMySql->update($this->deleteSql));
+	}
+	
+	public function testGetDriver(){
+		$this->assertEquals($this->config[IWindDbConfig::CONN_DRIVER],$this->WindMySql->getDriver());
+	}
+	
+	public function testGetSchema(){
+		$this->assertEquals($this->config[IWindDbConfig::CONN_NAME],$this->WindMySql->getSchema());
+	}
+	
+	public function testGetConfig(){
+		$config = $this->WindMySql->getConfig();
+		$this->assertEquals($this->config[IWindDbConfig::CONN_NAME],$config[IWindDbConfig::CONN_NAME]);
+	}
+	
+	public function testGetConnection(){
+		$this->assertTrue(is_resource($this->WindMySql->getConnection()));
+	}
+	/*
+	public function testBeginTrans(){
+		$this->assertTrue($this->WindMySql->beginTrans());
+	}
+	
+	public function testCommitTrans(){
+		$this->assertTrue($this->WindMySql->commitTrans());
+	}*/
 	
 	
-	public function tearDown() {
+	
+	
+	
+	
+	public function __destruct(){
 		$this->WindMySql = null;
 	}
 }
