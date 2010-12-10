236b52e0cac8c5ca073f316afa4126b78976160e
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-10 01:56:54 +0000

    framework db重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@535 18ba2127-5a84-46d4-baec-3457e417f034

--- test/component/db/WindConnectionManagerTest.php
+++ test/component/db/WindConnectionManagerTest.php
@@ -5,3 +5,13 @@
  * @copyright Copyright &copy; 2003-2110 phpwind.com
  * @license 
  */
+
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindConnectionManagerTest extends BaseTestCase{
+	
+}
--- test/component/db/drivers/mysql/WindMySqlTest.php
+++ test/component/db/drivers/mysql/WindMySqlTest.php
@@ -8,6 +8,12 @@
 require_once(dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'BaseTestCase.php');
 L::import(WIND_PATH . '/component/db/drivers/mysql/WindMySql.php');
 
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
 class WindMySqlTest extends  baseTestCase{
 	
 }
