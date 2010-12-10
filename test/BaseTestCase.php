9e351f2d627c9629fb438b9bb58a81f0453de487
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-10 10:26:33 +0000

    framework db重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@554 18ba2127-5a84-46d4-baec-3457e417f034

--- test/BaseTestCase.php
+++ test/BaseTestCase.php
@@ -7,17 +7,12 @@
  */
 !defined('R_P') && define('R_P', dirname(dirname(__FILE__)));
 require_once 'PHPUnit/Framework/TestCase.php';
-
 require_once R_P .  DIRECTORY_SEPARATOR . 'wind/WindBase.php';
 W::init();
-
+C::init(include R_P . '/test/config.php');
 
 abstract class BaseTestCase extends PHPUnit_Framework_TestCase {
-	public function __construct() {
-		parent::__construct();
-		$appConfig = include R_P . '/test/config.php';
-		C::init($appConfig);
-	}
+
 }
 
 abstract class BaseTestSuite extends PHPUnit_Framework_TestSuite {
