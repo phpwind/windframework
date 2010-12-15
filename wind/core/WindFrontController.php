baa3e8632677b9a11741092acb2689ed8f2f17d2
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-15 08:56:01 +0000

    代码重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@655 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/WindFrontController.php
+++ wind/core/WindFrontController.php
@@ -24,13 +24,13 @@ L::import('WIND:core.base.WindServer');
 class WindFrontController extends WindServer {
 	private $applicationType;
 	
-	public function __construct() {
+	public function __construct($systemConfig = array(), $type) {
 		parent::__construct();
+		$this->applicationType = $type;
 		$this->initConfig();
 	}
 	
-	public function run($applicationType = 'web') {
-		$this->applicationType = $applicationType;
+	public function run() {
 		$this->beforProcess();
 		parent::run();
 		$this->afterProcess();
