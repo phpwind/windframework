d790e6cfa6faa4747fbb8aae806950cc97ab6ee5
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-16 01:58:59 +0000

    framework db重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@663 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/parser/WindXmlParser.php
+++ wind/component/parser/WindXmlParser.php
@@ -14,8 +14,56 @@
  * @package 
  */
 class WindXmlParse{
+	/**
+	 * @var string 节点名称
+	 */
+	const NAME = 'name';
+	/**
+	 * @var Domdocument DOM解析器
+	 */
+	private $dom = null;
+	/**
+	 * @param string $version xml版本
+	 * @param string $encode  xml编码
+	 */
+	public function __construct($version = '1.0',$encode = 'utf-8'){
+		$this->dom = new DOMDocument($version,$encode);
+	}
 	
-	public function parse(){
+	/**
+	 * @param string $filename xml 文件名
+	 * @param int $option 解析选项
+	 * @return array
+	 */
+	public function parse($filename,$option = null){
+		if(!is_file($filename)){
+			return false;
+		}
+		$this->dom->load($filename,$option);
+		$root = $this->dom->documentElement;
+		return $this->buildData($root,$root->nodeName);
 		
 	}
+	
+	/**
+	 * @param DOMNodeList $node 要解析的XMLDOM节点
+	 * @param stiring $lastNodeName 上一个XMLDOM节点名称
+	 * @param array $data 要解析XMLDOM节点的子节点
+	 * @param array $lastData 上一个XMLDOM节点下的子节点
+	 * @return array 返回解析后的值 
+	 */
+	public function buildData($node,$lastNodeName ,&$data = array(),&$lastData=array()){
+		if($node->hasChildNodes()){
+			foreach($node->childNodes as $node){
+				if(3 == $node->nodeType && trim($node->nodeValue)){
+					$lastData[$lastNodeName] = $node->nodeValue;
+				}
+				if(1 == $node->nodeType){
+					$nodeName = ($name = $node->getAttribute(self::NAME)) ? $name : $node->nodeName;
+					$this->buildData($node,$nodeName,$data[$nodeName],$data);
+				}
+			}
+		}
+		return $data;
+	}	
 }
