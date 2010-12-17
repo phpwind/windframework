01b0385ef05538c33ca536e74ad4868cae867011
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-17 05:19:21 +0000

    字符格式化工具
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@694 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/format/WindStringFormat.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Qian Su <aoxue.1988.su.qian@163.com> 2010-12-17
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+/**
+ * 字符串格式化
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindStringFormat{
+	
+}
