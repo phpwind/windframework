d9774b0fa5b9d7021a0d75300dfcf390a9d98206
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-20 09:46:43 +0000

    代码重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@725 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/core/WindViewTest.php
+++ _tests/core/WindViewTest.php
@@ -1,20 +1,93 @@
 <?php
+require_once 'core/WindView.php';
 
-
-/**
- * test case.
- */
-class WindViewTest extends PHPUnit_Framework_TestCase {
+class WindViewTest extends BaseTestCase {
+	private $templateConfig = 'default';
 	
-	/**
-	 * Prepares the environment before running a test.
-	 */
+	function testCreateViewerResolver() {
+		$windView = $this->createWindView();
+		$viewResolver = $windView->createViewerResolver();
+		$this->assertEquals('WindViewer', get_class($viewResolver), 'test create viewer resolver failed.');
+	}
+	
+	public function testInitViewWithForward() {
+		$windView = $this->createWindView();
+		$this->assertEquals($windView->templateDefault, 'index');
+		$this->assertEquals($windView->initViewWithForward($this->createWindForward())->templateDefault, 'testIndex');
+	}
+	
+	public function testDoAction() {
+		
+	}
+	
+	public function testCreateWindView() {
+		$this->templateConfig = 'default';
+		C::init(WindViewData::getData1());
+		$windView = $this->createWindView();
+		$this->assertEquals($windView->templateDir, 'template');
+		$this->assertEquals($windView->templateExt, 'htm');
+		$this->assertEquals($windView->templateDefault, 'index');
+		$this->assertEquals($windView->templateCacheDir, 'cache');
+		$this->assertEquals($windView->templateCompileDir, 'compile');
+	}
+	
+	public function testCreateWindView1() {
+		$this->templateConfig = '';
+		C::init(WindViewData::getData1());
+		try {
+			$windView = $this->createWindView();
+		} catch (Exception $exception) {
+			$this->assertEquals('WindException', get_class($exception));
+		}
+	}
+	
+	private function createWindForward() {
+		$forward = new WindForward();
+		$forward->setTemplateName('testIndex');
+		return $forward;
+	}
+	
+	private function createWindView() {
+		return new WindView($this->templateConfig);
+	}
+	
 	protected function setUp() {
 		parent::setUp();
-		
-	// TODO Auto-generated WindViewTest::setUp()
+	}
 	
+	protected function tearDown() {
+		parent::tearDown();
+	}
+}
 
+class WindViewData {
+	static public function getData2() {
+		return array(
+			'templates' => array(), 
+			'viewerResolvers' => array(
+				'default' => 'WIND:core.viewer.WindViewer'));
+	}
+	static public function getData1() {
+		return array(
+			'templates' => array(
+				'default' => array(
+					'dir' => 'template', 
+					'default' => 'index', 
+					'ext' => 'htm', 
+					'resolver' => 'default', 
+					'isCache' => '0', 
+					'cacheDir' => 'cache', 
+					'compileDir' => 'compile'), 
+				'wind' => array(
+					'dir' => 'template', 
+					'default' => 'index', 
+					'ext' => 'htm', 
+					'resolver' => 'default', 
+					'isCache' => '0', 
+					'cacheDir' => 'cache', 
+					'compileDir' => 'compile')), 
+			'viewerResolvers' => array(
+				'default' => 'WIND:core.viewer.WindViewer'));
 	}
 }
 
