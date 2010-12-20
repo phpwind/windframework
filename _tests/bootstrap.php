ad7f13f787ef2c2ae316eb2452ac8bf921614f30
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-20 05:47:42 +0000

    代码重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@705 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/bootstrap.php
+++ _tests/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
-require_once('BaseTestCase.php');
-
-
-
+$path = '/../wind/';
+$_includePaths = array_unique(explode(PATH_SEPARATOR, get_include_path()));
+if (($pos = array_search('.', $_includePaths, true)) !== false) unset($_includePaths[$pos]);
+array_unshift($_includePaths, $path);
+if (set_include_path('.' . PATH_SEPARATOR . implode(PATH_SEPARATOR, $_includePaths)) === false) {
+	throw new Exception('set include path error.');
+}
+require_once ('BaseTestCase.php');
 ?>
