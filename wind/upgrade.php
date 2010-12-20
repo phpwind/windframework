9ee3a104da61dccafd1df350ece54e9c7949848b
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-20 10:58:26 +0000

    日期格式化工具
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@729 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/upgrade.php
+++ wind/upgrade.php
@@ -8,21 +8,21 @@
 define('D_S', DIRECTORY_SEPARATOR);
 define('WIND_PATH', dirname(__FILE__) . D_S);
 define('COMPILE_PATH', WIND_PATH . 'compile' . D_S);
-define('VERSION','1.0.5');
+define('VERSION', '1.0.5');
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qian Su <aoxue.1988.su.qian@163.com>
  * @version $Id$ 
  * @package 
  */
-class upgrade{
-	static function upgradePreLoad(){
-		if(defined('COMPILE_PATH') && is_writable(COMPILE_PATH)){
-			$packfile = COMPILE_PATH.'preload_'.VERSION.'.php';
-			if(!is_file($packfile)){
-				require WIND_PATH.'utility'.D_S.'WindPack.php';
+class upgrade {
+	static function upgradePreLoad() {
+		if (defined('COMPILE_PATH') && is_writable(COMPILE_PATH)) {
+			$packfile = COMPILE_PATH . 'preload_' . VERSION . '.php';
+			if (!is_file($packfile)) {
+				require WIND_PATH . 'utility' . D_S . 'WindPack.php';
 				$pack = new WindPack();
-				$pack->pack(array('core'),$packfile);
+				$pack->pack(array('core'), $packfile);
 			}
 		}
 	}
