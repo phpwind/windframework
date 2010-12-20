fa37510566d0bfabe264b3316f69a8f1d5cea5df
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-20 07:04:26 +0000

    更新配置文件测试
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@708 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/config.php
+++ _tests/config.php
@@ -1,48 +1,59 @@
 <?php
  return array(
-	'app' => array(
-		'name' => 'FormDemo',
-		'configPath' => 'D:\\PHPAPP\\phpwindframework\\demos\\formapp\\compile\\config.php',
-		'rootPath' => 'D:\\PHPAPP\\phpwindframework\\demos\\formapp',
-	),
-	'modules' => array(
-		'default' => array(
-			'name' => 'default',
-			'path' => 'form',
-		),
-	),
-	'template' => array(
-		'path' => 'templates',
-		'name' => 'index',
-		'ext' => 'html',
-		'resolver' => 'default',
-		'isCache' => 'false',
-		'cacheDir' => 'cache',
-		'compileDir' => 'compile',
-	),
 	'applications' => array(
 		'web' => array(
-			'name' => 'default',
 			'class' => 'WIND:core.WindWebApplication',
 		),
 		'command' => array(
-			'name' => 'default',
 			'class' => 'WIND:core.WindCommandApplication',
 		),
 	),
-	'errorMessage' => array(
-		'errorAction' => 'WIND:core.WindErrorAction',
+	'modules' => array(
+		'default' => array(
+			'path' => 'actionControllers',
+			'template' => 'default',
+			'controllerSuffix' => 'controller',
+			'actionSuffix' => 'action',
+			'method' => 'run',
+		),
+		'other' => array(
+			'path' => 'otherControllers',
+			'template' => 'wind',
+			'controllerSuffix' => 'controller',
+			'actionSuffix' => 'action',
+			'method' => 'run',
+		),
+	),
+	'error' => array(
+		'default' => 'WIND:core.WindErrorAction',
 	),
 	'filters' => array(
 		'WindFormFilter' => array(
-			'filterName' => 'WindFormFilter',
-			'filterPath' => 'WIND:component.form.WindFormFilter',
+			'filterPath' => 'WIND:core.filter.WindFormFilter',
+		),
+	),
+	'templates' => array(
+		'default' => array(
+			'dir' => 'template',
+			'default' => 'index',
+			'ext' => 'htm',
+			'resolver' => 'default',
+			'isCache' => '0',
+			'cacheDir' => 'cache',
+			'compileDir' => 'compile',
+		),
+		'wind' => array(
+			'dir' => 'template',
+			'default' => 'index',
+			'ext' => 'htm',
+			'resolver' => 'default',
+			'isCache' => '0',
+			'cacheDir' => 'cache',
+			'compileDir' => 'compile',
 		),
 	),
 	'viewerResolvers' => array(
-		'default' => 'WIND:component.viewer.WindViewer',
-		'pw' => 'WIND:component.viewer.WindPWViewer',
-		'smarty' => 'libs.WindSmarty',
+		'default' => 'WIND:core.viewer.WindViewer',
 	),
 	'router' => array(
 		'parser' => 'url',
@@ -54,56 +65,9 @@
 				'c' => 'index',
 				'm' => 'default',
 			),
-			'path' => 'WIND:component.router.WindUrlBasedRouter',
+			'path' => 'WIND:core.router.WindUrlBasedRouter',
 		),
 	),
-	'database' => array(
-		'connections' => array(
-			'phpwind_8' => array(
-				'driver' => 'mysql',
-				'type' => 'master',
-				'host' => 'localhost',
-				'user' => 'xxx',
-				'password' => 'xxx',
-				'port' => '3306',
-				'name' => 'phpwindteam',
-			),
-			'phpwind_beta' => array(
-				'driver' => 'mysql',
-				'type' => 'slave',
-				'host' => 'localhost',
-				'user' => 'xxx',
-				'password' => 'xxx',
-				'port' => '3306',
-				'name' => 'phpwindteam',
-			),
-			'user' => array(
-				'driver' => 'mssql',
-				'type' => 'slave',
-				'host' => 'localhost',
-				'user' => 'sa',
-				'password' => '151@suqian',
-				'name' => 'phpwind',
-			),
-		),
-		'drivers' => array(
-			'mysql' => array(
-				'builder' => 'mysql',
-				'class' => 'WIND:component.db.drivers.mysql.WindMySql',
-			),
-			'mssql' => array(
-				'builder' => 'mssql',
-				'class' => 'WIND:component.db.drivers.mssql.WindMsSql',
-			),
-		),
-		'builders' => array(
-			'mysql' => array(
-				'class' => 'WIND:component.db.drivers.mysql.WindMySqlBuilder',
-			),
-			'mssql' => array(
-				'class' => 'WIND:component.db.drivers.mssql.WindMsSqlBuilder',
-			),
-		),
-	),
+	'rootPath' => 'D:\\PHPAPP\\phpwindframework/_tests/component/config',
 );
 ?>
