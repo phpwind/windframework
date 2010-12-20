8e1186b783b8f105f106865e94d1e4d3dc96886d
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-20 11:31:36 +0000

    代码重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@733 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/viewer/WindViewer.php
+++ wind/core/viewer/WindViewer.php
@@ -39,7 +39,7 @@ class WindViewer implements IWindViewer {
 		ob_start();
 		if (($segments = $this->parserLayout()) == null) {
 			$template = $this->getViewTemplate($template);
-			if ($template) include $template;
+			if (is_file($template)) @include $template;
 		} else {
 			foreach ($segments as $value) {
 				$template = $this->getViewTemplate($value);
