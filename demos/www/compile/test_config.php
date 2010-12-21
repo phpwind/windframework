2563b80ac36f8b8a8beb8a8c0046a079813ac3b0
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-21 02:50:22 +0000

    代码重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@742 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ demos/www/compile/test_config.php
@@ -0,0 +1,57 @@
+<?php
+ return array(
+	'rootPath' => 'E:/workspace_c/wind/demos',
+	'applications' => array(
+		'web' => array(
+			'class' => 'WIND:core.WindWebApplication',
+		),
+		'command' => array(
+			'class' => 'WIND:core.WindCommandApplication',
+		),
+	),
+	'modules' => array(
+		'default' => array(
+			'path' => 'helloworld.actionControllers',
+			'template' => 'front',
+			'controllerSuffix' => 'controller',
+			'actionSuffix' => 'action',
+			'method' => 'run',
+		),
+	),
+	'error' => array(
+		'default' => 'WIND:core.WindErrorAction',
+	),
+	'filters' => array(
+		'WindFormFilter' => array(
+			'filterPath' => 'WIND:core.filter.WindFormFilter',
+		),
+	),
+	'templates' => array(
+		'front' => array(
+			'dir' => 'front',
+			'default' => 'index',
+			'ext' => 'htm',
+			'resolver' => 'default',
+			'isCache' => 'false',
+			'cacheDir' => 'cache',
+			'compileDir' => 'compile',
+		),
+	),
+	'viewerResolvers' => array(
+		'default' => 'WIND:core.viewer.WindViewer',
+	),
+	'router' => array(
+		'parser' => 'url',
+	),
+	'routerParsers' => array(
+		'url' => array(
+			'rule' => array(
+				'a' => 'run',
+				'c' => 'index',
+				'm' => 'default',
+			),
+			'path' => 'WIND:core.router.WindUrlBasedRouter',
+		),
+	),
+);
+?>
---
+++ demos/www/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-11-6
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license
+ */
+$start = microtime(true);
+header("Content-type: text/html; charset=utf8");
+define('R_P', dirname(__FILE__) . '/');
+define('IS_DEBUG', true);
+
+/* 框架文件路径 */
+define('FREAMWORK_PATH', R_P . '/../../wind/');
+
+/* 缓存文件路径 */
+define('COMPILE_PATH', R_P . 'compile/');
+require_once (FREAMWORK_PATH . '/wind.php');
+
+W::application('test', R_P . 'config.xml')->run();
+echo microtime(true) - $start;
