54ed17c1299ac5a5ed50031590fd470a540e028d
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-22 13:19:10 +0000

    代码重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@817 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/helloworld/index.php
+++ demos/helloworld/index.php
@@ -8,7 +8,7 @@
 $start = microtime(true);
 header("Content-type: text/html; charset=utf8");
 define('R_P', dirname(__FILE__) . '/');
-define('IS_DEBUG', true);
+define('IS_DEBUG', false);
 
 /* 框架文件路径 */
 define('FREAMWORK_PATH', R_P . '/../../wind/');
@@ -18,9 +18,11 @@ require_once (FREAMWORK_PATH . '/wind.php');
 
 /*$_GET['formName'] = 'userForm';
 $_POST['username'] = 'asssss';*/
-W::application('test', '')->run();
+W::application('test')->run();
 //W::application('test', R_P . 'config.xml')->run();
 //W::application('test', R_P . 'config.php')->run();
 //W::application('test', R_P . 'config.ini')->run();
 //W::application('test', R_P . 'config.properties')->run();
 echo microtime(true) - $start;
+echo '<br>';
+echo memory_get_usage()/1024/1024;
