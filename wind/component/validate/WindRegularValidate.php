da19ab6a87d5ef051f56c4f624fb1ec098e52e94
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-22 10:34:38 +0000

    正则验证
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@803 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/validate/WindRegularValidate.php
+++ wind/component/validate/WindRegularValidate.php
@@ -23,7 +23,7 @@ class WindRegularValidate{
 	 * @return number
 	 */
 	public static function validateByRegExp($regExp,$string,&$matches=array(),$ifall = false){
-		if($ifAll){
+		if(true === $ifAll){
 			return preg_match_all($regExp,$string,$matches);
 		}
 		return preg_match($regExp,$string,$matches);
@@ -36,9 +36,15 @@ class WindRegularValidate{
 	 * @param boolean $ifAll   是否进行全局正则表达式匹配
 	 * @return number
 	 */
-	public static function validateEmail($string,&$matches=array(),$ifAll = false){
-		return $this->validateByRegExp("/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/",$string,$matches,$ifall);
+	public static function hasEmail($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/",$string,$matches,$ifall);
 	}
+	
+	public static function isEmail($string){
+		return 0 < preg_match("/^\w+(?:[-+.']\w+)*@\w+(?:[-.]\w+)*\.\w+(?:[-.]\w+)*$/",$string);
+	}
+	
+	
 	
 	/**
 	 * 验证是否是合法的身份证号
@@ -47,8 +53,12 @@ class WindRegularValidate{
 	 * @param boolean $ifAll   是否进行全局正则表达式匹配
 	 * @return number
 	 */
-	public static function validateIdCard($string,&$matches=array(),$ifAll = false){
-		return $this->validateByRegExp("/\d{17}[\d|X]|\d{15}/",$string,$matches,$ifall);
+	public static function hasIdCard($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/\d{17}[\d|X]|\d{15}/",$string,$matches,$ifall);
+	}
+	
+	public static function isIdCard($string){
+		return 0 < preg_match("/^(?:\d{17}[\d|X]|\d{15})$/",$string);
 	}
 	
 	/**
@@ -58,8 +68,12 @@ class WindRegularValidate{
 	 * @param boolean $ifAll   是否进行全局正则表达式匹配
 	 * @return number
 	 */
-	public static function validateUrl($string,&$matches = array(),$ifAll = false){
-		return $this->validateByRegExp("/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/",$string,$matches,$ifall);
+	public static function hasUrl($string,&$matches = array(),$ifAll = false){
+		return self::validateByRegExp("/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/",$string,$matches,$ifall);
+	}
+	
+	public static function isUrl($string){
+		return 0 < preg_match("/^(?:http(?:s)?:\/\/(?:[\w-]+\.)+[\w-]+(?:\:\d+)*+(?:\/[\w- .\/?%&=]*)?)$/",$string);
 	}
 	
 	/**
@@ -69,8 +83,12 @@ class WindRegularValidate{
 	 * @param boolean $ifAll   是否进行全局正则表达式匹配
 	 * @return number
 	 */
-	public static function validateChinese($string,&$matches=array(),$ifAll = false){
-		return $this->validateByRegExp("/^[\u4e00-\u9fa5]+/",$string,$matches,$ifall);
+	public static function hasChinese($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/[\x{4e00}-\x{9fa5}]+/u",$string,$matches,$ifall);
+	}
+	
+	public static function isChinese($string){
+		return 0 < preg_match("/^[\x{4e00}-\x{9fa5}]+$/u",$string);
 	}
 	
 	/**
@@ -80,8 +98,8 @@ class WindRegularValidate{
 	 * @param boolean $ifAll   是否进行全局正则表达式匹配
 	 * @return number
 	 */
-	public static function validateHtml($string,&$matches=array(),$ifAll = false){
-		return $this->validateByRegExp("/<(.*)>.*|<(.*)\/>/",$string,$matches,$ifall);
+	public static function hasHtml($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/<(.*)>.*|<(.*)\/>/",$string,$matches,$ifall);
 	}
 	
 	
@@ -92,7 +110,78 @@ class WindRegularValidate{
 	 * @param boolean $ifAll   是否进行全局正则表达式匹配
 	 * @return number
 	 */
-	public static function validateDoubleType($string,&$matches=array(),$ifAll = false){
-		return $this->validateByRegExp("/[^\x00-\xff]+/",$string,$matches,$ifall);
+	public static function hasDoubleType($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/[^\x00-\xff]+/",$string,$matches,$ifall);
+	}
+	
+	/**
+	 * 验证是否是合法的电话号码
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public static function hasTel($string,&$matches=array(),$ifAll = false){
+		$regExp = "/(^(0\d{2})?-?(\d{7,8})$)|(^(0\d{3})?-?(\d{7,8})$)|(^(0\d{2})?-?(\d{8})-(\d+)$)|(^(0\d{3})?-?(\d{7})-(\d+)$)/";
+		return self::validateByRegExp($regExp,$string,$matches,$ifall);
+	}
+	
+	/**
+	 * 验证是否是非负整数
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public static function isNonNegative($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/^\d+$/",$string,$matches,$ifall);
+	}
+	
+	/**
+	 * 验证是否是正数
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public static function isPositive ($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/^[1-9][0-9]*$/",$string,$matches,$ifall);
+	}
+	
+	/**
+	 * 验证是否是负数
+	 * @param string $string   被搜索的 字符串
+	 * @param array $matches   会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public static function isNegative($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/^-[1-9][0-9]*$/",$string,$matches,$ifall);
+	}
+	
+	/**
+	 * 验证是否是合法的ipv4地址
+	 * @param string $string   被搜索的 字符串
+	 * @param array $matches   会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public static function hasIP($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/(\d+)\.(\d+)\.(\d+)\.(\d+)/",$string,$matches,$ifall);
+	}
+	
+	public static function isIP($string){
+		return 0 < preg_match('/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/',$string); 
+	}
+	
+	/**
+	 * 验证是否是客户端脚本
+	 * @param string $string   被搜索的 字符串
+	 * @param array $matches   会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public static function hasScript($string,&$matches=array(),$ifAll = false){
+		return self::validateByRegExp("/<script(.*?)>([^\x00]*?)<\/script>/i",$string,$matches,$ifall);
 	}
 }
