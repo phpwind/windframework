0a5e9fa6f1da74646580153500767b83d060f64c
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-28 10:50:49 +0000

    http 协议
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@907 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/mail/protocol/WindImap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+
+class WindImap {
+
+
+}
