930b55851577fec5a3677670a7865775c343f9eb
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2010-12-28 10:49:31 +0000

    smtp邮件协议
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@904 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/mail/protocol/WindSocket.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+
+/**
+ * socket套接字操作
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindSocket {
+	
+	protected $host = '127.0.0.1';
+	protected $port = 80;
+	protected $timeout = 5;
+	protected $errno = 0;
+	protected $errstr = '';
+	protected $socket = null;
+	
+	public function __construct($host = '127.0.0.1', $port = 80, $timeout = 5) {
+		$this->setHost($host);
+		$this->setPort($port);
+		$this->setTimeout($timeout);
+	}
+	
+	/**
+	 * 打开一个连接
+	 */
+	public function open() {
+		if (null == $this->socket) {
+			$this->socket = fsockopen($this->host, $this->port, $this->errno, $this->errstr, $this->timeout);
+		}
+	}
+	
+	/**
+	 * 发送请求
+	 * @param string $request
+	 */
+	public function request($request) {
+		return fputs($this->socket, $request);
+	}
+	
+	/**
+	 * 响应请求
+	 * @return string
+	 */
+	public function response() {
+		$response = '';
+		while (!feof($this->socket)) {
+			$response .= fgets($this->socket);
+		}
+		return $response;
+	}
+	
+	/**
+	 * 响应请求,只返回一行
+	 * @return string
+	 */
+	public function responseLine() {
+		return feof($this->socket) ? '' : fgets($this->socket);
+	}
+	
+	/**
+	 *关闭连接 
+	 */
+	public function close() {
+		if ($this->socket) {
+			fclose($this->socket);
+			$this->socket = null;
+		}
+	}
+	/**
+	 * 获取请求中的错误
+	 * @return string
+	 */
+	public function getError() {
+		return $this->errstr ? $this->errno . ':' . $this->errstr : '';
+	}
+	
+	/**
+	 * 设置主机
+	 * @param string $host
+	 */
+	public function setHost($host) {
+		$this->host = $host;
+	
+	}
+	/**
+	 * 设置端口
+	 * @param string $port
+	 */
+	public function setPort($port) {
+		$this->port = $port;
+	}
+	/**
+	 * 设置超时
+	 * @param int $timeout
+	 */
+	public function setTimeout($timeout) {
+		$this->timeout = $timeout;
+	}
+	
+	public function setSocketTimeOut($timeout) {
+		return stream_set_timeout($this->socket, $timeout);
+	}
+}
