e71b0e0631b66307ab01a445dc521c0bf0548dd0
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-01 10:05:05 +0000

    php mail
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@946 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/mail/send/WindPhpMail.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+
+/**
+ * 使用php内部函数发送邮件
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindPhpMail{
+	
+}
