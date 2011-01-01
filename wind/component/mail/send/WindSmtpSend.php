9f57b396ff6275ad15718e9edb0a0601631e8e28
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-01 09:29:03 +0000

    smtp邮件协议
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@943 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/mail/send/WindSmtpSend.php
+++ wind/component/mail/send/WindSmtpSend.php
@@ -8,12 +8,68 @@
  */
 
 class WindSmtpSend{
+	/**
+	 * @var WindSmtp 邮件发送服务器
+	 */
 	protected $smtp = null;
-	public function __construct($host,$port){
+	/**
+	 * @var boolean 是否启用验证
+	 */
+	protected $auth = true;
+	/**
+	 * @var string 邮件主机名
+	 */
+	protected $name = 'localhost';
+	/**
+	 * @var string 邮件用户名
+	 */
+	protected $username = '';
+	/**
+	 * @var string 邮件密码
+	 */
+	protected $password = '';
+	
+	public function __construct($host,$port,$name = 'localhost',$auth = true){
+		$this->auth = true;
+		$this->name = $name;
 		$this->smtp = new WindSmtp($host,$port);
+		$this->smtp->open();
 	}
 	
-	public function send(WindMail $mailBuilder){
-		
+	/**
+	 * 发送邮件
+	 * @param WindMail $mail 邮件消息封装对象
+	 */
+	public function send(WindMail $mail){
+		$this->smtp->ehlo($this->name);
+		if($this->auth){
+			$this->smtp->authLogin($this->username,$this->password);
+		}
+		$this->smtp->mailFrom($mail->getFrom());
+		foreach($mail->getRecipients() as $rcpt){
+			$this->smtp->rcptTo($rcpt);
+		}
+		$header = $mail->createHeader();
+		$body = $mail->createBody();
+		$data = $header.$body;
+		$this->smtp->data($header.$body);
+		$this->smtp->quit();
+	}
+	
+	/**
+	 * 设置验证参数
+	 * @param string $username 用户名
+	 * @param string $password 密码
+	 */
+	public function setAuthParams($username,$password){
+		$this->username = $username;
+		$this->password = $password;
+	}
+	
+
+	
+	public function __destruct(){
+		$this->smtp->close();
+		$this->smtp = null;
 	}
 }
