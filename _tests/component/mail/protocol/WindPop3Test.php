845edcc5dfa05c8e5d72d771fe9d64b83f4c34aa
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-05 04:50:16 +0000

    mail WindSmtp 单元测试
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@962 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ _tests/component/mail/protocol/WindPop3Test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
