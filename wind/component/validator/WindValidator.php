cfa2a9d83bcd17747af7c34f84d3c09fd37a3824
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-07 08:08:31 +0000

    修改名字
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@984 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/validator/WindValidator.php
+++ wind/component/validator/WindValidator.php
@@ -12,8 +12,8 @@
  * @version $Id$ 
  * @package 
  */
-class WindValidate {
-	
+class WindValidator {
+
 	/**
 	 * 在 $string 字符串中搜索与 $regExp 给出的正则表达式相匹配的内容。
 	 * @param string $regExp  搜索的规则(正则)
@@ -28,7 +28,7 @@ class WindValidate {
 		}
 		return preg_match($regExp, $string, $matches);
 	}
-	
+
 	/**
 	 * 验证是否是有合法的email
 	 * @param string $string  被搜索的 字符串
@@ -39,7 +39,7 @@ class WindValidate {
 	public static function hasEmail($string, &$matches = array(), $ifAll = false) {
 		return self::validateByRegExp("/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/", $string, $matches, $ifAll);
 	}
-	
+
 	/**
 	 * 验证是否是合法的email
 	 * @param string $string
@@ -48,7 +48,7 @@ class WindValidate {
 	public static function isEmail($string) {
 		return 0 < preg_match("/^\w+(?:[-+.']\w+)*@\w+(?:[-.]\w+)*\.\w+(?:[-.]\w+)*$/", $string);
 	}
-	
+
 	/**
 	 * 验证是否有合法的身份证号
 	 * @param string $string  被搜索的 字符串
@@ -59,7 +59,7 @@ class WindValidate {
 	public static function hasIdCard($string, &$matches = array(), $ifAll = false) {
 		return self::validateByRegExp("/\d{17}[\d|X]|\d{15}/", $string, $matches, $ifAll);
 	}
-	
+
 	/**
 	 * 验证是否是合法的身份证号
 	 * @param string $string
@@ -68,7 +68,7 @@ class WindValidate {
 	public static function isIdCard($string) {
 		return 0 < preg_match("/^(?:\d{17}[\d|X]|\d{15})$/", $string);
 	}
-	
+
 	/**
 	 * 验证是否有合法的URL
 	 * @param string $string  被搜索的 字符串
@@ -79,6 +79,7 @@ class WindValidate {
 	public static function hasUrl($string, &$matches = array(), $ifAll = false) {
 		return self::validateByRegExp('/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/', $string, $matches, $ifAll);
 	}
+
 	/**
 	 * 验证是否是合法的url
 	 * @param string $string
@@ -87,7 +88,7 @@ class WindValidate {
 	public static function isUrl($string) {
 		return 0 < preg_match('/^(?:http(?:s)?:\/\/(?:[\w-]+\.)+[\w-]+(?:\:\d+)*+(?:\/[\w- .\/?%&=]*)?)$/', $string);
 	}
-	
+
 	/**
 	 * 验证是否有中文
 	 * @param string $string  被搜索的 字符串
@@ -98,7 +99,7 @@ class WindValidate {
 	public static function hasChinese($string, &$matches = array(), $ifAll = false) {
 		return self::validateByRegExp('/[\x{4e00}-\x{9fa5}]+/u', $string, $matches, $ifAll);
 	}
-	
+
 	/**
 	 * 验证是否是中文
 	 * @param string $string
@@ -107,7 +108,7 @@ class WindValidate {
 	public static function isChinese($string) {
 		return 0 < preg_match('/^[\x{4e00}-\x{9fa5}]+$/u', $string);
 	}
-	
+
 	/**
 	 * 验证是否有html标记
 	 * @param string $string  被搜索的 字符串
@@ -118,6 +119,7 @@ class WindValidate {
 	public static function hasHTML($string, &$matches = array(), $ifAll = false) {
 		return self::validateByRegExp('/<(.*)>.*|<(.*)\/>/', $string, $matches, $ifAll);
 	}
+
 	/**
 	 * 验证是否是合法的html标记
 	 * @param string $string
@@ -126,7 +128,7 @@ class WindValidate {
 	public function isHTML($string) {
 		return 0 < preg_match('/^<(.*)>.*|<(.*)\/>$/', $string);
 	}
-	
+
 	/**
 	 * 验证是否是非负整数
 	 * @param string $string  被搜索的 字符串
@@ -135,16 +137,16 @@ class WindValidate {
 	public static function isNonNegative($string) {
 		return 0 < preg_match("/^\d+$/", $string);
 	}
-	
+
 	/**
 	 * 验证是否是正数
 	 * @param string $string  被搜索的 字符串
 	 * @return number
 	 */
 	public static function isPositive($string) {
-		return 0 < preg_match('/^[1-9][0-9]*$/',$string);
+		return 0 < preg_match('/^[1-9][0-9]*$/', $string);
 	}
-	
+
 	/**
 	 * 验证是否是负数
 	 * @param string $string   被搜索的 字符串
@@ -153,7 +155,7 @@ class WindValidate {
 	public static function isNegative($string) {
 		return 0 < preg_match('/^-[1-9][0-9]*$/', $string);
 	}
-	
+
 	/**
 	 * 验证是否有合法的ipv4地址
 	 * @param string $string   被搜索的 字符串
@@ -164,7 +166,7 @@ class WindValidate {
 	public static function hasIP($string, &$matches = array(), $ifAll = false) {
 		return self::validateByRegExp('/(\d+)\.(\d+)\.(\d+)\.(\d+)/', $string, $matches, $ifAll);
 	}
-	
+
 	/**
 	 * 验证是否是合法的IP
 	 * @param string $string
@@ -173,7 +175,7 @@ class WindValidate {
 	public static function isIP($string) {
 		return 0 < preg_match('/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/', $string);
 	}
-	
+
 	/**
 	 * 验证是否有客户端脚本
 	 * @param string $string   被搜索的 字符串
@@ -184,6 +186,7 @@ class WindValidate {
 	public static function hasScript($string, &$matches = array(), $ifAll = false) {
 		return self::validateByRegExp('/<script(.*?)>([^\x00]*?)<\/script>/', $string, $matches, $ifAll);
 	}
+
 	/**
 	 * 验证是否是合法的客户端脚本
 	 * @param string $string
@@ -192,27 +195,27 @@ class WindValidate {
 	public static function isScript($string) {
 		return 0 < preg_match('/<script(?:.*?)>(?:[^\x00]*?)<\/script>/', $string);
 	}
-	
-	public static function isInt($value){
+
+	public static function isInt($value) {
 		return is_int($value);
 	}
-	
-	public static function isFloat($value){
+
+	public static function isFloat($value) {
 		return is_float($value);
 	}
-	
-	public static function isArray($value){
+
+	public static function isArray($value) {
 		return is_array($value);
 	}
-	
-	public static function isBool($value){
+
+	public static function isBool($value) {
 		return is_bool($value);
 	}
-	
-	public static function isEmpty($value){
+
+	public static function isEmpty($value) {
 		return empty($value);
 	}
-	
+
 	/**
 	 * 验证字符串的长度
 	 * @param string $string 要验证的字符串
@@ -220,9 +223,9 @@ class WindValidate {
 	 * @param string $charset 字符编码
 	 * @return boolean
 	 */
-	public static function isLegalLength($string,$length,$charset='utf8'){
-		L::import ( 'WIND:component.format.WindString' );
-		return WindString::strlen($string,$charset) > (int)$length;
+	public static function isLegalLength($string, $length, $charset = 'utf8') {
+		L::import('WIND:component.format.WindString');
+		return WindString::strlen($string, $charset) > (int) $length;
 	}
-	
+
 }
