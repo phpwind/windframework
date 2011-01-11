acfc149b5c86574926310bb6824f997f6b6313a7
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-11 08:42:18 +0000

    WindLog单元测试
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@999 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ _tests/component/log/WindLogTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+define ('LOG_PATH', COMPILE_PATH.'/log/' );
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindLogTest extends BaseTestCase {
+	
+	public function init() {
+		require_once ('component/Log/WindLog.php');
+	}
+	
+	public function setUp() {
+		parent::setUp();
+		$this->init();
+	}
+	
+	public function tearDown() {
+		parent::tearDown();
+	}
+	
+	public function testFlushWithLog(){
+		WindLog::add("test log",WindLog::TRACE);
+		WindLog::add("test log again",WindLog::INFO);
+		$this->assertTrue(WindLog::flush());
+	}
+	
+	public function testFlushWithHtml(){
+		WindLog::setLogDisplay(WindLog::HTML);
+		WindLog::add("test log",WindLog::TRACE);
+		WindLog::add("test log again",WindLog::INFO);
+		$this->assertTrue(WindLog::flush());
+	}
+	
+	public function testLog(){
+		WindLog::log("test record log by log",WindLog::ERROR);
+	}
+	
+	public function testClearFiles(){
+		$this->assertTrue(WindLog::clearFiles());
+	}
+}
