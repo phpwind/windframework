0773f1a4dbb872a743c7f09d65fe8877726745f0
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-12 09:55:33 +0000

    框架缓存部署
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1012 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/cache/stored/WindFileCache.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+
+/**
+ * 文件缓存类
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindFileCache implements IWindCache{
+	
+	protected static $tags;
+	
+	public function set($key,$value){
+		
+	}
+	
+	public function add($key,$value){
+		
+	}
+	
+	public function replace($key,$value){
+		
+	}
+	
+	public function fetch($key){
+		
+	}
+	
+	public function fetchByTags($tags){
+		
+	}
+	
+	public function batchFetch($keys){
+		
+	}
+	
+	public function delete($key){
+		
+	}
+	
+	public function deteteByTags($tags){
+		
+	}
+	
+	public function batchDelete($keys){
+		
+	}
+	
+	public function flush(){
+		
+	}
+}
---
+++ wind/component/cache/view/WindPageCache.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+
+/**
+ * 页面缓存
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindPageCache{
+	
+}
