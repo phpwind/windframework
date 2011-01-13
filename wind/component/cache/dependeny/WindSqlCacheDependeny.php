3fb3743f653e4b5624d3f80a7271d6f2e6dacca7
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-13 07:20:19 +0000

    sql缓存依赖
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1016 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/cache/dependeny/WindSqlCacheDependeny.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+
+/**
+ * 监视特定的数据库表，以便在该表发生更改时，自动从 Cache 中删除与该表关联的项。数据库表发生更改时，将自动删除缓存项
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindSqlCacheDependeny extends WindCacheDependeny{
+	
+}
