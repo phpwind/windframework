c4ab83cdc0df95fdaf7da7bcb2eddc49a86ecf3c
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-13 10:04:27 +0000

    增加类代理对象接口
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1027 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/factory/IWindFactory.php
+++ wind/core/factory/IWindFactory.php
@@ -16,12 +16,24 @@
 interface IWindFactory {
 
 	/**
-	 * 根据类的别名获得一个类实例变量
+	 * 描述：根据类的别名获得一个类实例变量
+	 * 返回：类的实例对象
+	 * 
+	 * 该方法首先通过类的别名到<b>类的配置文件中</b>找到类的相关配置信息，
+	 * 加载类的路径并创建类的依赖
 	 * 
 	 * @param string $classAlias
 	 */
 	public function getInstance($classAlias);
 
+	/**
+	 * 根据类名称创建类对象
+	 * 返回一个类类型的实例对象，通过此方法创建类实例，并不能自动获取类路径信息
+	 * 
+	 * @param string $className | 类名称
+	 * @param array $args	| 类参数信息
+	 * @return Object | 返回的类类型的实例对象
+	 */
 	public function createInstance($className, $args);
 
 }
