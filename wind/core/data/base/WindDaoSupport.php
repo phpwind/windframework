cef283b85c5c65389d7834d45de0daa07e62c7ec
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-13 04:52:01 +0000

    数据库中间层解决方案
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1015 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/data/base/WindDaoSupport.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ * tags
+ */
+
+/**
+ * 以标准的方式使用不同的数据访问技术,方便不同数据库持久化技术间切换及各种技术中特定的异常
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+abstract class WindDaoSupport{
+	public function __construct(){
+		$this->init();
+	}
+	public abstract function init();
+	public abstract function getTemplate();
+	
+}
