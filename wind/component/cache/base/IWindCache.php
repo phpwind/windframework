404182d211e34be6f83c0b9a90057884df13ca2e
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-17 08:47:01 +0000

    数据库中间层解决方案
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1066 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/cache/base/IWindCache.php
+++ wind/component/cache/base/IWindCache.php
@@ -22,44 +22,34 @@ interface IWindCache{
 	 * @param string $value 缓存数据。
 	 * @param string $expires 缓存数据的过期时间
 	 * @param IWindCacheDependency $denpendency 缓存依赖
-	 * @param array $tags 缓存数据的标签
 	 * @return boolean
 	 */
-	public function add($key,$value,$expires = null,IWindCacheDependency $denpendency = null,$tags = array());
+	public function add($key,$value,$expires = 0,IWindCacheDependency $denpendency = null);
 	/**
 	 * 设置缓存，如果key不存在，设置缓存，否则，替换已有key的缓存。
 	 * @param string $key 保存缓存数据的键。
 	 * @param string $value 保存缓存数据。
 	 * @param int $expires 缓存数据的过期时间
 	 * @param IWindCacheDependency $denpendency 缓存依赖
-	 * @param array $tags 缓存数据的标签
 	 * @return boolean
 	 */
-	public function set($key,$value,$expires = null,IWindCacheDependency $denpendency = null,$tags = array());
+	public function set($key,$value,$expires = 0,IWindCacheDependency $denpendency = null);
 	/**
 	 * 替换缓存，如果key不存在，抛出异常，否则，替换缓存。
 	 * @param string $key 保存缓存数据的键。
 	 * @param string $value 缓存数据数据。
 	 * @param int $expires 缓存数据的过期时间
  	 * @param IWindCacheDependency $denpendency 缓存依赖 
-	 * @param array $tags 缓存数据的标签
 	 * @return boolean
 	 */
-	public function replace($key,$value,$expires = null,IWindCacheDependency $denpendency = null,$tags = array());
+	public function replace($key,$value,$expires = 0,IWindCacheDependency $denpendency = null);
 	/**
 	 * 获取指定缓存
 	 * @param string $key 获取缓存数据的标识，即键
-	 * @param array $tags  缓存数据的标签
 	 * @return string
 	 */
-	public function fetch($key,$tags = array());
+	public function fetch($key);
 	
-	/**
-	 * 通过标记获取缓存数据
-	 * @param array $tags
-	 * @return array
-	 */
-	public function fetchByTags($tags);
 	/**
 	 * 通过key批量获取缓存数据
 	 * @param array $keys
@@ -69,16 +59,9 @@ interface IWindCache{
 	/**
 	 * 删除缓存数据
 	 * @param string $key 获取缓存数据的标识，即键
-	 * @param array $tags  缓存数据的标签
 	 * @return string
 	 */
-	public function delete($key,$tags = array());
-	/**
-	 * 通过标记获删除存数据
-	 * @param array $tags  缓存数据的标签
-	 * @return string
-	 */
-	public function deteteByTags($tags);
+	public function delete($key);
 	/**
 	 * 通过key批量删除缓存数据
 	 * @param array $keys
@@ -89,4 +72,5 @@ interface IWindCache{
 	 * 清空所有缓存
 	 */
 	public function flush();
+
 }
--- wind/component/cache/stored/WindMemcache.php
+++ wind/component/cache/stored/WindMemcache.php
@@ -13,40 +13,214 @@
  * @version $Id$ 
  * @package 
  */
-class WindMemcache implements IWindCache{
+final class WindMemcache implements IWindCache {
 	
-	public function add($key, $value, $expires = null, IWindCacheDependency $denpendency = null, $tags = array()) {
-		
+	/**
+	 * @var string key的安全码
+	 */
+	private $securityCode = '';
+	/**
+	 * @var array memcache服务器配置
+	 */
+	private $servers = array();
+	/**
+	 * @var Memcache memcache缓存操作句柄
+	 */
+	private $cache = null;
+	/**
+	 * @var int 是否对缓存采取压缩存储
+	 */
+	private $compress = 0;
+	//配置信息
+	const HOST = 'host';
+	const PORT = 'port';
+	const PCONN = 'pconn';
+	const WEIGHT = 'weight';
+	const TIMEOUT = 'timeout';
+	const RETRY = 'retry';
+	const STATUS = 'status';
+	const FCALLBACK = 'fcallback';
+	const SECURITY = 'security';
+	const COMPRESS = 'compress';
+	//存储数据
+	const DEPENDENCY = 'dependency';
+	const EXPIRES = 'EXPIRES';
+	const STORETIME = 'store';
+	const DATA = 'data';
+	
+	/**
+	 * 
+	 * @example array(
+	 * 		array(
+	 * 			'host'=>'localhost',
+	 * 			'port'=>11211
+	 * 			'pconn'=>true
+	 *      ),
+	 *      array(
+	 *      	'host'=>'localhost',
+	 * 			'port'=>11212
+	 * 			'pconn'=>false
+	 *      ),
+	 *      'compress'=>true,
+	 *      'security'=>'1x2aao@'
+	 * 
+	 * )
+	 * @param array $servers memcache 服务器配置
+	 */
+	public function __construct(array $servers = array()) {
+		$this->cache = new Memcache();
+		$hasServer = false;
+		foreach ($servers as $server) {
+			if(is_array($server)){
+				$hasServer = true;
+				$this->addServer($server);
+			}
+		}
+		if(false === $hasServer){
+			$this->addServer();
+		}
+		if (isset($servers[self::COMPRESS])) {
+			$this->compress = MEMCACHE_COMPRESSED;
+		}else{
+			$this->compress = 0;
+		}
+		if(isset($servers[self::SECURITY])){
+			$this->securityCode = $servers[self::SECURITY];
+		}
 	}
-	public function set($key, $value, $expires = null, IWindCacheDependency $denpendency = null, $tags = array()) {
-		
+	
+	/**
+	 * 添加memached服务器
+	 * @param array $server
+	 */
+	public function addServer(array $server = array()) {
+		if (!isset($server[self::HOST])) {
+			$server[self::HOST] = '127.0.0.1';
+		}
+		if (!isset($server[self::PORT])) {
+			$server[self::PORT] = 11211;
+		}
+		if (!isset($server[self::PCONN])) {
+			$server[self::PCONN] = true;
+		}
+		if (!isset($server[self::WEIGHT])) {
+			$server[self::WEIGHT] = 1;
+		}
+		if (!isset($server[self::TIMEOUT])) {
+			$server[self::TIMEOUT] = 15;
+		}
+		if (!isset($server[self::RETRY])) {
+			$server[self::RETRY] = 15;
+		}
+		if (!isset($server[self::STATUS])) {
+			$server[self::STATUS] = true;
+		}
+		if (!isset($server[self::FCALLBACK])) {
+			$server[self::FCALLBACK] = null;
+		}
+		list($host, $port, $pconn, $weight, $timeout, $retry, $status, $fcallback) = array_values($server);
+		$this->cache->addServer($host, $port, $pconn, $weight, $timeout, $retry, $status, $fcallback);
+		array_push($this->servers, $server);
 	}
-	public function replace($key, $value, $expires = null, IWindCacheDependency $denpendency = null, $tags = array()) {
-		
+	/* 
+	 * @see wind/component/cache/base/IWindCache#add()
+	 */
+	public function add($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$key = $this->buildSecurityKey($key);
+		$data = $this->storeData($key, $value, $expires, $denpendency);
+		return $this->cache->add($key, $data, $this->compress, (int) $expires);
 	}
-	public function fetch($key, $tags = array()) {
-		
+	/* 
+	 * @see wind/component/cache/base/IWindCache#set()
+	 */
+	public function set($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$key = $this->buildSecurityKey($key);
+		$data = $this->storeData($key, $value, $expires, $denpendency);
+		return $this->cache->set($key, $data, $this->compress, (int)$expires);
 	}
-	public function fetchByTags($tags) {
-		
+	/* 
+	 * @see wind/component/cache/base/IWindCache#replace()
+	 */
+	public function replace($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$key = $this->buildSecurityKey($key);
+		$data = $this->storeData($key, $value, $expires, $denpendency);
+		return $this->cache->replace($key, $data, $this->compress,(int)$expires);
 	}
-	public function batchFetch($keys) {
-		
+	/* 
+	 * @see wind/component/cache/base/IWindCache#fetch()
+	 */
+	public function fetch($key) {
+		$key = $this->buildSecurityKey($key);
+		$data = $this->cache->get($key,$this->compress);
+		if(!is_array($data)){
+			return $data;
+		}
+		if(isset($data[self::DEPENDENCY]) && $data[self::DEPENDENCY] instanceof IWindCacheDependency){
+			if($data[self::DEPENDENCY]->hasChanged()){
+				$this->cache->delete($key);
+				return '';
+			}
+		}
+		return isset($data[self::DATA]) ? $data[self::DATA] : '';
 	}
-	public function delete($key, $tags = array()) {
-		
+	
+	/*
+	 * @see wind/component/cache/base/IWindCache#batchFetch()
+	 */
+	public function batchFetch(array $keys) {
+		$data = array();
+		foreach($keys as $key){
+			if('' != ($value = $this->fetch($key))){
+				$data[$key] = $value;
+			}
+		}
+		return $data;
 	}
-
-	public function deteteByTags($tags) {
-		
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#delete()
+	 */
+	public function delete($key) {
+		return $this->cache->delete($this->buildSecurityKey($key));
 	}
-
-	public function batchDelete($keys) {
-		
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#batchDelete()
+	 */
+	public function batchDelete(array $keys) {
+		foreach($keys as $key){
+			$this->delete($key);
+		}
+		return true;
 	}
-
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#flush()
+	 */
 	public function flush() {
-		
+		return $this->cache->flush();
 	}
 	
+	/**
+	 * 生成安全的key
+	 * @param string $key
+	 * @return string
+	 */
+	private function buildSecurityKey($key){
+		return substr(sha1(md5($key).$this->securityCode),0,5);
+	}
+	
+	/* 
+	 * 获取存储的数据
+	 * @see wind/component/cache/stored/IWindCache#set()
+	 * @return array
+	 */
+	private function storeData($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$data = array(self::DATA=>$value, self::EXPIRES=> $expires,self::STORETIME=>time());
+		if ($denpendency && (($denpendency instanceof IWindCacheDependency))) {
+			$denpendency->injectDependent();
+			$data[self::DEPENDENCY] = $denpendency;
+		}
+		return $data;
+	}
 }
