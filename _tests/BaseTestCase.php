c07e9836ad6613b20ca8c750e0c9da27f2d59ab0
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-17 05:02:55 +0000

    NEW - bug 80: WindClassDefinition.php http://bugs.phpwind-inc.com/show_bug.cgi?id=80
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1059 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/BaseTestCase.php
+++ _tests/BaseTestCase.php
@@ -16,4 +16,19 @@ abstract class BaseTestCase extends PHPUnit_Framework_TestCase {
 		parent::tearDown();
 		spl_autoload_unregister('L::autoLoad');
 	}
+	
+	protected function assertArrayEquals($array1, $array2) {
+		(!is_array($array1)) && $this->fail("Error type for arg1");
+		$this->assertTrue(is_array($array2) && (count($array1) == count($array2)));
+		foreach ($array1 as $key => $value) {
+			$this->assertTrue(isset($array2[$key]));
+			if (is_array($value)) {
+				$this->assertArrayEquals($value, $array2[$key]);
+			} elseif (is_object($value)) {
+				$this->assertEquals(get_class($value), get_class($array2[$key]));
+			} else {
+				$this->assertEquals($value, $array2[$key]);
+			}
+		}
+	}
 }
