5d861f522be1adfb0d119c1ce0b28ebe79f5b1a9
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-24 03:53:17 +0000

    apc加速缓存和eaccelerator加速缓存实现
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1099 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/cache/stored/WindEacceleratorCache.php
+++ wind/component/cache/stored/WindEacceleratorCache.php
@@ -8,11 +8,128 @@
  */
 
 /**
+ * Eaccelerator是一款php加速器、优化器、编码器及动态内容缓存。
+ * WindEaccelerator实现Eaccelerator动态内容缓存功能。
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qian Su <aoxue.1988.su.qian@163.com>
  * @version $Id$ 
  * @package 
  */
-class WindEaccelerator{
+class WindEaccelerator implements IWindCache{
 	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#add()
+	 */
+	public function add($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$cacheData = $this->fetch($key);
+		if(false === empty($cacheData)){
+			$this->error("The cache already exists");
+		}
+		return eaccelerator_put($key,$this->storeData($value,$expires,$denpendency),$expires);
+	}
+
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#set()
+	 */
+	public function set($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		return eaccelerator_put($key,$this->storeData($value,$expires,$denpendency),$expires);
+	}
+
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#replace()
+	 */
+	public function replace($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$cacheData = $this->fetch($key);
+		if(empty($cacheData)){
+			$this->error("The cache does not exist");
+		}
+		return eaccelerator_put($key,$this->storeData($value,$expires,$denpendency),$expires);
+	}
+
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#fetch()
+	 */
+	public function fetch($key) {
+		$data = unserialize(eaccelerator_get($key));
+		if (empty($data) || !is_array($data)) {
+			return $data;
+		}
+		if (isset($data[self::DEPENDENCY]) && $data[self::DEPENDENCY] instanceof IWindCacheDependency) {
+			if ($data[self::DEPENDENCY]->hasChanged()) {
+				$this->delete($key);
+				return null;
+			}
+		}
+		return isset($data[self::DATA]) ? $data[self::DATA] : null;
+	}
+
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#batchFetch()
+	 */
+	public function batchFetch(array $keys) {
+		$data = array();
+		foreach ($keys as $key) {
+			if ('' != ($value = $this->fetch($key))) {
+				$data[$key] = $value;
+			}
+		}
+		return $data;
+	}
+
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#delete()
+	 */
+	public function delete($key) {
+		return eaccelerator_rm($key);
+	}
+
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#batchDelete()
+	 */
+	public function batchDelete(array $keys) {
+		foreach ($keys as $key) {
+			$this->delete($key);
+		}
+		return true;
+	}
+
+	/* 
+	 * @see wind/component/cache/base/IWindCache#flush()
+	 */
+	public function flush() {
+		eaccelerator_gc();
+		$cacheKeys = eaccelerator_list_keys();
+		foreach($cacheKeys as $key){
+			$this->delete(substr($key['name'], 1));
+		}
+	}
+	
+	/**
+	 * 错误处理
+	 * @param string $message
+	 * @param int $type
+	 */
+	public function error($message, $type = E_USER_ERROR) {
+		trigger_error($message, $type);
+	}
+	
+	/* 
+	 * 获取存储的数据
+	 * @see wind/component/cache/stored/IWindCache#set()
+	 * @return string
+	 */
+	protected function storeData($value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$data = array(self::DATA => $value, self::EXPIRES => $expires, self::STORETIME => time());
+		if ($denpendency && (($denpendency instanceof IWindCacheDependency))) {
+			$denpendency->injectDependent();
+			$data[self::DEPENDENCY] = $denpendency;
+		}
+		return serialize($data);
+	}
 }
