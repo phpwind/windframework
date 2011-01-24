194f88c69c965f13e7058a920abdcf340a2ffcae
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-24 09:42:06 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1115 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/request/IWindRequest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-11-3
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+/**
+ * 处理请求抽象基类
+ * 如http请求
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$ 
+ * @package
+ */
+interface IWindRequest {
+
+}
+
+
+
+
