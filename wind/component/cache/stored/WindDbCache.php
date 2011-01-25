184cdf198b188c79bffba52dc24d1155d5078062
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-25 02:30:22 +0000

    winddbcache缓存实现
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1119 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/cache/stored/WindDbCache.php
+++ wind/component/cache/stored/WindDbCache.php
@@ -13,6 +13,203 @@ L::import('WIND:component.cache.base.IWindCache');
  * @version $Id$ 
  * @package 
  */
-class WindDbCache{
+class WindDbCache implements IWindCache {
+	/**
+	 * @var WindConnectionManager 分布式管理
+	 */
+	protected $distributed = null;
+	/**
+	 * @var string 安全code
+	 */
+	protected $securityCode = '';
+	/**
+	 * @var string 缓存表
+	 */
+	protected $table = 'pw_cache';
 	
+	public function __construct(WindConnectionManager $distributed, $table = 'pw_cache', $securityCode = '') {
+		$this->distributed = $distributed;
+		$this->securityCode = $securityCode;
+		$this->table = $table;
+	}
+	
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#add()
+	 */
+	public function add($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		if ($this->fetch($key)) {
+			$this->error("The cache already exists");
+		}
+		$this->store($key, $value, $expires, $denpendency);
+	}
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#set()
+	 */
+	public function set($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		if ($this->fetch($key)) {
+			return $this->update($key, $value, $expires, $denpendency);
+		} else {
+			return $this->store($key, $value, $expires, $denpendency);
+		}
+		return true;
+	}
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#replace()
+	 */
+	public function replace($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		if ($this->fetch($key)) {
+			return $this->update($key, $value, $expires, $denpendency);
+		} else {
+			$this->error("The cache does not exist");
+			return false;
+		}
+		return true;
+	}
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#fetch()
+	 */
+	public function fetch($key) {
+		$data = $this->getSlaveConnection()->getSqlBuilder()->from($this->table)->field('value')->where('key = :key ', array(':key' => $this->buildSecurityKey($key)))->select()->getRow();
+		$data = unserialize($data['value']);
+		if (isset($data[self::DEPENDENCY]) && $data[self::DEPENDENCY] instanceof IWindCacheDependency) {
+			if ($data[self::DEPENDENCY]->hasChanged()) {
+				$this->delete($key);
+				return '';
+			}
+		}
+		return isset($data[self::DATA]) ? $data[self::DATA] : '';
+	}
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#batchFetch()
+	 */
+	public function batchFetch(array $keys) {
+		foreach ($keys as $key => $value) {
+			$keys[$key] = $this->buildSecurityKey($value);
+		}
+		$data = $this->getSlaveConnection()->getSqlBuilder()->from($this->table)->field('value', 'key')->where('key = :key ', array(':key' => $keys))->select()->getAllRow();
+		$result = $changed = array();
+		foreach ($data as $_data) {
+			$_data = unserialize($_data['value']);
+			if (isset($_data[self::DEPENDENCY]) && $_data[self::DEPENDENCY] instanceof IWindCacheDependency) {
+				if ($_data[self::DEPENDENCY]->hasChanged()) {
+					$changed[] = $_data['key'];
+				} else {
+					$result[$_data['key']] = $_data[self::DATA];
+				}
+			}
+		}
+		$changed && $this->batchDelete($changed);
+		return $result;
+	}
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#delete()
+	 */
+	public function delete($key) {
+		return $this->getMasterConnection()->getSqlBuilder()->from($this->table)->where('key = :key ', array(':key' => $this->buildSecurityKey($key)))->delete();
+	
+	}
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#batchDelete()
+	 */
+	public function batchDelete(array $keys) {
+		foreach ($keys as $key => $value) {
+			$keys[$key] = $this->buildSecurityKey($value);
+		}
+		return $this->getMasterConnection()->getSqlBuilder()->from($this->table)->where('key in (:key) ', array(':key' => $keys))->delete();
+	}
+	
+	/* 
+	 * @see wind/component/cache/base/IWindCache#flush()
+	 */
+	public function flush() {
+		return $this->getMasterConnection()->getSqlBuilder()->from($this->table)->delete();
+	}
+	
+	public function deleteExpiredCache() {
+		return $this->getMasterConnection()->getSqlBuilder()->from($this->table)->where('expires < :expires', array(':expires' => time()))->delete();
+	}
+	
+	/**
+	 * 错误处理
+	 * @param string $message
+	 * @param int $type
+	 */
+	public function error($message, $type = E_USER_ERROR) {
+		trigger_error($message, $type);
+	}
+	
+	/**
+	 * 存储数据
+	 * @param string $key
+	 * @param string $value
+	 * @param int $expires
+	 * @param IWindCacheDependency $denpendency
+	 * @return boolean
+	 */
+	protected function store($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$data = $this->storeData($value, $expires, $denpendency);
+		return $this->getMasterConnection()->getSqlBuilder()->from($this->table)->field('key', 'value', 'expires')->data($key, $data, $expires)->insert();
+	}
+	
+	/**
+	 * 更新数据
+	 * @param string $key
+	 * @param int $value
+	 * @param int $expires
+	 * @param IWindCacheDependency $denpendency
+	 * @return boolean
+	 */
+	protected function update($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$data = $this->storeData($value, $expires, $denpendency);
+		return $this->getMasterConnection()->getSqlBuilder()->from($this->table)->set('value = :value,expires = :expires', array(':value' => $data, ':expires' => $expires))->where('key=:key', array(':key' => $this->buildSecurityKey($key)))->update();
+	}
+	
+	/* 
+	 * 获取存储的数据
+	 * @see wind/component/cache/stored/IWindCache#set()
+	 * @return string
+	 */
+	protected function storeData($value, $expires = 0, IWindCacheDependency $denpendency = null) {
+		$data = array(self::DATA => $value, self::EXPIRES => $expires, self::STORETIME => time());
+		if ($denpendency && (($denpendency instanceof IWindCacheDependency))) {
+			$denpendency->injectDependent();
+			$data[self::DEPENDENCY] = $denpendency;
+		}
+		return serialize($data);
+	}
+	
+	/**
+	 * @return WindDbAdapter
+	 */
+	private function getMasterConnection() {
+		return $this->distributed->getMasterConnection();
+	}
+	
+	/**
+	 * @return WindDbAdapter
+	 */
+	private function getSlaveConnection() {
+		return $this->distributed->getSlaveConnection();
+	}
+	
+	/**
+	 * 生成安全的key
+	 * @param string $key
+	 * @return string
+	 */
+	private function buildSecurityKey($key) {
+		return substr(md5($key . $this->securityCode), 0, 5);
+	}
+	
+	public function __destruct() {
+		$this->deleteExpiredCache();
+	}
+
 }
