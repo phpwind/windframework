c6a9b4d22632ec061d8ba2e735cae1e5b963644a
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-26 06:19:33 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1152 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/WindComponentModule.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-12-21
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license
+ */
+
+L::import('WIND:core.WindModule');
+/**
+ * 框架核心组件基类
+ * the last known user to change this file in the repository  <$LastChangedBy: yishuo $>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id: WindComponent.php 809 2010-12-22 11:28:28Z yishuo $
+ * @package
+ */
+abstract class WindComponentModule extends WindModule {
+
+	private $_attribute = array();
+
+	private $_config = null;
+
+	/**
+	 * Enter description here ...
+	 */
+	public function getAttribute($alias) {
+		return isset($this->_attribute[$alias]) ? $this->_attribute[$alias] : null;
+	}
+
+	/**
+	 * Enter description here ...
+	 * 
+	 * @param string $alias
+	 * @param object $object
+	 */
+	public function setAttribute($alias, $object) {
+		$this->_attribute[$alias] = $object;
+	}
+
+	/**
+	 * @return WindConfig
+	 */
+	public function getConfig() {
+		return $this->_config;
+	}
+
+	/**
+	 * @param WindConfig $config
+	 */
+	public function setConfig($config) {
+		$this->_config = $config;
+	}
+
+}
---
+++ wind/core/WindModule.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author $Author$ <papa0924@gmail.com>
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2010 phpwind.com
+ * @license 
+ */
+
+/**
+ * 所有module的基础抽象类
+ * 主要实现__get(), __set()等方法
+ * 通过继承该类
+ * 
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$ 
+ */
+abstract class WindModule {
+
+	private $_classProxy = null;
+
+	public function toArray() {
+		$class = new ReflectionClass(get_class($this));
+		$properties = $class->getProperties();
+		$vars = array();
+		foreach ($properties as $property) {
+			$_propertyName = $property->name;
+			$vars[$_propertyName] = $this->$_propertyName;
+		}
+		return $vars;
+	}
+
+	/**
+	 * @return the $_classProxy
+	 */
+	public function getClassProxy() {
+		return ($this->_classProxy instanceof WindClassProxy) ? $this->_classProxy : null;
+	}
+
+	/**
+	 * @param WindClassProxy $classProxy
+	 */
+	public function setClassProxy($classProxy) {
+		$this->_classProxy = $classProxy->initClassProxy($this);
+	}
+
+}
