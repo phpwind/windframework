7135aa9c5825607ccba427be77f19f926743ec0c
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-26 06:04:27 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1145 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/config/WindConfig.php
+++ wind/core/config/WindConfig.php
@@ -6,274 +6,142 @@
  * @license 
  */
 
-L::import('WIND:core.config.AbstractWindConfig');
+L::import('WIND:core.WindModule');
 /**
- * 框架配置对象windConfig类，
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
  * @version $Id$ 
  * @package 
  */
-class WindConfig extends AbstractWindConfig {
+class WindConfig extends WindModule {
 
-	const CLASS_PATH = 'class';
+	/* 配置解析信息 */
+	protected $configParser = null;
 
-	const PATH = 'path';
+	protected $cacheName;
 
-	/**
-	 * import 外部配置文件包含
-	 * */
-	const IMPORTS = 'imports';
+	protected $append;
 
-	const IMPORTS_RESOURCE = 'resource';
-
-	const IMPORTS_SUFFIX = 'suffix';
-
-	const IMPORTS_IS_APPEND = 'is-append';
-
-	/*
-	 * app 相关配置
-	 * */
-	const WEB_APPS = 'web-apps';
-
-	const WEB_APP_ROOT_PATH = 'root-path';
-
-	const WEB_APP_FACTORY = 'factory';
-
-	const WEB_APP_FACTORY_CLASS_DEFINITION = 'class-definition';
-
-	const WEB_APP_FILTER = 'filters';
-
-	const WEB_APP_ROUTER = 'router';
-
-	const WEB_APP_MODULE = 'modules';
-
-	const WEB_APP_TEMPLATE = 'template';
+	protected $config = array();
 
 	/**
-	 * 应用的入口地址
-	 */
-	const ERROR = 'error';
-
-	const ERROR_ERRORACTION = 'errorAction';
-
-	/**
-	 * 模板相关配置信息
-	 * 1.模板文件存放路径
-	 * 2.默认的模板文件名称
-	 * 3.模板文件后缀名
-	 * 4.视图解析器
-	 * 5.模板文件的缓存路径
-	 * 6.模板编译路径
-	 */
-	const TEMPLATE = 'templates';
-
-	const TEMPLATE_DIR = 'dir';
-
-	const TEMPLATE_DEFAULT = 'default';
-
-	const TEMPLATE_EXT = 'ext';
-
-	const TEMPLATE_RESOLVER = 'resolver';
-
-	const TEMPLATE_ISCACHE = 'isCache';
-
-	const TEMPLATE_CACHE_DIR = 'cacheDir';
-
-	const TEMPLATE_COMPILER_DIR = 'compileDir';
-
-	/**
-	 * 模板引擎配置信息
-	 */
-	const VIEWER_RESOLVERS = 'viewerResolvers';
-
-	/**
-	 * 路由解析器配置
-	 */
-	const ROUTER_PARSERS = 'routerParsers';
-
-	const ROUTER_PARSERS_RULE = 'rule';
-
-	const ROUTER_PARSERS_CLASS = 'class';
-
-	protected $appName = '';
-
-	protected $imports = array();
-
-	/**
-	 * Enter description here ...
-	 * 
-	 * @param string $config
-	 * @param WindConfigParser $configParser
-	 * @param string $appName
-	 */
-	public function __construct($config, $configParser, $appName) {
-		$cacheName = $appName . '_config';
-		$this->appName = $appName;
-		parent::__construct($config, $configParser, $cacheName);
-	}
-
-	/**
-	 * @param string $appName | 应用名称
 	 * @param string $config | 配置文件路径信息
+	 * @param string $configParser	| 配置解析器
+	 * @param string $cacheName | 配置文件缓存文件名称
+	 * @param WindConfigParser $configParser | 配置解析器
 	 */
-	public function initConfig($config) {
-		if (!is_array($config)) {
-			if ($this->getConfigParser() === null) throw new WindException('configParser is null.');
-			$config = $this->getConfigParser()->parseConfig($config, $this->getCacheName());
-		}
-		$this->setConfig($config);
-		$this->checkWindConfig();
-	}
-
-	/* (non-PHPdoc)
-	 * @see AbstractWindConfig::getConfig()
-	 */
-	public function getConfig($configName = '', $subConfigName = '', $config = array()) {
-		$imports = parent::getConfig(self::IMPORTS);
-		if (key_exists($configName, $imports)) {
-			return $this->parseImport($configName);
-		}
-		return parent::getConfig($configName, $subConfigName, $config);
+	public function __construct($config, $configParser = null, $cacheName = '', $append = false) {
+		$this->setConfigParser($configParser);
+		$this->setCacheName($cacheName);
+		$this->setAppend($append);
+		$this->initConfig($config);
 	}
 
 	/**
-	 * @return the $appName
-	 */
-	public function getAppName() {
-		return $this->appName;
-	}
-
-	/**
-	 * 返回当前应用的启动脚本位置
-	 */
-	public function getAppClass() {
-		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
-		return $this->getConfig(self::CLASS_PATH, '', $_config);
-	}
-
-	/**
-	 * 返回应用路径信息
+	 * 根据配置名取得相应的配置
 	 * 
+	 * @param string $configName
+	 * @param string $subConfigName
 	 * @return string
 	 */
-	public function getRootPath($appName = '') {
-		if ($appName === '') {
-			$appName = $this->appName;
+	public function getConfig($configName = '', $subConfigName = '', $config = array()) {
+		if (!$config) $config = $this->config;
+		if ($configName === '') return $config;
+		
+		$_config = array();
+		if (isset($config[$configName])) {
+			$_config = $config[$configName];
 		}
-		$propertyName = $appName . 'RootPath';
-		if (!isset($this->$propertyName)) {
-			$appConfig = $this->getConfig(self::WEB_APPS, $appName);
-			$rootPath = isset($appConfig[self::WEB_APP_ROOT_PATH]) ? $appConfig[self::WEB_APP_ROOT_PATH] : dirname($_SERVER['SCRIPT_FILENAME']);
-			//TODO 绝对路径相对路径判断，相对于webroot，支持自定义路径
-			$this->$propertyName = $rootPath;
+		if ($subConfigName === '') return $_config;
+		
+		$_subConfig = array();
+		if (is_array($_config) && isset($_config[$subConfigName])) {
+			$_subConfig = $_config[$subConfigName];
 		}
-		return $this->$propertyName;
+		return $_subConfig;
 	}
 
 	/**
-	 * @param string $name
+	 * 初始化配置文件对象,如果参数是非数据格式，
+	 * 则该方法会尝试调用注册进来的配置解析器进行配置解析
+	 * 如果没有注册过任何配置解析器，则抛出异常
+	 * 
+	 * @param array $config
 	 */
-	public function getFactory($name = '') {
-		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
-		return $this->getConfig(self::WEB_APP_FACTORY, $name, $_config);
-	}
-
-	/**
-	 * @param string $name
-	 * @return array|string
-	 */
-	public function getFilters($name = '') {
-		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
-		return $this->getConfig(self::WEB_APP_FILTER, $name, $_config);
-	}
-
-	/**
-	 * @param string $name
-	 * @return array|string
-	 */
-	public function getRouter($name = '') {
-		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
-		return $this->getConfig(self::WEB_APP_ROUTER, $name, $_config);
-	}
-
-	/**
-	 * @param string $name
-	 * @return array|string
-	 */
-	public function getModules($name = '') {
-		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
-		return $this->getConfig(self::WEB_APP_MODULE, $name, $_config);
-	}
-
-	/**
-	 * @param string $name
-	 * @return array|string
-	 */
-	public function getTemplate($name = '') {
-		return $this->getConfig(self::TEMPLATE, $name);
-	}
-
-	/**
-	 * @param string $name
-	 * @return array|string
-	 */
-	public function getViewerResolvers($name = '') {
-		return $this->getConfig(self::VIEWER_RESOLVERS, $name);
-	}
-
-	/**
-	 * @param string $name
-	 * @return Ambigous <string, multitype:, unknown>
-	 */
-	public function getApplications($name = '') {
-		return $this->getConfig(self::APPLICATIONS, $name);
-	}
-
-	/**
-	 * @param string $name
-	 * @return Ambigous <string, multitype:, unknown>
-	 */
-	public function getErrorMessage($name = '') {
-		return $this->getConfig(self::ERROR, $name);
-	}
-
-	/**
-	 * 配置信息合法性检查
-	 */
-	protected function checkWindConfig() {
-		if (!$this->getConfig(self::WEB_APPS, $this->appName)) {
-			throw new WindException('config error.');
+	protected function initConfig($config) {
+		if (!is_array($config)) {
+			$config = $this->parseConfig($config, $this->getCacheName(), $this->getAppend());
 		}
+		$this->setConfig($config);
 	}
 
 	/**
-	 * @param name
-	 * @param cacheName
-	 * @param configPath
-	 * @param append
+	 * 解析配置信息,返回解析后的配置结果
+	 * @param string $config | 配置文件源路径
+	 * @param string $cacheName | 缓存文件名称 | key值
+	 * @param string $append | 配置缓存是否追加到该缓存文件下面
+	 * @return array
 	 */
-	protected function parseImport($name) {
-		if (!isset($this->imports[$name])) {
-			$imports = $this->getConfig(self::IMPORTS);
-			if (!isset($imports[$name])) return array();
-			$import = $imports[$name];
-			$config = array();
-			if (is_array($import) && !empty($import)) {
-				$configPath = L::getRealPath($import[self::IMPORTS_RESOURCE], $import[self::IMPORTS_SUFFIX]);
-				if (!isset($import[self::IMPORTS_IS_APPEND]) || $import[self::IMPORTS_IS_APPEND] === 'true') {
-					$append = $this->cacheName;
-				} elseif ($import[self::IMPORTS_IS_APPEND] === 'false' || $import[self::IMPORTS_IS_APPEND] === '') {
-					$append = false;
-				} else {
-					$append = $import[self::IMPORTS_IS_APPEND];
-				}
-				$cacheName = $append ? $name : $this->appName . '_' . $name . '_config';
-				$config = $this->parseConfig($configPath, $cacheName, $append);
-			}
-			$this->imports[$name] = $config;
+	protected function parseConfig($config, $cacheName, $append) {
+		if ($this->getConfigParser() === null) {
+			throw new WindException('configParser is null.');
 		}
-		return $this->imports[$name];
+		return $this->getConfigParser()->parse($config, $cacheName, $append);
+	}
+
+	/**
+	 * @param $config the $config to set
+	 * @author Qiong Wu
+	 */
+	protected function setConfig($config) {
+		if (!is_array($config)) throw new WindException('config error.');
+		$this->config = $config;
+	}
+
+	/**
+	 * @return WindConfigParser $configParser
+	 */
+	public function getConfigParser() {
+		return $this->configParser;
+	}
+
+	/**
+	 * @param WindConfigParser $configParser
+	 * @author Qiong Wu
+	 */
+	public function setConfigParser($configParser) {
+		if ($this->configParser || $configParser == null) return;
+		$this->configParser = $configParser;
+	}
+
+	/**
+	 * @return the $cacheName
+	 */
+	public function getCacheName() {
+		return $this->cacheName;
+	}
+
+	/**
+	 * @return the $append
+	 */
+	public function getAppend() {
+		return $this->append;
+	}
+
+	/**
+	 * @param $cacheName the $cacheName to set
+	 * @author Qiong Wu
+	 */
+	public function setCacheName($cacheName) {
+		$this->cacheName = $cacheName;
+	}
+
+	/**
+	 * @param $append the $append to set
+	 * @author Qiong Wu
+	 */
+	public function setAppend($append) {
+		$this->append = $append;
 	}
 
 }
---
+++ wind/core/config/WindSystemConfig.php
@@ -0,0 +1,273 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-12-21
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+L::import('WIND:core.config.WindConfig');
+/**
+ * 框架配置对象windConfig类，
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindSystemConfig extends WindConfig {
+
+	const CLASS_PATH = 'class';
+
+	const PATH = 'path';
+
+	/**
+	 * import 外部配置文件包含
+	 * */
+	const IMPORTS = 'imports';
+
+	const IMPORTS_RESOURCE = 'resource';
+
+	const IMPORTS_SUFFIX = 'suffix';
+
+	const IMPORTS_IS_APPEND = 'is-append';
+
+	/*
+	 * app 相关配置
+	 * */
+	const WEB_APPS = 'web-apps';
+
+	const WEB_APP_ROOT_PATH = 'root-path';
+
+	const WEB_APP_FACTORY = 'factory';
+
+	const WEB_APP_FACTORY_CLASS_DEFINITION = 'class-definition';
+
+	const WEB_APP_FILTER = 'filters';
+
+	const WEB_APP_ROUTER = 'router';
+
+	const WEB_APP_MODULE = 'modules';
+
+	const WEB_APP_TEMPLATE = 'template';
+
+	/**
+	 * 应用的入口地址
+	 */
+	const ERROR = 'error';
+
+	const ERROR_ERRORACTION = 'errorAction';
+
+	/**
+	 * 模板相关配置信息
+	 * 1.模板文件存放路径
+	 * 2.默认的模板文件名称
+	 * 3.模板文件后缀名
+	 * 4.视图解析器
+	 * 5.模板文件的缓存路径
+	 * 6.模板编译路径
+	 */
+	const TEMPLATE = 'templates';
+
+	const TEMPLATE_DIR = 'dir';
+
+	const TEMPLATE_DEFAULT = 'default';
+
+	const TEMPLATE_EXT = 'ext';
+
+	const TEMPLATE_RESOLVER = 'resolver';
+
+	const TEMPLATE_ISCACHE = 'isCache';
+
+	const TEMPLATE_CACHE_DIR = 'cacheDir';
+
+	const TEMPLATE_COMPILER_DIR = 'compileDir';
+
+	/**
+	 * 模板引擎配置信息
+	 */
+	const VIEWER_RESOLVERS = 'viewerResolvers';
+
+	protected $appName = '';
+
+	protected $imports = array();
+
+	/**
+	 * Enter description here ...
+	 * 
+	 * @param string $config
+	 * @param WindConfigParser $configParser
+	 * @param string $appName
+	 */
+	public function __construct($config, $configParser, $appName) {
+		$cacheName = $appName . '_config';
+		$this->appName = $appName;
+		parent::__construct($config, $configParser, $cacheName);
+	}
+
+	/**
+	 * @param string $appName | 应用名称
+	 * @param string $config | 配置文件路径信息
+	 */
+	public function initConfig($config) {
+		if (!is_array($config)) {
+			if ($this->getConfigParser() === null) throw new WindException('configParser is null.');
+			$config = $this->getConfigParser()->parseConfig($config, $this->getCacheName());
+		}
+		$this->setConfig($config);
+		$this->checkWindConfig();
+	}
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindConfig::getConfig()
+	 */
+	public function getConfig($configName = '', $subConfigName = '', $config = array()) {
+		$imports = parent::getConfig(self::IMPORTS);
+		if (key_exists($configName, $imports)) {
+			return $this->parseImport($configName);
+		}
+		return parent::getConfig($configName, $subConfigName, $config);
+	}
+
+	/**
+	 * @return the $appName
+	 */
+	public function getAppName() {
+		return $this->appName;
+	}
+
+	/**
+	 * 返回当前应用的启动脚本位置
+	 */
+	public function getAppClass() {
+		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
+		return $this->getConfig(self::CLASS_PATH, '', $_config);
+	}
+
+	/**
+	 * 返回应用路径信息
+	 * 
+	 * @return string
+	 */
+	public function getRootPath($appName = '') {
+		if ($appName === '') $appName = $this->appName;
+		
+		$propertyName = $appName . '_RootPath';
+		if (!isset($this->$propertyName)) {
+			$appConfig = $this->getConfig(self::WEB_APPS, $appName);
+			if (isset($appConfig[self::WEB_APP_ROOT_PATH]) && !empty($appConfig[self::WEB_APP_ROOT_PATH]))
+				$rootPath = $appConfig[self::WEB_APP_ROOT_PATH];
+			else
+				$rootPath = dirname($_SERVER['SCRIPT_FILENAME']);
+			
+		//TODO 绝对路径相对路径判断，相对于webroot，支持自定义路径
+			$this->$propertyName = $rootPath;
+		}
+		return $this->$propertyName;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function getFactory($name = '') {
+		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
+		return $this->getConfig(self::WEB_APP_FACTORY, $name, $_config);
+	}
+
+	/**
+	 * @param string $name
+	 * @return array|string
+	 */
+	public function getFilters($name = '') {
+		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
+		return $this->getConfig(self::WEB_APP_FILTER, $name, $_config);
+	}
+
+	/**
+	 * @param string $name
+	 * @return array|string
+	 */
+	public function getRouter($name = '') {
+		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
+		return $this->getConfig(self::WEB_APP_ROUTER, $name, $_config);
+	}
+
+	/**
+	 * @param string $name
+	 * @return array|string
+	 */
+	public function getModules($name = '') {
+		$_config = $this->getConfig(self::WEB_APPS, $this->appName);
+		return $this->getConfig(self::WEB_APP_MODULE, $name, $_config);
+	}
+
+	/**
+	 * @param string $name
+	 * @return array|string
+	 */
+	public function getTemplate($name = '') {
+		return $this->getConfig(self::TEMPLATE, $name);
+	}
+
+	/**
+	 * @param string $name
+	 * @return array|string
+	 */
+	public function getViewerResolvers($name = '') {
+		return $this->getConfig(self::VIEWER_RESOLVERS, $name);
+	}
+
+	/**
+	 * @param string $name
+	 * @return Ambigous <string, multitype:, unknown>
+	 */
+	public function getApplications($name = '') {
+		return $this->getConfig(self::APPLICATIONS, $name);
+	}
+
+	/**
+	 * @param string $name
+	 * @return Ambigous <string, multitype:, unknown>
+	 */
+	public function getErrorMessage($name = '') {
+		return $this->getConfig(self::ERROR, $name);
+	}
+
+	/**
+	 * 配置信息合法性检查
+	 */
+	protected function checkWindConfig() {
+		if (!$this->getConfig(self::WEB_APPS, $this->appName)) {
+			throw new WindException('config error.');
+		}
+	}
+
+	/**
+	 * @param name
+	 * @param cacheName
+	 * @param configPath
+	 * @param append
+	 */
+	protected function parseImport($name) {
+		if (!isset($this->imports[$name])) {
+			$imports = $this->getConfig(self::IMPORTS);
+			if (!isset($imports[$name])) return array();
+			$import = $imports[$name];
+			$config = array();
+			if (is_array($import) && !empty($import)) {
+				$configPath = L::getRealPath($import[self::IMPORTS_RESOURCE], $import[self::IMPORTS_SUFFIX]);
+				if (!isset($import[self::IMPORTS_IS_APPEND]) || $import[self::IMPORTS_IS_APPEND] === 'true') {
+					$append = $this->cacheName;
+				} elseif ($import[self::IMPORTS_IS_APPEND] === 'false' || $import[self::IMPORTS_IS_APPEND] === '') {
+					$append = false;
+				} else {
+					$append = $import[self::IMPORTS_IS_APPEND];
+				}
+				$cacheName = $append ? $name : $this->appName . '_' . $name . '_config';
+				$config = $this->parseConfig($configPath, $cacheName, $append);
+			}
+			$this->imports[$name] = $config;
+		}
+		return $this->imports[$name];
+	}
+
+}
