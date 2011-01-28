9f0094654baa5c9aea35c3c8bbad21a30655dd36
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-01-28 05:33:31 +0000

    集合类创建.
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1195 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/connections/WindQueue.php
+++ wind/component/connections/WindQueue.php
@@ -6,3 +6,128 @@
  * @package 
  * tags
  */
+
+/**
+ * 队列操作，先进先出
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindQueue implements IteratorAggregate,Countable{
+	/**
+	 * @var array 队列列表
+	 */
+	private $list = array();
+	/**
+	 * @var string 列表总数指示器
+	 */
+	private $count = 0;
+	
+	/**
+	 * 移除并返回位于 Queue顶部的元素。
+	 * @return mixed
+	 */
+	public function dequeue(){
+		if(!$this->count){
+			throw new WindException("The queue is empty");
+		}
+		--$this->count;
+		return array_shift($this->list);
+	}
+	
+	/**
+	 * 返回位于 Queue顶部的对象但不将其移除。
+	 * @return mixed
+	 */
+	public function peek(){
+		if(!$this->count){
+			throw new WindException("The queue is empty");
+		}
+		return $this->list[0];
+	}
+	
+	/**
+	 * 将元素添加到 Queue的底部。
+	 * @param mixed $value
+	 * @return number
+	 */
+	public function enqueue($value){
+		++$this->count;
+		return array_push($this->list,$value);
+	}
+	
+	/**
+	 * 确定某元素是否在 Queue中。
+	 * @param mixed $value
+	 * @return boolean
+	 */
+	public function contain($value){
+		return false !==array_search($value, $this->list, true);
+	}
+	
+	/**
+	 * 将数组中的值合并到当前WindQueue队列
+	 * @param array $array 要合并的数组
+	 * @return boolean
+	 */
+	public function mergeFromArray(array $array) {
+		foreach ($array as $value) {
+			$this->enqueue($value);
+		}
+		return true;
+	}
+	/**
+	 * 将WindQueue队列集合合并到当前WindQueue队列
+	 * @param WindQueue $list 要合并的WindQueue集合
+	 * @return boolean
+	 */
+	public function mergeFromList(WindQueue $queue) {
+		foreach ($queue as $value) {
+			$this->enqueue($value);
+		}
+		return true;
+	}
+	
+	/**
+	 *清空队列 
+	 */
+	public function clear(){
+		$this->list = array();
+		$this->count = 0;
+	}
+	
+	/**
+	 * 创建 Queue的浅表副本。
+	 * @return WindQueue
+	 */
+	public function __clone(){
+		return new self();
+	}
+	
+	/**
+	 * 取得队列个数
+	 * @return int
+	 */
+	public function getCount(){
+		return $this->count;
+	}
+	
+	
+	/* 
+	 * 计算队列个数
+	 * @see Countable#count()
+	 */
+	public function count() {
+		return $this->getCount();
+	}
+
+	/**
+	 * 取得队列的迭代器
+	 */
+	public function getIterator() {
+		return new ArrayIterator($this->list);
+	}
+
+	
+}
--- wind/component/connections/WindStack.php
+++ wind/component/connections/WindStack.php
@@ -6,3 +6,119 @@
  * @package 
  * tags
  */
+
+/**
+ * 堆栈操作，先进后出
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindStack implements IteratorAggregate,Countable{
+	
+	/**
+	 * @var array 集合列表
+	 */
+	private $list = array();
+	/**
+	 * @var string 列表总数指示器
+	 */
+	private $count = 0;
+	
+	/**
+	 * 移除并返回位于 Stack 底部的对象。
+	 * @return mixed
+	 */
+	public function pop(){
+		if(!$this->count){
+			throw new WindException("The stack is empty");
+		}
+		--$this->count;
+		return array_pop($this->list);
+	}
+	
+	/**
+	 * 返回位于 Stack底部的对象但不将其移除。
+	 * @return mixed
+	 */
+	public function peek(){
+		if(!$this->count){
+			throw new WindException("The stack is empty");
+		}
+		return $this->list[$this->count-1];
+	}
+	
+	/**
+	 * 确定某元素是否在 Stack中。
+	 * @param mixed $value
+	 * @return boolean
+	 */
+	public function contain($value){
+		return false !== array_search($value, $this->list, true);
+	}
+	/**
+	 * 将元素插入 Stack 的底部。
+	 * @param mixed $value
+	 * @return number
+	 */
+	public function push($value){
+		++$this->count;
+		return array_push($this->list,$value);
+	}
+	/**
+	 * 清空队列
+	 */
+	public function clear(){
+		$this->count = 0;
+		$this->list = array();
+	}
+    /**
+	 * 将数组中的值合并到当前tack队列
+	 * @param array $array 要合并的数组
+	 * @return boolean
+	 */
+	public function mergeFromArray(WindStack $array) {
+		foreach ($array as $value) {
+			$this->push($value);
+		}
+		return true;
+	}
+	/**
+	 * 将WindStack堆栈集合合并到当前Stack队列
+	 * @param WindStack $list 要合并的WindStack集合
+	 * @return boolean
+	 */
+	public function mergeFromList(WindStack  $stack) {
+		foreach ($stack as $value) {
+			$this->push($value);
+		}
+		return true;
+	}
+	/**
+	 * 取得堆栈个数
+	 * @return string
+	 */
+	public function getCount(){
+		return $this->count;
+	}
+	/* 
+	 * 计算堆栈的总个数
+	 * @see Countable#count()
+	 */
+	public function count() {
+		return $this->getCount();
+	}
+	/**
+	 * 取得堆栈的迭代器
+	 */
+	public function getIterator() {
+		return new ArrayIterator($this->list);
+	}
+	/**
+	 * 创建 stack的浅表副本。
+	 * @return WindStack
+	 */
+	public function __clone(){
+		return new self();
+	}
+}
