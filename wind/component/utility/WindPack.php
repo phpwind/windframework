b4c378f7a658f61cd76a873f60c5f62acf8de853
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-17 06:42:49 +0000

    组件目录调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1257 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/utility/WindPack.php
@@ -0,0 +1,426 @@
+<?php
+/**
+ * @author Qian Su <aoxue.1988.su.qian@163.com> 2010-11-19
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+/**
+ * 程序打包工具
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindPack {
+
+	const STRIP_SELF = 'stripWhiteSpaceBySelf';
+
+	const STRIP_PHP = 'stripWhiteSpaceByPhp';
+
+	const STRIP_TOKEN = 'stripWhiteSpaceByToken';
+
+	private $packList = array();
+
+	private $contentInjectionPosition;
+
+	private $contentInjectionCallBack = '';
+
+	/**
+	 * 将指定文件类型且指定文件夹下的所指定文件打包成一个易阅读的文件,
+	 * @param mixed $dir 要打包的目录
+	 * @param string $dst 文件名
+	 * @param string $packMethod 打包方式
+	 * @param boolean $compress 是否压缩
+	 * @param string $absolutePath 文件路径
+	 * @param array $ndir 不须要打包的目录
+	 * @param array $suffix 不永许打包的文件类型
+	 * @return string
+	 */
+	public function packFromDir($dir, $dst, $packMethod = WindPack::STRIP_PHP, $compress = true, $absolutePath = '', $ndir = array('.','..','.svn'), $suffix = array(), $nfile = array()) {
+		if (empty($dst) || empty($dir)) {
+			return false;
+		}
+		$suffix = is_array($suffix) ? $suffix : array($suffix);
+		if (!($content = $this->readContentFromDir($packMethod, $dir, $absolutePath, $ndir, $suffix, $nfile))) {
+			return false;
+		}
+		$fileSuffix = $this->getFileSuffix($dst);
+		$replace = $compress ? ' ' : "\n";
+		$content = implode($replace, $content);
+		$content = $this->callBack($content, $replace);
+		$content = $this->stripNR($content, $replace);
+		$content = $this->stripPhpIdentify($content, '');
+		$content = $this->stripImport($content, '');
+		$content = $this->getContentBySuffix($content, $fileSuffix, $replace);
+		$this->writeContentToFile($dst, $content);
+		return true;
+	}
+
+	/**
+	 * @param mixed $fileList
+	 * @param string $dst
+	 * @param method $packMethod
+	 * @param boolean $compress
+	 * @param string $absolutePath
+	 * @return string|string
+	 */
+	public function packFromFileList($fileList, $dst, $packMethod = WindPack::STRIP_PHP, $compress = true, $absolutePath = '') {
+		if (empty($dst) || empty($fileList)) {
+			return false;
+		}
+		$content = array();
+		$this->readContentFromFileList($fileList, $packMethod, $absolutePath, $content);
+		
+		$fileSuffix = $this->getFileSuffix($dst);
+		$replace = $compress ? ' ' : "\n";
+		$content = implode($replace, $content);
+		$content = $this->callBack($content, $replace);
+		$content = $this->stripNR($content, $replace);
+		$content = $this->stripPhpIdentify($content, '');
+		$content = $this->stripImport($content, '');
+		$content = $this->getContentBySuffix($content, $fileSuffix, $replace);
+		$this->writeContentToFile($dst, $content);
+		return true;
+	}
+
+	/**
+	 * 去除指定文件的注释及空白
+	 * @param string $filename 文件名
+	 */
+	public function stripWhiteSpaceByPhp($filename) {
+		return php_strip_whitespace($filename);
+	}
+
+	public function stripWhiteSpaceBySelf($filename, $compress = true) {
+		$content = $this->getContentFromFile($filename);
+		$content = $this->stripComment($content, '');
+		$content = $this->stripSpace($content, ' ');
+		return $content;
+	}
+
+	public function stripWhiteSpaceByToken($filename) {
+		$content = $this->getContentFromFile($filename);
+		$compressContent = '';
+		$lastToken = 0;
+		foreach (token_get_all($content) as $key => $token) {
+			if (is_array($token)) {
+				if (in_array($token[0], array(T_COMMENT, T_WHITESPACE, T_DOC_COMMENT))) {
+					continue;
+				}
+				$compressContent .= ' ' . $token[1];
+			} else {
+				$compressContent .= $token;
+			}
+			$lastToken = $token[0];
+		}
+		return $compressContent;
+	}
+
+	/**
+	 * 去除注释
+	 * @param string $content 要去除的内容
+	 * @param mixed $replace 要替换的文本
+	 * @return string
+	 */
+	public function stripComment($content, $replace = '') {
+		return preg_replace('/(?:\/\*.*\*\/)*|(?:\/\/[^\r\n]*[\r\n])*/Us', $replace, $content);
+	}
+
+	/**
+	 * 去除换行
+	 * @param string $content 要去除的内容
+	 * @param mixed $replace 要替换的文本
+	 * @return string
+	 */
+	public function stripNR($content, $replace = array("\n","\r\n","\r")) {
+		return preg_replace("/[\n\r]+/", $replace, $content);
+	}
+
+	/**
+	 * 去除空格符
+	 * @param string $content 要去除的内容
+	 * @param mixed $replace 要替换的文本
+	 * @return string
+	 */
+	public function stripSpace($content, $replace = ' ') {
+		return preg_replace("/[ ]+/", $replace, $content);
+	}
+
+	/**
+	 * 去除php标识
+	 * @param string $content
+	 * @param mixed $replace
+	 * @return string
+	 */
+	public function stripPhpIdentify($content, $replace = '') {
+		return preg_replace("/(?:<\?(?:php)*)|(\?>)/i", $replace, $content);
+	}
+
+	/**
+	 * 根据指定规则替换指定内容中相应的内容
+	 * @param string $content
+	 * @param string $rule
+	 * @param $mixed $replace
+	 * @return string
+	 */
+	public function stripStrByRule($content, $rule, $replace = '') {
+		return preg_replace("/$rule/", $replace, $content);
+	}
+
+	/**
+	 * 去除多余的文件导入信息
+	 * @param string $content
+	 * @param mixed $replace
+	 * @return string
+	 */
+	public function stripImport($content, $replace = '') {
+		$str = preg_match_all('/L[\t ]*::[\t ]*import[\t ]*\([\t ]*[\'\"]([^$][\w\.:]+)[\"\'][\t ]*\)[\t ]*/', $content, $matchs);
+		if ($matchs[1]) {
+			foreach ($matchs[1] as $key => $value) {
+				$name = substr($value, strrpos($value, '.') + 1);
+				if (preg_match("/(abstract[\t ]*|class|interface)[\t ]+$name/i", $content)) {
+					$strip = str_replace(array('(', ')'), array('\(', '\)'), addslashes($matchs[0][$key])) . '[\t ]*;';
+					$content = $this->stripStrByRule($content, $strip, $replace);
+				}
+			}
+		}
+		return $content;
+	}
+
+	/**
+	 * 取得被打包的文件列表
+	 * @return array:
+	 */
+	public function getPackList() {
+		return $this->packList;
+	}
+
+	/**
+	 *从文件读取内容
+	 * @param string $filename 文件名
+	 * @return string
+	 */
+	public function getContentFromFile($filename) {
+		if ($this->isFile($filename)) {
+			$content = '';
+			$fp = fopen($filename, "r");
+			while (!feof($fp)) {
+				$line = fgets($fp);
+				if (in_array(strlen($line), array(2, 3)) && in_array(ord($line), array(9, 10, 13))) continue;
+				$content .= $line;
+			}
+			fclose($fp);
+			return $content;
+		}
+		return false;
+	}
+
+	/**
+	 * 将内容打包的文件
+	 * @param string $filename 文件内容
+	 * @param string $content  要打包的指定文件的内容
+	 * @return string
+	 */
+	public function writeContentToFile($filename, $content) {
+		$fp = fopen($filename, "w");
+		fwrite($fp, $content);
+		fclose($fp);
+		return true;
+	}
+
+	/**
+	 * 根据文件后缀得取对应的mime内容
+	 * @param string $content 要打包的内容内容
+	 * @param string $suffix 文件后缀类型
+	 * @param string $replace
+	 * @return string
+	 */
+	public function getContentBySuffix($content, $suffix, $replace = ' ') {
+		switch ($suffix) {
+			case 'php':
+				$content = '<?php' . $replace . $content . '?>';
+			default:
+				;
+		}
+		return $content;
+	}
+
+	/**
+	 * 从各个目录中取得对应的每个文件的内容 
+	 * @param string $packMethod 打包方式
+	 * @param mixed $dir 目录名
+	 * @param string $absolutePath 绝对路径名
+	 * @param array $ndir 不须要打包的文件夹
+	 * @param array $suffix 不须要打包的文件类型
+	 * @param array $nfile 不须要打包的文件
+	 * @return array
+	 */
+	public function readContentFromDir($packMethod = WindPack::STRIP_PHP, $dir = array(), $absolutePath = '', $ndir = array('.','..','.svn'), $suffix = array(), $nfile = array()) {
+		static $content = array();
+		if (empty($dir) || false === $this->isValidatePackMethod($packMethod)) {
+			return false;
+		}
+		$dir = is_array($dir) ? $dir : array($dir);
+		foreach ($dir as $_dir) {
+			$_dir = is_dir($absolutePath) ? $this->realDir($absolutePath) . $_dir : $_dir;
+			if ($this->isDir($_dir)) {
+				$handle = dir($_dir);
+				while (false != ($tmp = $handle->read())) {
+					$name = $this->realDir($_dir) . $tmp;
+					if ($this->isDir($name) && !in_array($tmp, $ndir)) {
+						$this->readContentFromDir($packMethod, $name, $absolutePath, $ndir, $suffix, $nfile);
+					}
+					if ($this->isFile($name) && !in_array($this->getFileSuffix($name), $suffix) && !in_array($file = $this->getFileName($name), $nfile)) {
+						$content[] = $this->$packMethod($name);
+						$this->setPackList($file, $name);
+					}
+				}
+				$handle->close();
+			}
+		}
+		return $content;
+	}
+
+	/**
+	 * array('E://www/wind/core/WindAciton.php'=>'WindAction')
+	 * @param mixed $fileList
+	 * @param method $packMethod
+	 * @param string $absolutePath
+	 * @return array:
+	 */
+	public function readContentFromFileList($fileList, $packMethod = WindPack::STRIP_PHP, $absolutePath = '', &$content = array()) {
+		if (empty($fileList) || false === $this->isValidatePackMethod($packMethod)) {
+			return array();
+		}
+		$fileList = is_array($fileList) ? $fileList : array($fileList);
+		foreach ($fileList as $key => $value) {
+			if (is_array($value) && isset($value[1])) {
+				$parents = class_parents($value[1]);
+				$_fileList = $this->buildFileList($parents, $fileList);
+				$this->readContentFromFileList($_fileList, $packMethod, $absolutePath, $content);
+				
+				$implements = class_implements($value[1]);
+				$_fileList = $this->buildFileList($implements, $fileList);
+				$this->readContentFromFileList($_fileList, $packMethod, $absolutePath, $content);
+				
+				if (key_exists($key, $this->getPackList())) continue;
+				
+				$file = is_dir($absolutePath) ? $this->realDir($absolutePath) . $key : $key;
+				if (is_file($file)) {
+					$content[] = $this->$packMethod($file);
+					$this->setPackList($key, $value);
+				}
+			}
+		}
+	}
+
+	private function buildFileList($list, $fileList) {
+		$_temp = array();
+		foreach ($list as $fileName) {
+			foreach ($fileList as $key => $value) {
+				if ($value[1] == $fileName) {
+					$_temp[$key] = $value;
+					break;
+				}
+			}
+		}
+		return $_temp;
+	}
+
+	private function isValidatePackMethod($packMethod) {
+		return method_exists($this, $packMethod) && in_array($packMethod, array(WindPack::STRIP_PHP, 
+			WindPack::STRIP_SELF, WindPack::STRIP_TOKEN));
+	}
+
+	/**
+	 * 添加被打包的文件到列表
+	 * @param  string $key
+	 * @param  string $value
+	 */
+	private function setPackList($key, $value) {
+		if (isset($this->packList[$key])) {
+			if (is_array($this->packList[$key])) {
+				array_push($this->packList[$key], $value);
+			} else {
+				$tmp_name = $this->packList[$key];
+				$this->packList[$key] = array($tmp_name, $value);
+			}
+		} else {
+			$this->packList[$key] = $value;
+		}
+	}
+
+	/**
+	 * 取得真实的目录
+	 * @param string $path 路径名
+	 * @return string
+	 */
+	public function realDir($path) {
+		if (($pos = strrpos($path, DIRECTORY_SEPARATOR)) === strlen($path) - 1) {
+			return $path;
+		}
+		return $path . DIRECTORY_SEPARATOR;
+	}
+
+	/**
+	 * 判断是否是一个文件
+	 * @param string $filename 文件名
+	 * @return boolean
+	 */
+	public function isFile($filename) {
+		return is_file($filename);
+	}
+
+	/**
+	 * 判断是否是一个目录
+	 * @param string $dir 目录名
+	 * @return boolean
+	 */
+	public function isDir($dir) {
+		return is_dir($dir);
+	}
+
+	public function getFileSuffix($filename) {
+		return substr($filename, strrpos($filename, '.') + 1);
+	}
+
+	public function getFileName($path, $ifsuffix = false) {
+		$filename = substr($path, strrpos($path, DIRECTORY_SEPARATOR) + 1);
+		return $ifsuffix ? $filename : substr($filename, 0, strrpos($filename, '.'));
+	}
+
+	public function getFilePath($path) {
+		return substr($path, 0, strrpos($path, DIRECTORY_SEPARATOR));
+	}
+
+	/**
+	 * @param $contentInjectionCallBack the $contentInjectionCallBack to set
+	 * @param string $position 调用位置(before|after)
+	 * @author Qiong Wu
+	 */
+	public function setContentInjectionCallBack($contentInjectionCallBack, $position = 'before') {
+		if (!in_array($position, array('before', 'after'))) $position = 'before';
+		$this->contentInjectionPosition = $position;
+		$this->contentInjectionCallBack = $contentInjectionCallBack;
+	}
+
+	/**
+	 * 回调函数调用
+	 * @param string $content
+	 * @param string $replace
+	 * @return string
+	 */
+	public function callBack($content, $replace = '') {
+		if ($this->contentInjectionCallBack !== '') {
+			$_content = call_user_func_array($this->contentInjectionCallBack, array($this->getPackList()));
+			if ($this->contentInjectionPosition == 'before') {
+				$content = $replace . $_content . $content;
+			} elseif ($this->contentInjectionPosition == 'after')
+				$content .= $replace . $_content . $replace;
+		}
+		return $content;
+	}
+
+}
---
+++ wind/component/utility/WindValidator.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * @author Qian Su <aoxue.1988.su.qian@163.com> 2010-12-22
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qian Su <aoxue.1988.su.qian@163.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindValidator {
+	
+	public function validate($input, $validator) {
+		return $this->$validator($input);
+	}
+	
+	/**
+	 * 在 $string 字符串中搜索与 $regExp 给出的正则表达式相匹配的内容。
+	 * @param string $regExp  搜索的规则(正则)
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function validateByRegExp($regExp, $string, &$matches = array(), $ifAll = false) {
+		if (true === $ifAll) {
+			return preg_match_all($regExp, $string, $matches);
+		}
+		return preg_match($regExp, $string, $matches);
+	}
+	
+	/**
+	 * 验证是否是有合法的email
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function hasEmail($string, &$matches = array(), $ifAll = false) {
+		return self::validateByRegExp("/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/", $string, $matches, $ifAll);
+	}
+	
+	/**
+	 * 验证是否是合法的email
+	 * @param string $string
+	 * @return boolean
+	 */
+	public function isEmail($string) {
+		return 0 < preg_match("/^\w+(?:[-+.']\w+)*@\w+(?:[-.]\w+)*\.\w+(?:[-.]\w+)*$/", $string);
+	}
+	
+	/**
+	 * 验证是否有合法的身份证号
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function hasIdCard($string, &$matches = array(), $ifAll = false) {
+		return self::validateByRegExp("/\d{17}[\d|X]|\d{15}/", $string, $matches, $ifAll);
+	}
+	
+	/**
+	 * 验证是否是合法的身份证号
+	 * @param string $string
+	 * @return boolean
+	 */
+	public function isIdCard($string) {
+		return 0 < preg_match("/^(?:\d{17}[\d|X]|\d{15})$/", $string);
+	}
+	
+	/**
+	 * 验证是否有合法的URL
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function hasUrl($string, &$matches = array(), $ifAll = false) {
+		return self::validateByRegExp('/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/', $string, $matches, $ifAll);
+	}
+	
+	/**
+	 * 验证是否是合法的url
+	 * @param string $string
+	 * @return boolean
+	 */
+	public function isUrl($string) {
+		return 0 < preg_match('/^(?:http(?:s)?:\/\/(?:[\w-]+\.)+[\w-]+(?:\:\d+)*+(?:\/[\w- .\/?%&=]*)?)$/', $string);
+	}
+	
+	/**
+	 * 验证是否有中文
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function hasChinese($string, &$matches = array(), $ifAll = false) {
+		return self::validateByRegExp('/[\x{4e00}-\x{9fa5}]+/u', $string, $matches, $ifAll);
+	}
+	
+	/**
+	 * 验证是否是中文
+	 * @param string $string
+	 * @return boolean
+	 */
+	public function isChinese($string) {
+		return 0 < preg_match('/^[\x{4e00}-\x{9fa5}]+$/u', $string);
+	}
+	
+	/**
+	 * 验证是否有html标记
+	 * @param string $string  被搜索的 字符串
+	 * @param array $matches 会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function hasHTML($string, &$matches = array(), $ifAll = false) {
+		return self::validateByRegExp('/<(.*)>.*|<(.*)\/>/', $string, $matches, $ifAll);
+	}
+	
+	/**
+	 * 验证是否是合法的html标记
+	 * @param string $string
+	 * @return boolean
+	 */
+	public function isHTML($string) {
+		return 0 < preg_match('/^<(.*)>.*|<(.*)\/>$/', $string);
+	}
+	
+	/**
+	 * 验证是否是非负整数
+	 * @param string $string  被搜索的 字符串
+	 * @return number
+	 */
+	public function isNonNegative($string) {
+		return 0 < preg_match("/^\d+$/", $string);
+	}
+	
+	/**
+	 * 验证是否是正数
+	 * @param string $string  被搜索的 字符串
+	 * @return number
+	 */
+	public function isPositive($string) {
+		return 0 < preg_match('/^[1-9][0-9]*$/', $string);
+	}
+	
+	/**
+	 * 验证是否是负数
+	 * @param string $string   被搜索的 字符串
+	 * @return number
+	 */
+	public function isNegative($string) {
+		return 0 < preg_match('/^-[1-9][0-9]*$/', $string);
+	}
+	
+	/**
+	 * 验证是否有合法的ipv4地址
+	 * @param string $string   被搜索的 字符串
+	 * @param array $matches   会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function hasIP($string, &$matches = array(), $ifAll = false) {
+		return self::validateByRegExp('/(\d+)\.(\d+)\.(\d+)\.(\d+)/', $string, $matches, $ifAll);
+	}
+	
+	/**
+	 * 验证是否是合法的IP
+	 * @param string $string
+	 * @return boolean
+	 */
+	public function isIP($string) {
+		return 0 < preg_match('/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/', $string);
+	}
+	
+	/**
+	 * 验证是否有客户端脚本
+	 * @param string $string   被搜索的 字符串
+	 * @param array $matches   会被搜索的结果
+	 * @param boolean $ifAll   是否进行全局正则表达式匹配
+	 * @return number
+	 */
+	public function hasScript($string, &$matches = array(), $ifAll = false) {
+		return self::validateByRegExp('/<script(.*?)>([^\x00]*?)<\/script>/', $string, $matches, $ifAll);
+	}
+	
+	/**
+	 * 验证是否是合法的客户端脚本
+	 * @param string $string
+	 * @return boolean
+	 */
+	public function isScript($string) {
+		return 0 < preg_match('/<script(?:.*?)>(?:[^\x00]*?)<\/script>/', $string);
+	}
+	
+	public function isInt($value) {
+		return is_int($value);
+	}
+	
+	public function isFloat($value) {
+		return is_float($value);
+	}
+	
+	public function isArray($value) {
+		return is_array($value);
+	}
+	
+	public function isBool($value) {
+		return is_bool($value);
+	}
+	
+	public function isEmpty($value) {
+		return empty($value);
+	}
+	
+	/**
+	 * required验证，如果输入的值为：空字符串，empty，null则返回false
+	 * @param string $value
+	 * @return boolean
+	 */
+	public function isRequired($value) {
+		if (empty($value)) return false;
+		if ($value === null) return false;
+		return true;
+	}
+	
+	/**
+	 * 验证字符串的长度
+	 * @param string $string 要验证的字符串
+	 * @param string $length 指定的合法的长度
+	 * @param string $charset 字符编码
+	 * @return boolean
+	 */
+	public function isLegalLength($string, $length, $charset = 'utf8') {
+		L::import('WIND:component.format.WindString');
+		return WindString::strlen($string, $charset) > (int) $length;
+	}
+
+}
