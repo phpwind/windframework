1c061b3bfc215c073a3869c4ad335b84fd044c90
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-18 02:31:28 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1271 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/dal/template/WindDbTemplate.php
+++ wind/core/dal/template/WindDbTemplate.php
@@ -1,12 +1,7 @@
 <?php
-/**
- * the last known user to change this file in the repository  <$LastChangedBy$>
- * @author Qian Su <aoxue.1988.su.qian@163.com>
- * @version $Id$ 
- * @package 
- * tags
- */
+
 L::import('WIND:component.db.WindConnectionManager');
+
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qian Su <aoxue.1988.su.qian@163.com>
@@ -14,20 +9,22 @@ L::import('WIND:component.db.WindConnectionManager');
  * @package 
  */
 class WindDbTemplate extends WindTemplate {
+
 	/**
 	 * @var WindConnectionManager 分布式管理与数据库驱动工厂
 	 */
 	protected $distributed = null;
+
 	/**
 	 * @var WindDbAdapter 数据库操作适配器
 	 */
 	protected $connection = null;
-	
-	
+
 	public function __construct(array $config) {
 		$this->distributed = new WindConnectionManager($config);
 		$this->getMasterConnection();
 	}
+
 	/**
 	 * 将查询的结果作为对象返回
 	 * @param string $sql 要执行的sql语句
@@ -39,7 +36,7 @@ class WindDbTemplate extends WindTemplate {
 		$result = $this->queryBySql($sql, $fetch_type);
 		return $this->bindValueToObject(new stdClass(), $result, $colAsProp);
 	}
-	
+
 	/**
 	 * 将查询的结果作为数组返回
 	 * @param string $sql 要执行的sql语句
@@ -49,7 +46,7 @@ class WindDbTemplate extends WindTemplate {
 	public function queryForArray($sql, $fetch_type = IWindDbConfig::RESULT_ASSOC) {
 		return $this->queryBySql($sql, $fetch_type);
 	}
-	
+
 	/**
 	 * 新增操作
 	 * @param string $sql
@@ -58,6 +55,7 @@ class WindDbTemplate extends WindTemplate {
 	public function insert($sql) {
 		return $this->write($sql);
 	}
+
 	/**
 	 * 删除操作
 	 * @param string $sql
@@ -66,6 +64,7 @@ class WindDbTemplate extends WindTemplate {
 	public function delete($sql) {
 		return $this->write($sql);
 	}
+
 	/**
 	 * 更新操作
 	 * @param string $sql
@@ -74,6 +73,7 @@ class WindDbTemplate extends WindTemplate {
 	public function update($sql) {
 		return $this->write($sql);
 	}
+
 	/**
 	 * 查询操作
 	 * @param string $sql
@@ -85,6 +85,7 @@ class WindDbTemplate extends WindTemplate {
 		}
 		return array();
 	}
+
 	/**
 	 * 将查询的结果作为对象返回，采用sql语句组装器实现要执行的sql语句,，便于实现跨数据库引挚操作
 	 * @param WindSqlBuilder $builder sql语句组装器
@@ -96,6 +97,7 @@ class WindDbTemplate extends WindTemplate {
 		$result = $this->queryByBuilder($builder, $fetch_type);
 		return $this->bindValueToObject(new stdClass(), $result, $colAsProp);
 	}
+
 	/**
 	 * 将查询的结果作为数组返回,采用sql语句组装器实现要执行的sql语句,，便于实现跨数据库引挚操作
 	 * @param WindSqlBuilder $builder sql语句组装器
@@ -105,44 +107,48 @@ class WindDbTemplate extends WindTemplate {
 	public function queryForArrayByBuilder(WindSqlBuilder $builder, $fetch_type = IWindDbConfig::RESULT_ASSOC) {
 		return $this->queryByBuilder($builder, $fetch_type);
 	}
+
 	/**
 	 * 新增操作,由sql语句组装器组装的sql语句操作，便于实现跨数据库引挚操作
 	 * 哎，如果php支持多态那是多好啊
 	 * @param WindSqlBuilder $builder sql语句生成器
 	 * @return boolean;
 	 */
-	public function insertByBuilder(WindSqlBuilder $builder){
+	public function insertByBuilder(WindSqlBuilder $builder) {
 		return $this->write($builder->getInsertSql());
 	}
+
 	/**
 	 * 删除操作,由sql语句组装器组装的sql语句操作，便于实现跨数据库引挚操作
 	 * @param WindSqlBuilder $builder
 	 * @return boolean;
 	 */
-	public function deleteByBuilder(WindSqlBuilder $builder){
+	public function deleteByBuilder(WindSqlBuilder $builder) {
 		return $this->write($builder->getDeleteSql());
 	}
+
 	/**
 	 * 更新操作,由sql语句组装器组装的sql语句操作，便于实现跨数据库引挚操作
 	 * @param WindSqlBuilder $builder
 	 * @return boolean;
 	 */
-	public function updateByBuilder(WindSqlBuilder $builder){
+	public function updateByBuilder(WindSqlBuilder $builder) {
 		return $this->write($builder->getUpdateSql());
 	}
+
 	/**
 	 * 查询操作,由sql语句组装器组装的sql语句操作，便于实现跨数据库引挚操作
 	 * @param WindSqlBuilder $builder
 	 * @param string $fetch_type
 	 * @return array
 	 */
-	public function queryByBuilder(WindSqlBuilder $builder,$fetch_type = IWindDbConfig::RESULT_ASSOC){
+	public function queryByBuilder(WindSqlBuilder $builder, $fetch_type = IWindDbConfig::RESULT_ASSOC) {
 		if (true === ($query = $this->read($builder->getSelectSql()))) {
 			return $this->getConnection()->getAllRow($fetch_type);
 		}
 		return array();
 	}
-	
+
 	/**
 	 * 分布式采取读写分离，从主服务器执行写的操作
 	 * @param string $sql
@@ -151,6 +157,7 @@ class WindDbTemplate extends WindTemplate {
 	public function write($sql) {
 		return $this->getMasterConnection()->query($sql);
 	}
+
 	/**
 	 * 分布式采取读写分离，从从服务器执行读的操作
 	 * @param string $sql
@@ -159,6 +166,7 @@ class WindDbTemplate extends WindTemplate {
 	public function read($sql) {
 		return $this->getSlaveConnection()->query($sql);
 	}
+
 	/**
 	 * 获取分布式数据库适配器管理工厂
 	 * @return WindConnectionManager
@@ -166,7 +174,7 @@ class WindDbTemplate extends WindTemplate {
 	public function getDistributed() {
 		return $this->distributed;
 	}
-	
+
 	/**
 	 * 获取当前数据库操作的适配器
 	 * @return WindDbAdapter
@@ -174,23 +182,23 @@ class WindDbTemplate extends WindTemplate {
 	public function getConnection() {
 		return $this->connection;
 	}
-	
+
 	/**
 	 * 获取主数据库适配器
 	 * @return WindDbAdapter
 	 */
 	public function getMasterConnection() {
-		return  $this->connection = $this->getDistributed()->getMasterConnection();
+		return $this->connection = $this->getDistributed()->getMasterConnection();
 	}
-	
+
 	/**
 	 * 获取从数据库适配器
 	 * @return WindDbAdapter
 	 */
 	public function getSlaveConnection() {
-		return  $this->connection = $this->getDistributed()->getSlaveConnection();
+		return $this->connection = $this->getDistributed()->getSlaveConnection();
 	}
-	
+
 	/**
 	 * 获取当前数据库操作的适配器对应的sql语句组装器
 	 * @return WindSqlBuilder
@@ -198,7 +206,7 @@ class WindDbTemplate extends WindTemplate {
 	public function getSqlBuilder() {
 		return $this->getConnection()->getSqlBuilder();
 	}
-	
+
 	/**
 	 * 将数组格式的访问转化为数组
 	 * @param stdClass $object 对象的初始化
