025009f641b7ae4c7d979496695315ad398d42d0
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-22 02:43:16 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1317 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/dao/AbstractWindDao.php
+++ wind/core/dao/AbstractWindDao.php
@@ -9,37 +9,39 @@
  * @package 
  */
 abstract class AbstractWindDao extends WindModule {
-	
+
 	protected $dbClass = 'WIND:component.db.WindConnectionManager';
-	
+
 	protected $dbConfig = 'WIND:component.db.db_config';
-	
+
 	protected $dbConfigSuffix = 'xml';
-	
+
 	protected $dbDefinition = null;
-	
+
 	protected $cacheClass = 'WIND:component.cache.stored.WindMemcache';
-	
+
 	protected $cacheConfig = 'WIND:component.cache.cache_config';
-	
+
 	protected $cacheConfigSuffix = 'xml';
-	
+
 	protected $cacheDefinition = null;
-	
+
 	protected $isDataCache = true;
+
 	/**
 	 * @var WindConnectionManager 分布式管理与数据库驱动工厂
 	 */
 	protected $dbHandler = null;
-	
+
 	protected $cacheHandler = null;
+
 	/* (non-PHPdoc)
 	 * @see WindModule::getWriteTableForGetterAndSetter()
 	 */
 	protected function getWriteTableForGetterAndSetter() {
-		return array('dbClass', 'dbConfig', 'cacheClass', 'isDataCache', 'dbHandler','cacheConfig','cacheHandler');
+		return array('dbClass', 'dbConfig', 'cacheClass', 'isDataCache', 'dbHandler', 'cacheConfig', 'cacheHandler');
 	}
-	
+
 	/**
 	 * 获得DB类定义
 	 * @return WindComponentDefinition
@@ -57,8 +59,12 @@ abstract class AbstractWindDao extends WindModule {
 		}
 		return $this->dbDefinition;
 	}
-	
-	public function getCacheDefinition(){
+
+	/**
+	 * 获得Cache类定义
+	 * @return WindComponentDefinition
+	 */
+	public function getCacheDefinition() {
 		if ($this->cacheDefinition === null) {
 			L::import('WIND:core.factory.WindComponentDefinition');
 			$definition = new WindComponentDefinition();
@@ -71,7 +77,7 @@ abstract class AbstractWindDao extends WindModule {
 		}
 		return $this->cacheDefinition;
 	}
-	
+
 	/**
 	 * 获得需要缓存的处理的方法名称数组
 	 * 
@@ -80,10 +86,6 @@ abstract class AbstractWindDao extends WindModule {
 	public function getCacheMethods() {
 		return array();
 	}
-	
-	
-	
-	
 
 }
 
