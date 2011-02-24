ba4df2b056e2a235ea5d139193719eafb8ad14d7
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-24 09:35:10 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1384 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/exception/WindActionException.php
@@ -0,0 +1,52 @@
+<?php
+
+L::import('WIND:core.exception.WindException');
+/**
+ * 模板视图异常类
+ *
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+class WindActionException extends WindException {
+
+	private $error;
+
+	/**
+	 * @param WindErrorMessage $error
+	 */
+	public function __construct($error) {
+		$this->setError($error);
+		parent::__construct('');
+	}
+
+	/**
+	 * 自定义异常号的对应异常信息
+	 * 
+	 * @param int $code  异常号
+	 * @return string 返回异常号对应的异常组装信息原型
+	 */
+	protected function messageMapper($code) {
+		$messages = array();
+		
+		return isset($messages[$code]) ? $messages[$code] : '$message';
+	}
+
+	/**
+	 * @return WindErrorMessage $error
+	 */
+	public function getError() {
+		return $this->error;
+	}
+
+	/**
+	 * @param WindErrorMessage $error
+	 */
+	public function setError($error) {
+		$this->error = $error;
+	}
+
+}
+
+?>
