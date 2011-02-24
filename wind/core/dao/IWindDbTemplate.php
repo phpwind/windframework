dbf9d2fbcd8ea4f4c1e4d0f14885a9d7f4889cb9
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-24 09:34:54 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1383 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/dao/IWindDbTemplate.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Enter description here ...
+ *
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+interface IWindDbTemplate {
+	
+	
+}
+
+?>
---
+++ wind/core/dao/WindDbTemplate.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Enter description here ...
+ *
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+class WindDbTemplate {
+
+	private $connection = null;
+	
+	public function find(){
+		$this->connection->query();
+		$this->connection->getSqlBuiler();
+	}
+
+}
+
+?>
