712bd733633759f0a1a6d37665d9ef72ae1e36b3
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-25 11:50:50 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1443 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/viewer/compiler/WindTemplateCompilerScript.php
@@ -0,0 +1,36 @@
+<?php
+
+L::import('WIND:core.viewer.compiler.AbstractWindTemplateCompiler');
+/**
+ * Enter description here ...
+ *
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+class WindTemplateCompilerScript extends AbstractWindTemplateCompiler {
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindTemplateCompiler::compile()
+	 */
+	public function compile($key, $content) {
+		$content = preg_replace_callback('/{\s*\$(\w)+((\-\>\w+)*(\(.*\))*(\[.*\])*)*?[;\s]*}/i', array($this, 
+			'doCompile'), $content);
+		return $content;
+	}
+
+	/**
+	 * 编译匹配到的结果
+	 * @param string $content
+	 */
+	public function doCompile($content) {
+		if (empty($content))
+			return '';
+		$_output = preg_replace(array('/^[\n\s{]+/i', '/[\n\s}\;]+$/i'), array('', ''), $content[0]);
+		return '<?php echo ' . $_output . ';?>';
+	}
+
+}
+
+?>
