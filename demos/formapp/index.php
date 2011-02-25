4879b8fcf74ab84226d9523f84a4eef3e603bee6
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-25 10:58:56 +0000

    formapp调试
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1439 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/formapp/index.php
+++ demos/formapp/index.php
@@ -15,5 +15,5 @@ define('FREAMWORK_PATH', R_P . '/../../wind/');
 /* 缓存文件路径 */
 define('COMPILE_PATH', R_P . 'compile/');
 require_once (FREAMWORK_PATH . '/wind.php');
-
-W::application('FormDemo', R_P . 'config.xml')->run();
+echo R_P . 'config.xml';
+W::application('formdemo', R_P . 'config.xml')->run();
