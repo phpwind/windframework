b0fbd0bd742b2ba2672ea4d78c0d762925a3bb77
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-26 08:55:08 +0000

    组件目录结调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1468 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/utility/WindUtility.php
+++ wind/component/utility/WindUtility.php
@@ -9,7 +9,7 @@
  * @package 
  */
 class WindUtility {
-
+	
 	/**
 	 * 获得随机数字符串
 	 * 
@@ -29,6 +29,39 @@ class WindUtility {
 			}
 		}
 		return $randstr;
+	}
+	
+	/**
+	 * 分页函数
+	 * @param $page		当前页
+	 * @param $total	总页数
+	 * @param $url		连接地址
+	 * @param $ajaxCallBack
+	 * @return string
+	 */
+	static public function getPages($page, $total, $url, $ajaxCallBack = '') {
+		if (1 >= $total || !is_numeric($page)) {
+			return '';
+		}
+		$ajaxurl = $ajaxCallBack ? " onclick=\"return $ajaxCallBack(this.href);\"" : '';
+		list($url, $mao) = explode('#', $url);
+		$mao && $mao = '#' . $mao;
+		$pages = "<div class=\"pages\"><a href=\"{$url}page=1$mao\"{$ajaxurl}>&laquo;</a>";
+		for ($i = $page - 3; $i <= $page - 1; $i++) {
+			if ($i < 1) continue;
+			$pages .= "<a href=\"{$url}page=$i$mao\"{$ajaxurl}>$i</a>";
+		}
+		$pages .= "<b>$page</b>";
+		if ($page < $total) {
+			$flag = 0;
+			for ($i = $page + 1; $i <= $total; $i++) {
+				$pages .= "<a href=\"{$url}page=$i$mao\"{$ajaxurl}>$i</a>";
+				$flag++;
+				if ($flag == 4) break;
+			}
+		}
+		$pages .= "<a href=\"{$url}page=$total$mao\"{$ajaxurl}>&raquo;</a><div class=\"fl\">共{$total}页</div><span class=\"pagesone\"><input type=\"text\" size=\"3\" onkeydown=\"javascript: if(event.keyCode==13){var page=(this.value>$total) ? $total : this.value; " . ($ajaxurl ? "$ajaxCallBack('{$url}page='+page);" : " location='{$url}page='+page+'{$mao}';") . " return false;}\"><button onclick=\"javascript: var page=(this.previousSibling.value>$total) ? $total : this.previousSibling.value; " . ($ajaxurl ? "$ajaxCallBack('{$url}page='+page);" : " location='{$url}page='+page+'{$mao}';") . " return false;\">Go</button></span></div>";
+		return $pages;
 	}
 
 }
