60f0d7358e6ee35650c921c59aa84a6c50dfaf23
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-02-28 11:52:53 +0000

    dbTemplate修改
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1526 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/dao/IWindDbTemplate.php
+++ wind/core/dao/IWindDbTemplate.php
@@ -12,6 +12,7 @@ interface IWindDbTemplate {
 	/**
 	 * 单条查询
 	 * 
+	 * @param string $tableName 数据库表明
 	 * @param array $condition
 	 * array(
 	 * 'field' => '*' 查询的字段
@@ -22,13 +23,14 @@ interface IWindDbTemplate {
 	 * 'havingValue' => array(),  having条件中的变量值
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * )
-	 * @param array $config	独立配置信息
 	 * @return array
 	 */
-	public function find($condition = array(), $table = '', $config = array());
+	public function find($tableName, $condition = array());
    
 	/**
 	 * 查询多条
+	 * 
+	 * @param string $tableName 数据库表名
 	 * @param array $condition
 	 * array(
 	 * 'field' => '*' 查询的字段
@@ -42,35 +44,33 @@ interface IWindDbTemplate {
 	 * 'offset'=> '' 和limit配合使用
 	 * )
 	 * @param bool $ifCount	是否需要统计总个数
-	 * @param array $config	数据库独立配置信息
 	 * @return array() | array($result,$count)
 	 */
-	public function findAll($condition = array(), $ifCount = false, $table = '', $config = array());
+	public function findAll($tableName, $condition = array(), $ifCount = false);
 	
 	/**
 	 * 通过某一字段查询
 	 * @param string $table 查询的数据表
 	 * @param string $filed	字段名
 	 * @param string $value	该字段的值
-	 * @param array $config	独立配置信息
 	 * @return array
 	 */
-	public function findByField($filed, $value, $table = '', $config = array());
+	public function findByField($tableName, $filed, $value) ;
 	
 	/**
 	 * 插入一条数据
 	 * 
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	插入的数据
 	 * @param array $field	相关的字段（可选）
 	 * @return bool
 	 */
-	public function insert($data, $field = array(), $table = '') ;
+	public function insert($tableName, $data, $field = array()) ;
    
 	/**
 	 * 删除数据
 	 * 
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param srting $condition	更新的条件
 	 * array(
 	 * 'where' => '',  查询的条件
@@ -78,32 +78,35 @@ interface IWindDbTemplate {
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * 'limit' => '' 查询的数量
 	 * )
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function delete($condition, $table = '');
+	public function delete($tableName, $condition, $isGetAffectedRows = false);
 	
 	/**
 	 * 通过某个字段删除数据
 	 * 
-	 * @param string $table 删除的数据表
+	 * @param string $tableName 删除的数据表
 	 * @param string $filed	所依据的字段名
 	 * @param string $value 该字段的值
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function deleteByField($filed, $value, $table = '');
+	public function deleteByField($tableName, $filed, $value, $isGetAffectedRows = false);
 	
 	/**
 	 * 更新一条数据
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	更新的数据
 	 * @param array $field	相关的字段（可选）
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function replace($data, $field = array(), $table = '');
+	public function replace($tableName, $data, $field = array(), $isGetAffectedRows = false);
     
 	/**
 	 * 更新数据
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	更新的数据
 	 * @param srting $condition	更新的条件
 	 * array(
@@ -112,32 +115,34 @@ interface IWindDbTemplate {
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * 'limit' => '' 查询的数量
 	 * )
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function update($data, $condition = array(), $table = '');
+	public function update($tableName, $data, $condition = array(), $isGetAffectedRows = false);
 	
 	/**
 	 * 通过字段更新
 	 * 
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	更新的数据
 	 * @param string $filed	条件字段
 	 * @param string $value	该字段的值
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool 
 	 */
-	public function updateByField($data, $filed, $value, $table = '');
+	public function updateByField($tableName, $data, $filed, $value, $isGetAffectedRows = false);
     
 	/**
 	 * 执行一条sql语句
 	 * @param string $sql	sql语句
-	 * @param string $type	主从选项  支持两个参数：slave 和  master
-	 * @param array $config	独立配置信息
-	 * @return resource|bool  数据库句柄
+	 * @return array|bool  数据库句柄
 	 */
-	public function query($sql, $config = array()) ;
+	public function query($sql) ;
 	
 	/**
 	 * 统计个数
+	 * 
+	 * @param string $tableName 数据库表
 	 * @param array $condition
 	 * array(
 	 * 'field' => '*' 查询的字段
@@ -148,10 +153,9 @@ interface IWindDbTemplate {
 	 * 'havingValue' => array(),  having条件中的变量值
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * )
-	 * @param array $config	独立配置信息
 	 * @return int
 	 */
-	public function count($condition, $table = '', $config = array());
+	public function count($tableName, $condition);
 
 	
 	/**
--- wind/core/dao/dbtemplate/WindConnectionManagerBasedDbTemplate.php
+++ wind/core/dao/dbtemplate/WindConnectionManagerBasedDbTemplate.php
@@ -20,37 +20,13 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	private $connectionManager = null;
 
 	/**
-	 * 设置查询的表名
-	 * 
-	 * @var string
-	 */
-	private $tableName = '';
-
-	/**
-	 * 获得数据库链接管理
-	 * @return WindConnectionManager
-	 */
-	private function getConnectionManager() {
-		return $this->connectionManager;
-	}
-
-	/**
 	 * 设置数据库链接管理
 	 * @param WindConnectionManager $connection
 	 */
-	public function setConnection($connection) {
-		$this->connectionManager = $connection;
+	public function setConnection($connectionManager) {
+		$this->connectionManager = $connectionManager;
 	}
-    
-	/**
-	 * 添加一个链接
-	 *
-	 * @param unknown_type $connection
-	 */
-	public function addConnection($connection) {
-		//TODO
-	}
-	
+
 	/**
 	 * 获得数据库链接
 	 * @return WindDbAdapter $connection
@@ -58,38 +34,23 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	public function getConnection() {
 		return $this->connectionManager;
 	}
-    
+
 	public function getDbHandler() {
 		return $this->connectionManager->getConnection();
-	}
-	/**
-	 * @return the $tableName
-	 */
-	public function getTableName() {
-		return $this->tableNameName;
-	}
-
-	/**
-	 * @param field_type $tableName
-	 */
-	public function setTableName($tableName) {
-		$this->tableNameName = $tableName;
 	}
 
 	/**
 	 * 执行一条sql语句
 	 * @param string $sql	sql语句
-	 * @param string $type	主从选项  支持两个参数：slave 和  master
-	 * @param array $config	独立配置信息
-	 * @return resource|bool  数据库句柄
+	 * @return array|bool  数据库句柄
 	 */
-	public function query($sql, $config = array()) {
+	public function query($sql) {
 		return $this->getDbHandler()->query($sql);
 	}
 
 	/**
 	 * 更新数据
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	更新的数据
 	 * @param srting $condition	更新的条件
 	 * array(
@@ -98,65 +59,65 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * 'limit' => '' 查询的数量
 	 * )
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function update($data, $condition = array(), $table = '') {
+	public function update($tableName, $data, $condition = array(), $isGetAffectedRows = false) {
 		$condition = $this->cookCondition($condition);
-		$table = trim($table) ? trim($table) : $this->tableNameName;
 		$db = $this->getDbHandler();
-		$db->getSqlBuilder()->from($table)->set($data)->where($condition['where'], $condition['whereValue'])->order($condition['order'])->limit($condition['limit'])->update();
-		return $db->getAffectedRows();
+		$result = $db->getSqlBuilder()->from($tableName)->set($data)->where($condition['where'], $condition['whereValue'])->order($condition['order'])->limit($condition['limit'])->update();
+		return $isGetAffectedRows ? $db->getAffectedRows() : $result;
 	}
 
 	/**
 	 * 通过字段更新
 	 * 
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	更新的数据
 	 * @param string $filed	条件字段
 	 * @param string $value	该字段的值
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool 
 	 */
-	public function updateByField($data, $filed, $value, $table = '') {
+	public function updateByField($tableName, $data, $filed, $value, $isGetAffectedRows = false) {
 		if (!$this->checkFiled($filed)) return false;
-		return $this->update($data, array('where' => "$filed = ?", 'whereValue' => $value), $table);
+		return $this->update($tableName, $data, array('where' => "$filed = ?", 'whereValue' => $value), $isGetAffectedRows);
 	}
 
 	/**
 	 * 插入一条数据
 	 * 
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	插入的数据
 	 * @param array $field	相关的字段（可选）
 	 * @return bool
 	 */
-	public function insert($data, $field = array(), $table = '') {
+	public function insert($tableName, $data, $field = array()) {
 		empty($field) && list($field, $data) = $this->parseData($data);
-		$table = trim($table) ? trim($table) : $this->tableName;
 		$db = $this->getDbHandler();
-		$db->getSqlBuilder()->from($table)->field($field)->data($data)->insert();
+		$db->getSqlBuilder()->from($tableName)->field($field)->data($data)->insert();
 		return $db->getLastInsertId();
 	}
 
 	/**
 	 * 更新一条数据
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param array $data	更新的数据
 	 * @param array $field	相关的字段（可选）
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function replace($data, $field = array(), $table = '') {
+	public function replace($tableName, $data, $field = array(), $isGetAffectedRows = false) {
 		empty($field) && list($field, $data) = $this->parseData($data);
-		$table = trim($table) ? trim($table) : $this->tableName;
 		$db = $this->getDbHandler();
-		$db->getSqlBuilder()->from($table)->field($field)->data($data)->replace();
-		return $db->getAffectedRows();
+		$result = $db->getSqlBuilder()->from($tableName)->field($field)->data($data)->replace();
+		return $isGetAffectedRows ? $db->getAffectedRows() : $result;
 	}
 
 	/**
 	 * 删除数据
 	 * 
-	 * @param string $table 更新的数据表
+	 * @param string $tableName 更新的数据表
 	 * @param srting $condition	更新的条件
 	 * array(
 	 * 'where' => '',  查询的条件
@@ -164,32 +125,34 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * 'limit' => '' 查询的数量
 	 * )
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function delete($condition, $table = '') {
+	public function delete($tableName, $condition, $isGetAffectedRows = false) {
 		$condition = $this->cookCondition($condition);
-		$table = trim($table) ? trim($table) : $this->tableName;
 		$db = $this->getDbHandler();
-		$db->getSqlBuilder()->from($table)->where($condition['where'], $condition['whereValue'])->order($condition['order'])->limit($condition['limit'])->delete();
-		return $db->getAffectedRows();
+		$result = $db->getSqlBuilder()->from($tableName)->where($condition['where'], $condition['whereValue'])->order($condition['order'])->limit($condition['limit'])->delete();
+		return $isGetAffectedRows ? $db->getAffectedRows() : $result;
 	}
 
 	/**
 	 * 通过某个字段删除数据
 	 * 
-	 * @param string $table 删除的数据表
+	 * @param string $tableName 删除的数据表
 	 * @param string $filed	所依据的字段名
 	 * @param string $value 该字段的值
+	 * @param return $isGetAffectedRows 是否返回影响行数
 	 * @return bool
 	 */
-	public function deleteByField($filed, $value, $table = '') {
+	public function deleteByField($tableName, $filed, $value, $isGetAffectedRows = false) {
 		if (!$this->checkFiled($filed)) return array();
-		return $this->delete(array('where' => "$filed = ?", 'whereValue' => $value), $table);
+		return $this->delete($tableName, array('where' => "$filed = ?", 'whereValue' => $value), $isGetAffectedRows);
 	}
 
 	/**
 	 * 单条查询
 	 * 
+	 * @param string $tableName 数据库表明
 	 * @param array $condition
 	 * array(
 	 * 'field' => '*' 查询的字段
@@ -200,14 +163,12 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	 * 'havingValue' => array(),  having条件中的变量值
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * )
-	 * @param array $config	独立配置信息
 	 * @return array
 	 */
-	public function find($condition = array(), $table = '', $config = array()) {
+	public function find($tableName, $condition = array()) {
 		$condition = $this->cookCondition($condition);
-		$table = trim($table) ? trim($table) : $this->tableName;
 		$db = $this->getDbHandler();
-		$query = $db->getSqlBuilder()->from($table)->field($condition['field'])->where($condition['where'], $condition['whereValue'])->group($condition['group'])->having($condition['having'], $condition['havingValue'])->order($condition['order'])->limit(1)->select();
+		$query = $db->getSqlBuilder()->from($tableName)->field($condition['field'])->where($condition['where'], $condition['whereValue'])->group($condition['group'])->having($condition['having'], $condition['havingValue'])->order($condition['order'])->limit(1)->select();
 		return $db->getRow($query);
 	}
 
@@ -216,16 +177,17 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	 * @param string $table 查询的数据表
 	 * @param string $filed	字段名
 	 * @param string $value	该字段的值
-	 * @param array $config	独立配置信息
 	 * @return array
 	 */
-	public function findByField($filed, $value, $table = '', $config = array()) {
+	public function findByField($tableName, $filed, $value) {
 		if (!$this->checkFiled($filed)) return array();
-		return $this->find(array('where' => "$filed = ?", 'whereValue' => $value), $table, $config);
+		return $this->find($tableName, array('where' => "$filed = ?", 'whereValue' => $value));
 	}
 
 	/**
 	 * 查询多条
+	 * 
+	 * @param string $tableName 数据库表名
 	 * @param array $condition
 	 * array(
 	 * 'field' => '*' 查询的字段
@@ -239,22 +201,22 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	 * 'offset'=> '' 和limit配合使用
 	 * )
 	 * @param bool $ifCount	是否需要统计总个数
-	 * @param array $config	数据库独立配置信息
 	 * @return array() | array($result,$count)
 	 */
-	public function findAll($condition = array(), $ifCount = false, $table = '', $config = array()) {
+	public function findAll($tableName, $condition = array(), $ifCount = false) {
 		$condition = $this->cookCondition($condition);
-		$table = trim($table) ? trim($table) : $this->tableName;
 		$db = $this->getDbHandler();
-		$query = $db->getSqlBuilder()->from($table)->field($condition['field'])->where($condition['where'], $condition['whereValue'])->group($condition['group'])->having($condition['having'], $condition['havingValue'])->order($condition['order'])->limit($condition['limit'], $condition['offset'])->select();
+		$query = $db->getSqlBuilder()->from($tableName)->field($condition['field'])->where($condition['where'], $condition['whereValue'])->group($condition['group'])->having($condition['having'], $condition['havingValue'])->order($condition['order'])->limit($condition['limit'], $condition['offset'])->select();
 		$result = $db->getAllRow();
 		if (!$ifCount) return $result;
-		$count = $this->count($condition, $table, $config);
+		$count = $this->count($tableName, $condition);
 		return array($result, $count);
 	}
 
 	/**
 	 * 统计个数
+	 * 
+	 * @param string $tableName 数据库表
 	 * @param array $condition
 	 * array(
 	 * 'field' => '*' 查询的字段
@@ -265,13 +227,12 @@ class WindConnectionManagerBasedDbTemplate implements IWindDbTemplate {
 	 * 'havingValue' => array(),  having条件中的变量值
 	 * 'order' => array(), 排序类型  默认是降序排列，支持多字段排序  array('id'=>true,'name'=>false)
 	 * )
-	 * @param array $config	独立配置信息
 	 * @return int
 	 */
-	public function count($condition, $table = '', $config = array()) {
+	public function count($tableName, $condition) {
 		$condition = $this->cookCondition($condition);
 		$condition['field'] = ' COUNT(*) as total';
-		$result = $this->find($condition, $table, $config);
+		$result = $this->find($tableName, $condition);
 		return (int) $result['total'];
 	}
 
