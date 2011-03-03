9f2eee36e9c1a8dbac532af5807048d64f788e15
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-03 12:24:47 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1597 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/web/listener/WindFormListener.php
+++ wind/core/web/listener/WindFormListener.php
@@ -34,27 +34,23 @@ class WindFormListener extends WindHandlerInterceptor {
 	public function preHandle() {
 		//TODO 实现基于表单对象的表单验证机制，错误处理，表单对象集成WindEnableValidateModule
 		$className = L::import($this->formPath);
-		if (!class_exists($className)) throw new WindException('the form <' . $this->formPath . '> is not exists!');
+		if (!class_exists($className)) throw new WindException('the form \'' . $this->formPath . '\' is not exists!');
 		$form = new $className();
 		$methods = get_class_methods($form);
 		foreach ($methods as $method) {
-		    $tmp = strtolower($method);
-		    if (($pos = strpos($tmp, 'set')) !== 0 ) continue;
-		    $tmp = substr($tmp, 3);
-		    $value = $this->request->getPost($tmp) ? $this->request->getPost($tmp) : $this->request->getGet($tmp);
-		    if ($value === null) continue;
-		    call_user_func_array(array($form, $method), array($value));
+			$_tmp = strtolower($method);
+			if (($pos = strpos($_tmp, 'set')) !== 0) continue;
+			$_tmp = substr($_tmp, 3);
+			$value = $this->request->getPost($_tmp) ? $this->request->getPost($_tmp) : $this->request->getGet($_tmp);
+			if ($value === null) continue;
+			call_user_func_array(array($form, $method), array($value));
 		}
 		call_user_func_array(array($form, 'validate'), array($form));
 		if (($error = $form->getErrors())) {
-		    $this->errorMessage = new WindErrorMessage($error);
-		    $this->errorMessage->sendError();
+			$this->errorMessage = new WindErrorMessage($error);
+			$this->errorMessage->sendError();
 		}
 		$this->request->setAttribute('formData', $form);
-	}
-	
-	private function getParentClass($form) {
-	    
 	}
 
 	/* (non-PHPdoc)
