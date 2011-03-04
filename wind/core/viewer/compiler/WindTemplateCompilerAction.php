88bc1d4df2421fc76c22db2cf6aabc15c66371a6
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-04 11:14:50 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1635 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/viewer/compiler/WindTemplateCompilerAction.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Qiong Wu <papa0924@gmail.com> 2010-11-2
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2010 phpwind.com
+ * @license
+ */
+
+L::import('WIND:core.viewer.AbstractWindTemplateCompiler');
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+class WindTemplateCompilerAction extends AbstractWindTemplateCompiler {
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindTemplateCompiler::compile()
+	 */
+	public function compile($key, $content) {
+		
+	}
+
+}
