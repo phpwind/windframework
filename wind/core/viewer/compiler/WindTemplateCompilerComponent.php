f45902def5cc1f989705de671428be4e3df25d48
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-09 13:24:56 +0000

    增加模板变量管理
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1752 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/viewer/compiler/WindTemplateCompilerComponent.php
+++ wind/core/viewer/compiler/WindTemplateCompilerComponent.php
@@ -9,11 +9,43 @@ L::import('WIND:core.viewer.AbstractWindTemplateCompiler');
  */
 class WindTemplateCompilerComponent extends AbstractWindTemplateCompiler {
 
+	protected $name = '';
+
+	protected $app = '';
+
+	protected $args = '';
+
+	protected $tpl = '';
+
 	/* (non-PHPdoc)
 	 * @see AbstractWindTemplateCompiler::compile()
 	 */
 	public function compile($key, $content) {
+		$_output .= $this->getScript() . '<iframe src="http://t4.pw.com/comment/index.php?commonid=1&topicid=1" frameborder="0" scrolling="auto"></iframe>';
+		return $_output;
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getScript() {
+		$_tmp = '';
 		
+		return $_tmp;
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getTplContent() {
+		
+	}
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindTemplateCompiler::getProperties()
+	 */
+	protected function getProperties() {
+		return array('name', 'app', 'args', 'tpl');
 	}
 }
 
