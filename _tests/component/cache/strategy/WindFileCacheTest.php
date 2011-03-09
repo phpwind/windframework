f182dab3e619d2a449ec2aeb8e2655022eb78390
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-09 06:05:46 +0000

    工具库重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1729 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/component/cache/strategy/WindFileCacheTest.php
+++ _tests/component/cache/strategy/WindFileCacheTest.php
@@ -32,72 +32,60 @@ class WindFileCacheTest extends BaseTestCase{
 	public function requireFile() {
 		require_once ('component/cache/stored/WindFileCache.php');
 	}
-	
-	public function testAdd(){
-		$this->assertTrue(false !== $this->fileCache->add('key','value'));
-		$this->assertEquals('value',$this->fileCache->fetch('key'));
-		$this->assertTrue($this->fileCache->delete('key'));
-		$this->assertTrue(false !== $this->fileCache->add('newKey','newValue',1));
-	}
+
 	
 	public function testSet(){
-		$this->assertTrue(false !== $this->fileCache->add('key','value'));
+		$this->assertTrue(false !== $this->fileCache->set('key','value'));
 		$this->assertEquals('value',$this->fileCache->fetch('key'));
 		$this->assertTrue(false !== $this->fileCache->set('key','newValue'));
-		$this->assertEquals('newValue',$this->fileCache->fetch('key'));
+		$this->assertEquals('newValue',$this->fileCache->get('key'));
 		$this->assertTrue($this->fileCache->delete('key'));
 		$this->assertTrue(false !== $this->fileCache->set('newkey','newValue',1));
-		$this->assertEquals('newValue',$this->fileCache->fetch('newkey'));
+		$this->assertEquals('newValue',$this->fileCache->get('newkey'));
 	}
 	
-	public function testReplace(){
-		$this->assertTrue(false !== $this->fileCache->add('key','value'));
-		$this->assertEquals('value',$this->fileCache->fetch('key'));
-		$this->assertTrue(false !== $this->fileCache->replace('key','newValue'));
-		$this->assertEquals('newValue',$this->fileCache->fetch('key'));
-		$this->assertTrue($this->fileCache->delete('key'));
+	
+	public function testget(){
+		$this->assertTrue(false !== $this->fileCache->set('key','value',1));
+		$this->assertEquals('value',$this->fileCache->get('key'));
 	}
 	
-	public function testFetch(){
-		$this->assertTrue(false !== $this->fileCache->add('key','value',1));
-		$this->assertEquals('value',$this->fileCache->fetch('key'));
-	}
-	
-	public function testBatchFetch(){
-		$this->assertTrue(false !== $this->fileCache->add('name','phpwind',1));
-		$this->assertTrue(false !== $this->fileCache->add('age','100',1));
-		$result = $this->fileCache->batchFetch(array('name','age'));
+	public function testBatchget(){
+		$this->assertTrue(false !== $this->fileCache->set('name','phpwind',1));
+		$this->assertTrue(false !== $this->fileCache->set('age','100',1));
+		$result = $this->fileCache->batchGet(array('name','age'));
 		$this->assertTrue(2 === count($result) && 'phpwind' === $result['name'] && '100' === $result['age']);
 		$this->assertTrue($this->fileCache->batchDelete(array('name','age')));
 	}
 	
 	public function testDelete(){
-		$this->assertTrue(false !== $this->fileCache->add('name','phpwind'));
+		$this->assertTrue(false !== $this->fileCache->set('name','phpwind'));
 		$this->assertTrue($this->fileCache->delete('name'));
 	}
 	
 	public function testBatchDelete(){
-		$this->assertTrue(false !== $this->fileCache->add('name','phpwind',1));
-		$this->assertTrue(false !== $this->fileCache->add('age','100',1));
+		$this->assertTrue(false !== $this->fileCache->set('name','phpwind',1));
+		$this->assertTrue(false !== $this->fileCache->set('age','100',1));
 		$this->assertTrue($this->fileCache->batchDelete(array('name','age')));
 	}
 	
 	public function testFlush(){
-		$this->assertTrue(false !== $this->fileCache->add('one','ones'));
-		$this->assertTrue(false !== $this->fileCache->add('two','twos'));
-		$this->assertTrue(false !== $this->fileCache->add('three','threes'));
+		$this->assertTrue(false !== $this->fileCache->set('one','ones'));
+		$this->assertTrue(false !== $this->fileCache->set('two','twos'));
+		$this->assertTrue(false !== $this->fileCache->set('three','threes'));
 		$this->assertTrue($this->fileCache->flush());
-		$this->assertTrue($this->fileCache->add('one','ones',1));
+		$this->assertTrue($this->fileCache->set('one','ones',1));
 	}
 	
 	public function testMutiLevels(){
 		$config[WindFileCache::CACHEDIR] = dirname(dirname(dirname(dirname(__FILE__)))).'/data/cache/file/';
 		$config[WindFileCache::LEVEL] = 2;
 		$fileCache = new WindFileCache($config);
-		$this->assertTrue(false !== $fileCache->add('school','alibaba'));
-		$this->assertTrue(false !== $fileCache->add('bbs','phpwind'));
+		$this->assertTrue(false !== $fileCache->set('school','alibaba'));
+		$this->assertTrue(false !== $fileCache->set('bbs','phpwind'));
 		$this->assertTrue(false !== $fileCache->set('colleage','beida'));
-		$this->assertEquals('alibaba',$fileCache->fetch('school'));
+		$this->assertEquals('alibaba',$fileCache->get('school'));
 		$this->assertTrue($fileCache->flush());
+		
 	}
 }
