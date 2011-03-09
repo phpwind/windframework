f4c698a2d7d2f8b2e42ad7d85550064980c0bcdd
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-09 05:51:17 +0000

    NEW - bug 112: PHP Task: TODO 实现基于验证规则的表单验证机制，错误处理 http://bugs.phpwind-inc.com/show_bug.cgi?id=112 将构造规则转移到windutility工具类中
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1726 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/WindEnableValidateModule.php
+++ wind/core/WindEnableValidateModule.php
@@ -47,20 +47,6 @@ abstract class WindEnableValidateModule extends WindModule {
 	}
 
 	/**
-	 * 组装测试规则
-	 * @param string $field	| 验证字段名称
-	 * @param string $validator | 验证方法
-	 * @param array $args | 参数
-	 * @param string $default	| 默认值
-	 * @param string $message	| 错误信息
-	 * @return array
-	 */
-	protected function buildValidateRule($field, $validator, $args = array(), $default = null, $message = '') {
-		return array('field' => $field, 'validator' => $validator, 'args' => (array) $args, 'default' => $default, 
-			'message' => ($message ? $message : $this->_defaultMessage));
-	}
-
-	/**
 	 * 验证方法
 	 * 
 	 * @param array|WindModule $input
