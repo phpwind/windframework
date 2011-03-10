6faf9c86ba63a5a8f1b731421fe78e08168bf9c9
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-10 06:15:47 +0000

    工具库重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1763 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/component/utility/WindPackTest.php
+++ _tests/component/utility/WindPackTest.php
@@ -5,8 +5,6 @@
  * @copyright Copyright &copy; 2003-2110 phpwind.com
  * @license 
  */
-
-
 require_once ('component/utility/WindPack.php');
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
@@ -14,14 +12,14 @@ require_once ('component/utility/WindPack.php');
  * @version $Id$ 
  * @package 
  */
-class WindPackTest extends PHPUnit_Framework_TestCase {
+class WindPackTest extends BaseTestCase {
 	private $pack = null;
 	private $path = '';
 	
 	public function init() {
 		if (null === $this->pack) {
 			$this->pack = new WindPack();
-			$this->path = $this->pack->realDir($this->pack->getFilePath(dirname(dirname(__FILE__))));
+			$this->path = dirname(dirname(dirname(__FILE__))).'/';
 		}
 	}
 	
@@ -44,7 +42,7 @@ class WindPackTest extends PHPUnit_Framework_TestCase {
 	 * @dataProvider providerPachMethod
 	 */
 	public function testPackFromList($packMethod,$compress){
-		$fileList = array(__FILE__=>$this->pack->getFileName(__FILE__));
+		$fileList = array(__FILE__=>__FILE__);
 		$dst = $this->path."data/compile/file_{$packMethod}_{$compress}.php";
 		$result = $this->pack->packFromFileList($fileList, $dst, $packMethod, $compress);
 		$this->assertTrue($result && is_file($dst));
@@ -53,14 +51,14 @@ class WindPackTest extends PHPUnit_Framework_TestCase {
 	 * @dataProvider providerPachMethod
 	 */
 	public function testPackFromDir($packMethod,$compress){
-		$dir = $this->path.'component/db/';
+		$dir = $this->path.'component/utility/date';
 		$dst = $this->path."data/compile/dir_{$packMethod}_{$compress}.php";
 		$result = $this->pack->packFromDir($dir, $dst, $packMethod, $compress);
 		$this->assertTrue($result && is_file($dst));
 	}
 	
 	public function testPackFromFileByCallBack(){
-		$fileList = array(__FILE__=>$this->pack->getFileName(__FILE__));
+		$fileList = array(__FILE__=>__FILE__);
 		$dst = $this->path."data/compile/file_callback_pack.php";
 		$this->pack->setContentInjectionCallBack(array($this, 'callback'));
 		$result = $this->pack->packFromFileList($fileList, $dst, WindPack::STRIP_PHP, true);
