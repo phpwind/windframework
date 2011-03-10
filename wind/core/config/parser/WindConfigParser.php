e5fab5150b9525c38adfcd26b710b7a9629794bc
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-10 04:28:02 +0000

    NEW - bug 119:PHP Task: TODO 删除 $windConfig 默认定义的相关处理 http://bugs.phpwind-inc.com/show_bug.cgi?id=119
    重构configParse类
    
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1761 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/config/parser/WindConfigParser.php
+++ wind/core/config/parser/WindConfigParser.php
@@ -21,8 +21,6 @@ L::import('WIND:core.config.parser.IWindConfigParser');
  */
 class WindConfigParser implements IWindConfigParser {
 
-	const ISMERGE = 'isMerge';
-
 	/**
 	 * 配置文件支持的格式白名单
 	 */
@@ -37,70 +35,17 @@ class WindConfigParser implements IWindConfigParser {
 	const WIND_ROOT = 'wind';
 
 	/**
-	 * 框架缺省配置文件的名字
-	 * @var string $defaultConfig 
-	 */
-	private $windConfig = 'WIND:wind_config';
-
-	/**
 	 * 配置解析对象队列
 	 * @var array object $configParser
 	 */
 	private $configParsers = array();
 
 	/**
-	 * 配置文件解析出来的数据编码
-	 * @var string $encoding
-	 */
-	private $encoding = 'UTF-8';
-
-	/**
-	 * ISMERGE的有效值范围
-	 * @var array $mergeValue
-	 */
-	private $mergeValue = array('true', '1');
-
-	/**
 	 * 初始化
 	 * 设置解析数据输出的编码方式
 	 * @param String $outputEncoding	
 	 */
-	public function __construct($outputEncoding = 'UTF-8') {
-		//TODO 删除 $windConfig 默认定义的相关处理
-		$this->encoding = $outputEncoding;
-	}
-
-	/**
-	 * 解析文件，保存缓存，返回解析结果
-	 * 
-	 * 1、缺省的配置文件，采用XML格式解析返回
-	 * 2、如果输入的配置文件格式没有提供支持，则抛出异常
-	 * 3、根据格式进行解析
-	 * 4、参数三$isApp 用来配置该解析式组件格式的解析还是应用配置的解析，
-	 * 如果是应用解析需要进行merge操作，如果是组件解析则不用
-	 * 
-	 * @param string $name       解析后保存的文件名字
-	 * @param string $configPath 待解析文件的绝对路径
-	 * @param boolean $isApp     判断是应用解析还是组件配置解析
-	 * @return array             解析成功返回的数据
-	 */
-	public function parseConfig($configPath = '', $alias = '') {
-		$alias = trim($alias);
-		$cacheFile = '';
-		$config = array();
-		if ($alias) {
-			$cacheFile = $this->buildCacheFilePath($alias);
-			$config = $this->getCacheContent($cacheFile);
-			if (isset($config[self::WIND_ROOT]) && !$this->needCompiled())
-				return $config[self::WIND_ROOT];
-		}
-		$config = (array) $config;
-		$configPath = trim($configPath);
-		$parseFormat = $this->getConfigFormat($configPath);
-		$config[self::WIND_ROOT] = $this->doParser($configPath, $parseFormat);
-		$config[self::WIND_ROOT] = $this->mergeConfig($this->doParser($this->getWindConfigPath($parseFormat), $parseFormat), $config[self::WIND_ROOT]);
-		$alias && $this->saveConfigFile($cacheFile, $config);
-		return $config[self::WIND_ROOT];
+	public function __construct() {
 	}
 
 	/**
@@ -139,7 +84,7 @@ class WindConfigParser implements IWindConfigParser {
 		if (!$alias)
 			return $result;
 		$config[$alias] = $result;
-		$append && $this->saveConfigFile($cacheFileName, $config);
+		$this->saveConfigFile($cacheFileName, $config);
 		return $result;
 	}
 
@@ -165,7 +110,7 @@ class WindConfigParser implements IWindConfigParser {
 		switch ($type) {
 			case self::CONFIG_XML:
 				L::import("WIND:component.parser.WindXmlParser");
-				return new WindXmlParser('1.0', $this->encoding);
+				return new WindXmlParser();
 				break;
 			case self::CONFIG_INI:
 				L::import("WIND:component.parser.WindIniParser");
@@ -204,61 +149,6 @@ class WindConfigParser implements IWindConfigParser {
 		return $this->configParsers[$type]->parse($configFile);
 	}
 
-	/**
-	 * 合并配置文件
-	 * 
-	 * 如果应用配置中没有配置相关选项，则使用默认配置中的选项
-	 * 如果是需要合并的项，则将缺省项和用户配置项进行合并，合并规则为用户配置优先级大于缺省配置
-	 * 
-	 * @param array $defaultConfig  默认的配置文件
-	 * @param array $appConfig 		应用的配置文件
-	 * @return array  				合并后的配置
-	 */
-	private function mergeConfig($defaultConfig, $appConfig) {
-		if (!$defaultConfig || !is_array($defaultConfig))
-			return array();
-		list($defaultConfig, $mergeTags) = $this->getMergeTags($defaultConfig);
-		
-		if (!$appConfig || !is_array($appConfig))
-			return $defaultConfig;
-		list($appConfig) = $this->getMergeTags($appConfig);
-		
-		foreach ($defaultConfig as $key => $value) {
-			if (in_array($key, $mergeTags) && isset($appConfig[$key])) {
-				$defaultConfig[$key] = array_merge((array) $defaultConfig[$key], (array) $appConfig[$key]);
-				continue;
-			}
-			if (isset($appConfig[$key])) {
-				$defaultConfig[$key] = $appConfig[$key];
-				continue;
-			}
-		}
-		
-		if (!($difKeys = array_diff(array_keys($appConfig), array_keys($defaultConfig))))
-			return $defaultConfig;
-		foreach ($difKeys as $key) {
-			$defaultConfig[$key] = $appConfig[$key];
-		}
-		return $defaultConfig;
-	}
-
-	/**
-	 * 获得isMerge属性的标签，同时将该属性删除
-	 * 
-	 * @param array $parames     需要查找的数组
-	 * @return array 			(处理后的数组，含有merge属性的标签集合)
-	 */
-	private function getMergeTags($params) {
-		$mergeTags = array();
-		foreach ($params as $key => $value) {
-			if (is_array($value) && isset($value[self::ISMERGE])) {
-				($this->checkCanMerge($value[self::ISMERGE])) && $mergeTags[] = $key;
-				$params[$key][self::ISMERGE] = null;
-				unset($params[$key][self::ISMERGE]);
-			}
-		}
-		return array($params, $mergeTags);
-	}
 
 	/**
 	 * 返回是否需要执行解析
@@ -297,16 +187,6 @@ class WindConfigParser implements IWindConfigParser {
 	}
 
 	/**
-	 * 判断数值是否在IsMerge的可用范围内
-	 * 
-	 * @param string $value 需要判断的字串
-	 * @return boolean  	返回判断结果
-	 */
-	private function checkCanMerge($value) {
-		return in_array(strtolower(trim($value)), $this->mergeValue);
-	}
-
-	/**
 	 * 保存成文件
 	 * 
 	 * @param string $filename   保存的文件名
@@ -337,7 +217,6 @@ class WindConfigParser implements IWindConfigParser {
 	 * @return string 			 返回缓存文件的$fileName的绝对路径
 	 */
 	private function buildCacheFilePath($fileName) {
-		//TODO 用框架中统一的路径构造方法 getRealPath
 		return rtrim(COMPILE_PATH, '/') . D_S . strtolower($fileName) . '.php';
 	}
 
@@ -355,6 +234,6 @@ class WindConfigParser implements IWindConfigParser {
 	 * 
 	 */
 	public function __destruct() {
-		$this->configParser = null;
+		$this->configParser = array();
 	}
 }
