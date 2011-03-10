a5e7e27dea7f2cebd63f50095314dd28a8066c45
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-10 13:13:54 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1773 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/doc/defaultconfig/php/db_config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author xiaoxia xu <xiaoxa.xuxx@aliyun-inc.com> 2011-3-8
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+return array(
+	'comment' => array(
+		'driver' => 'mysql',
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => 'root',
+		'port' => '3306',
+		'name' => 'wfcomment',
+		'charset' => 'utf8',
+	),
+);
---
+++ wind/doc/defaultconfig/php/viewTemplateConfig.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author xiaoxia xu <xiaoxa.xuxx@aliyun-inc.com> 2011-3-8
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+
+/**
+ * 结构列子：
+return array(
+   'tag1' => array(
+        'tag' => '',
+        'pattern' => '',
+        'compiler' => '',
+    )
+);
+*/
