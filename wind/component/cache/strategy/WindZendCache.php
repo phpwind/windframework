08ff90216940e66409cc12e570e53373595ae7cb
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-11 11:35:32 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1806 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/cache/strategy/WindZendCache.php
+++ wind/component/cache/strategy/WindZendCache.php
@@ -15,39 +15,43 @@ L::import('WIND:component.cache.operator.WindUZendCache');
  * @package 
  */
 class WindZendCache extends AbstractWindCache {
+
 	/**
 	 * @var WindUZendCache
 	 */
 	protected $zendCache = null;
-	
-	public function __construct(){
+
+	public function __construct() {
 		$this->zendCache = new WindUZendCache();
 	}
-	/* 
-	 * @see wind/component/cache/base/IWindCache#set()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::set()
 	 */
 	public function set($key, $value, $expires = 0, IWindCacheDependency $denpendency = null) {
 		return $this->zendCache->set($this->buildSecurityKey($key), $this->storeData($value, $expires, $denpendency), $expires);
 	}
-	/* 
-	 * @see wind/component/cache/base/IWindCache#fetch()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::get()
 	 */
 	public function get($key) {
 		return $this->getDataFromMeta($key, unserialize($this->zendCache->get($this->buildSecurityKey($key))));
 	}
-	
-	/* 
-	 * @see wind/component/cache/base/IWindCache#delete()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::delete()
 	 */
 	public function delete($key) {
 		return $this->zendCache->delete($this->buildSecurityKey($key));
 	}
-	
-	/* 
-	 * @see wind/component/cache/base/IWindCache#flush()
+
+	/**
+	 * Enter description here ...
+	 * @return boolean
 	 */
 	public function flush() {
 		return $this->zendCache->flush();
 	}
-	
+
 }
