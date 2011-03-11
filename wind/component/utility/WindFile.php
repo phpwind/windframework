241b850a9069159b485674b0feecfea9bb6dc4f8
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-11 14:24:27 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1816 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/utility/WindFile.php
+++ wind/component/utility/WindFile.php
@@ -13,31 +13,38 @@
  * @version $Id$ 
  * @package 
  */
-class WindFile{
+class WindFile {
+
 	/**
 	 * @var string 以读的方式打开文件，具有较强的平台移植性
 	 */
 	const READ = 'rb';
+
 	/**
 	 * @var string 以读写的方式打开文件，具有较强的平台移植性
 	 */
 	const READWRITE = 'rb+';
+
 	/**
 	 * @var string 以写的方式打开文件，具有较强的平台移植性
 	 */
 	const WRITE = 'wb';
+
 	/**
 	 * @var string 以读写的方式打开文件，具有较强的平台移植性
 	 */
-	const WRITEREAD='wb+';
+	const WRITEREAD = 'wb+';
+
 	/**
 	 * @var string 以追加写入方式打开文件，具有较强的平台移植性
 	 */
 	const APPEND_WRITE = 'ab';
+
 	/**
 	 * @var string 以追加读写入方式打开文件，具有较强的平台移植性
 	 */
 	const APPEND_WRITEREAD = 'ab+';
+
 	/**
 	 * 保存文件
 	 * @param string $fileName          保存的文件名
@@ -50,9 +57,8 @@ class WindFile{
 		L::import("COM:utility.WindString");
 		$temp = "<?php\r\n ";
 		if (!$isBuildReturn && is_array($data)) {
-			foreach ( $data as $key => $value ) {
-				if (!preg_match ( '/^\w+$/', $key ))
-					continue;
+			foreach ($data as $key => $value) {
+				if (!preg_match('/^\w+$/', $key)) continue;
 				$temp .= "\$" . $key . " = " . WindString::varToString($value) . ";\r\n";
 			}
 			$temp .= "\r\n?>";
@@ -62,6 +68,7 @@ class WindFile{
 		}
 		return self::write($fileName, $temp, $method, $ifLock);
 	}
+
 	/**
 	 * 写文件
 	 *
@@ -85,7 +92,7 @@ class WindFile{
 		$ifChmod && chmod($fileName, 0777);
 		return $writeCheck;
 	}
-	
+
 	/**
 	 * 读取文件
 	 *
@@ -104,22 +111,21 @@ class WindFile{
 		}
 		return $data;
 	}
+
 	/**
 	 * 按目录删除文件
 	 * @param string  $dir 目录
 	 * @param boolean $ifexpiled 是否过期
 	 * @return boolean
 	 */
-	public static function clearDir($dir, $ifexpiled = true) {
-		if (false === ($handle = opendir($dir))) {
-			return false;
-		}
+	public static function clearDir($dir, $ifexpiled = false) {
+		if (!$handle = @opendir($dir)) return false;
 		while (false !== ($file = readdir($handle))) {
 			if ('.' === $file[0] || '..' === $file[0]) continue;
 			$fullPath = $dir . DIRECTORY_SEPARATOR . $file;
 			if (is_dir($fullPath)) {
 				self::clearDir($fullPath, $ifexpiled);
-			} else if (($ifexpiled && ($mtime =  filemtime($fullPath)) && $mtime < time()) || !$ifexpiled) {
+			} else if (($ifexpiled && ($mtime = filemtime($fullPath)) && $mtime < time()) || !$ifexpiled) {
 				self::delFile($fullPath);
 			}
 		}
@@ -127,6 +133,7 @@ class WindFile{
 		false === $ifexpiled && rmdir($dir);
 		return true;
 	}
+
 	/**
 	 * 批量删除文件
 	 * @param string $path
@@ -134,19 +141,19 @@ class WindFile{
 	 * @param int $level
 	 * @return string
 	 */
-	public static function delFiles($path, $delDir = false, $level = 0){	
+	public static function delFiles($path, $delDir = false, $level = 0) {
 		$path = rtrim($path, DIRECTORY_SEPARATOR);
-		if (!$handler = opendir($path)){
+		if (!$handler = opendir($path)) {
 			return false;
 		}
-		while(false !== ($filename = readdir($handler))){
-			if ("." != $filename  && ".." != $filename){
-				if (is_dir($path.DIRECTORY_SEPARATOR.$filename)){
-					if (substr($filename, 0, 1) != '.'){
-						self::delFiles($path.DIRECTORY_SEPARATOR.$filename, $delDir, $level + 1);
-					}				
-				}else{
-					self::delFile($path.DIRECTORY_SEPARATOR.$filename);
+		while (false !== ($filename = readdir($handler))) {
+			if ("." != $filename && ".." != $filename) {
+				if (is_dir($path . DIRECTORY_SEPARATOR . $filename)) {
+					if (substr($filename, 0, 1) != '.') {
+						self::delFiles($path . DIRECTORY_SEPARATOR . $filename, $delDir, $level + 1);
+					}
+				} else {
+					self::delFile($path . DIRECTORY_SEPARATOR . $filename);
 				}
 			}
 		}
@@ -154,37 +161,39 @@ class WindFile{
 		true == $delDir && $level > 0 && rmdir($path);
 		return true;
 	}
+
 	/**
 	 * 取得文件的mime类型
 	 * @param string $fileName 文件名
 	 * @return string
 	 */
-	public static function getMimeType($fileName){
+	public static function getMimeType($fileName) {
 		$suffix = self::getFileSuffix($fileName);
-		$mimes = require rtrim(WIND_PATH,D_S).D_S.'component/utility/WindMimeTypes.php';
-		if(isset($mimes[$suffix])){
+		$mimes = require rtrim(WIND_PATH, D_S) . D_S . 'component/utility/WindMimeTypes.php';
+		if (isset($mimes[$suffix])) {
 			return is_array($mimes[$suffix]) ? current($mimes[$suffix]) : $mimes[$suffix];
-		}else{
+		} else {
 			throw new WindException('Sorry, can not find the corresponding mime type of the file');
 		}
 		return false;
 	}
+
 	/**
 	 * 取得目录的迭代
 	 * @param string $dir 目录名
 	 * @return DirectoryIterator
 	 */
-	public static function getDirectoryIterator($dir){
+	public static function getDirectoryIterator($dir) {
 		return new DirectoryIterator($dir);
 	}
-	
+
 	/**
 	 * 取得文件信息
 	 * @param unknown_type $fileName
 	 * @return string|number
 	 */
-	public static function getFileInfo($fileName){
-		if (false === is_file($fileName)){
+	public static function getFileInfo($fileName) {
+		if (false === is_file($fileName)) {
 			return array();
 		}
 		$fileInfo['name'] = substr(strrchr($fileName, DIRECTORY_SEPARATOR), 1);
@@ -193,7 +202,7 @@ class WindFile{
 		$fileInfo['ctime'] = filectime($fileName);
 		$fileInfo['atime'] = fileatime($fileName);
 		$fileInfo['mtime'] = filemtime($fileName);
-		$fileInfo['readable'] = is_readable($fileName);	
+		$fileInfo['readable'] = is_readable($fileName);
 		$fileInfo['writable'] = is_writable($fileName);
 		$fileInfo['executable'] = is_executable($fileName);
 		$fileInfo['right'] = fileperms($fileName);
@@ -202,38 +211,38 @@ class WindFile{
 		$fileInfo['mime'] = self::getMimeType($fileName);
 		return $fileInfo;
 	}
-	
+
 	/**
 	 * 取得目录信息
 	 * @param unknown_type $dir
 	 * @return string|multitype:
 	 */
-	public static function getDirectoryInfo($dir){
-		if(false !== is_dir($dir)){
+	public static function getDirectoryInfo($dir) {
+		if (false !== is_dir($dir)) {
 			return array();
 		}
-		return  stat($dir);
+		return stat($dir);
 	}
+
 	/**
 	 * 删除文件
 	 * @param string $filename
 	 * @return boolean
 	 */
-	public static function delFile($filename){
+	public static function delFile($filename) {
 		return unlink($filename);
 	}
-	
-	
+
 	/**
 	 * 取得文件后缀
 	 * @param string $filename
 	 * @return string
 	 */
-	public static function getFileSuffix($filename){
-		$filename = explode($filename,'.');
-		return $filename[count($filename)-1];
+	public static function getFileSuffix($filename) {
+		$filename = explode($filename, '.');
+		return $filename[count($filename) - 1];
 	}
-	
+
 	/**
 	 * 取得真实的目录
 	 * @param string $path 路径名
@@ -242,5 +251,5 @@ class WindFile{
 	public static function appendSlashesToDir($path) {
 		return rtrim($path, '\\/') . DIRECTORY_SEPARATOR;
 	}
-	
+
 }
