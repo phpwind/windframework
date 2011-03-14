b3a7bc4456aa4efe4f8bc65ecb92e491f5dbef8d
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-14 05:34:36 +0000

    代码重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1846 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ demos/helloworld/db_config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author xiaoxia xu <xiaoxa.xuxx@aliyun-inc.com> 2011-3-8
+ * @link http://www.phpwind.com
+ * @copyright Copyright &copy; 2003-2110 phpwind.com
+ * @license 
+ */
+return array(
+	'comment' => array(
+		'driver' => 'mysql',
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => 'phpwind.net',
+		'port' => '3306',
+		'name' => 'yulingsp3',
+		'charset' => 'utf8',
+	),
+);
