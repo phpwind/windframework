914f4c0cf3818ead6ea2eb3ef1ecc0a7371baf13
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-14 03:13:51 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1828 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/cache/strategy/WindMemCache.php
+++ wind/component/cache/strategy/WindMemCache.php
@@ -15,61 +15,68 @@ L::import('WIND:component.cache.operator.WindUMemcache');
  * @package 
  */
 class WindMemCache extends AbstractWindCache {
-	
+
 	/**
 	 * @var WindMemcache memcache缓存操作句柄
 	 */
 	protected $memcache = null;
+
 	/**
 	 * @var int 是否对缓存采取压缩存储
 	 */
 	protected $compress = 0;
+
 	//配置信息
 	/**
 	 * @var int 是否对缓存进行压缩，如果缓存的值较大，可进行压缩
 	 */
 	const COMPRESS = 'compress';
+
 	/**
 	 * @var string 取得memcache配置项
 	 */
 	const SERVERCONFIG = 'servers';
-	
+
 	public function __construct() {
 		$this->memcache = new WindUMemcache();
 	}
-	/* 
-	 * @see wind/component/cache/base/IWindCache#set()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::set()
 	 */
 	public function set($key, $value, $expire = 0, IWindCacheDependency $denpendency = null) {
-		$expire = null === $expire  ? $this->getExpire() : $expire;
+		$expire = null === $expire ? $this->getExpire() : $expire;
 		return $this->memcache->set($this->buildSecurityKey($key), $this->storeData($value, $expire, $denpendency), $this->compress, (int) $expire);
 	}
-	/* 
-	 * @see wind/component/cache/base/IWindCache#fetch()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::get()
 	 */
 	public function get($key) {
 		return $this->getDataFromMeta($key, unserialize($this->memcache->get($this->buildSecurityKey($key), $this->compress)));
 	}
-	/* 
-	 * @see wind/component/cache/base/IWindCache#delete()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::delete()
 	 */
 	public function delete($key) {
 		return $this->memcache->delete($this->buildSecurityKey($key));
 	}
-	
-	/* 
-	 * @see wind/component/cache/base/IWindCache#clear()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::clear()
 	 */
 	public function clear() {
 		return $this->memcache->flush();
 	}
-	/* 
-	 * @see wind/component/cache/base/IWindCache#clearByType()
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindCache::clearByType()
 	 */
 	public function clearByType($key, $type) {
-		
+
 	}
-	
+
 	/* 
 	 * @example $config = array(
 	 * array(
@@ -85,16 +92,15 @@ class WindMemCache extends AbstractWindCache {
 	 * 'compress'=>true,
 	 * 'security'=>'1x2aao@'
 	 * 'prefix'=>'phpwind'
-	 * 
 	 * )
 	 * @see wind/core/WindComponentModule#setConfig()
+	 */
+	/* (non-PHPdoc)
+	 * @see WindComponentModule::setConfig()
 	 */
 	public function setConfig($config) {
 		parent::setConfig($config);
 		$_config = is_object($config) ? $config->getConfig() : $config;
-		if (!isset($_config[self::SERVERCONFIG]) || !is_array($_config[self::SERVERCONFIG])) {
-			throw new WindException('The server config is not exist');
-		}
 		$this->memcache->setServers($_config[self::SERVERCONFIG]);
 		$this->compress = isset($_config[self::COMPRESS]) ? MEMCACHE_COMPRESSED : 0;
 	}
