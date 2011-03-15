74c24a41ab4d56fe1f66564fd581628b2c3f1596
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-15 09:36:04 +0000

    代码重构,bug修复
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1873 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/web/WindWebApplication.php
+++ wind/core/web/WindWebApplication.php
@@ -45,10 +45,10 @@ class WindWebApplication extends WindComponentModule implements IWindApplication
 			$this->sendErrorMessage($actionException);
 		
 		} catch (WindSqlException $windSqlException) {
-			$this->noActionHandlerFound($windSqlException->getMessage());
-		
+			$this->sendErrorMessage($windSqlException->getMessage());
+			
 		} catch (WindViewException $windViewException) {
-
+			
 		}
 	}
 
