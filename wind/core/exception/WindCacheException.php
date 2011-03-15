73262ace391b5de867f62da350d10234d7ea18f2
weihu <weihu@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-15 03:03:48 +0000

    缓存重构
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1858 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/exception/WindCacheException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*@author Su Qian <weihu@alibaba-inc.com> 2010-11-7
+*@link http://www.phpwind.com
+*@copyright Copyright &copy; 2003-2110 phpwind.com
+*@license 
+*/
+
+L::import('WIND:core.exception.WindException');
+
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Su Qian <weihu@alibaba-inc.com>
+ * @version $Id$ 
+ * @package 
+ */
+class WindCacheException extends WindException{
+	
+}
