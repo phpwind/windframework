f2e288d636d035d8020be5138ad802ddc0a57dc1
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-03-18 06:05:44 +0000

    优化预加载
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@1953 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/WindBase.php
+++ wind/WindBase.php
@@ -386,33 +386,6 @@ class L {
 
 }
 
-/* 组件定义名称 */
-!defined('COMPONENT_WEBAPP') && define('COMPONENT_WEBAPP', 'windWebApp');
-!defined('COMPONENT_ERRORHANDLER') && define('COMPONENT_ERRORHANDLER', 'errorHandler');
-!defined('COMPONENT_LOGGER') && define('COMPONENT_LOGGER', 'windLogger');
-!defined('COMPONENT_FORWARD') && define('COMPONENT_FORWARD', 'forward');
-!defined('COMPONENT_ROUTER') && define('COMPONENT_ROUTER', 'urlBasedRouter');
-!defined('COMPONENT_URLHELPER') && define('COMPONENT_URLHELPER', 'urlHelper');
-!defined('COMPONENT_VIEW') && define('COMPONENT_VIEW', 'windView');
-!defined('COMPONENT_VIEWRESOLVER') && define('COMPONENT_VIEWRESOLVER', 'viewResolver');
-!defined('COMPONENT_TEMPLATE') && define('COMPONENT_TEMPLATE', 'template');
-!defined('COMPONENT_ERRORMESSAGE') && define('COMPONENT_ERRORMESSAGE', 'errorMessage');
-!defined('COMPONENT_DB') && define('COMPONENT_DB', 'db');
-
-//TODO 迁移更新框架内部的常量定义到这里  配置/异常类型等 注意区分异常命名空间和类型
-
-
-//********************约定变量***********************************
-define('WIND_M_ERROR', 'windError');
-define('CONFIG_CACHE', 'wind_components_config');
-
-//**********配置*******通用常量定义***************************************
-define('WIND_CONFIG_CONFIG', 'config');
-define('WIND_CONFIG_CLASS', 'class');
-define('WIND_CONFIG_CLASSPATH', 'path');
-define('WIND_CONFIG_RESOURCE', 'resource');
-define('WIND_CONFIG_VALUE', 'value');
-
 
 
 
