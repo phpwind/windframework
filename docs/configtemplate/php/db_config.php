8891b4f156305c79a4dad7bb8402853a466e9a49
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-07-15 04:34:55 +0000

    DB配置格式样式
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2147 18ba2127-5a84-46d4-baec-3457e417f034

--- docs/configtemplate/php/db_config.php
+++ docs/configtemplate/php/db_config.php
@@ -10,11 +10,9 @@
  * 可以配置多个链接，多个链接的时候必需指定每个链接的type类型是master/slave，当配置一个的时候可以不配置type项 
  */
 return array(
-	'connection' => array(
-		'dsn' => 'mysql:host=localhost;dbname=test',
-		'user' => 'root',
-		'pwd' => 'root',
-		'charset' => 'utf8',
-		'tablePrefix' => 'pw_'
-	),
+	'dsn' => 'mysql:host=localhost;dbname=test',
+	'user' => 'root',
+	'pwd' => 'root',
+	'charset' => 'utf8',
+	'tablePrefix' => 'pw_'
 );
