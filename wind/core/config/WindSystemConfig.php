5b2eaa5a1052ab7d34e2dd3161c72dbfafc9e893
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-07-25 08:05:16 +0000

    bug修复，config不匹配
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2213 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/config/WindSystemConfig.php
+++ wind/core/config/WindSystemConfig.php
@@ -30,10 +30,10 @@ class WindSystemConfig extends WindModule {
 			$configParser = $factory->getInstance(COMPONENT_CONFIGPARSER);
 			$config = $configParser->parse($config, $this->appName . '_config');
 		}
-		if (isset($config[$this->appName]))
-			$this->_config = $config[$this->appName];
+		if (isset($config['web-apps'][$this->appName]))
+			$this->_config = $config['web-apps'][$this->appName];
 		else
-			$this->_config = $config;
+			$this->_config = $config['web-apps'];
 	}
 
 	/**
@@ -185,7 +185,15 @@ class WindSystemConfig extends WindModule {
 	 */
 	public function getModuleControllerSuffixByModuleName($name, $default = '') {
 		$module = $this->getConfig('modules', $name);
-		return $this->getConfig('controller-suffix', '', $default, $module);
+		return $this->getConfig('controller-suffix', 'value', $default, $module);
+	}
+	
+	/**
+	 * 获得缓存数据
+	 * @param string $type
+	 */
+	public function getCacheConfig($type = '') {
+		return $this->getConfig('cache', $type);
 	}
 
 }
