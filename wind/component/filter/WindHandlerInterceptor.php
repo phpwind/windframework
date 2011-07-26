b69b42e1316d7b949a1876bdd89b044f06bbb0b9
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-07-26 09:04:12 +0000

    目录修改
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2226 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/component/filter/WindHandlerInterceptor.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+class WindHandlerInterceptor extends WindModule {
+	protected $result = null;
+	protected $interceptorChain = null;
+
+	/**
+	 * Enter description here ...
+	 */
+	public function preHandle() {}
+
+	/**
+	 * Enter description here ...
+	 */
+	public function postHandle() {}
+
+	/**
+	 * Enter description here ...
+	 * @return mixed
+	 */
+	public function handle() {
+		$args = func_get_args();
+		$this->result = call_user_func_array(array($this, 'preHandle'), $args);
+		if ($this->result !== null) {
+			return $this->result;
+		}
+		if (null !== ($handler = $this->interceptorChain->getHandler())) {
+			$this->result = call_user_func_array(array($handler, 'handle'), $args);
+		} else {
+			$this->result = $this->interceptorChain->execute();
+		}
+		call_user_func_array(array($this, 'postHandle'), $args);
+		return $this->result;
+	}
+
+	/**
+	 * 设置过滤链对象
+	 * 
+	 * @param WindHandlerInterceptorChain $interceptorChain
+	 */
+	public function setHandlerInterceptorChain($interceptorChain) {
+		$this->interceptorChain = $interceptorChain;
+	}
+}
+?>
---
+++ wind/component/filter/WindHandlerInterceptorChain.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+class WindHandlerInterceptorChain extends WindModule {
+	protected $_interceptors = array();
+	protected $_callBack = null;
+	protected $_args = array();
+	private $_state = true;
+
+	/**
+	 * 设置回调方法
+	 * 
+	 * @param string|array $callBack
+	 * @param array $args
+	 * @return
+	 */
+	public function setCallBack($callBack, $args = array()) {
+		$this->_callBack = $callBack;
+		$this->_args = $args;
+	}
+
+	/**
+	 * 执行callback方法
+	 * 
+	 * @throws WindException
+	 * @return void|mixed
+	 */
+	public function execute() {
+		if ($this->_callBack === null) return null;
+		if (is_string($this->_callBack) && !function_exists($this->_callBack)) {
+			throw new WindException('[core.filter.WindHandlerInterceptorChain.execute]' . $this->_callBack, 
+				WindException::ERROR_FUNCTION_NOT_EXIST);
+		}
+		return call_user_func_array($this->_callBack, (array) $this->_args);
+	}
+
+	/**
+	 * 返回处理句柄
+	 * 
+	 * @return WindHandlerInterceptor
+	 */
+	public function getHandler() {
+		if ($this->_state) {
+			$this->addInterceptors(new WindHandlerInterceptor());
+			$this->_state = false;
+		}
+		if (count($this->_interceptors) <= 0) return null;
+		$handler = array_shift($this->_interceptors);
+		if ($handler instanceof WindHandlerInterceptor) {
+			$handler->setHandlerInterceptorChain($this);
+			return $handler;
+		}
+		Wind::log(
+			'[core.filter.WindHandlerInterceptorChain.getHandler] the type of Interceptor ' . gettype($handler) .
+				 ' is not supported.', WindLogger::LEVEL_DEBUG, 'wind.core');
+		return $this->getHandler();
+	}
+
+	/**
+	 * 添加过滤连中的拦截器对象, 支持数组和对象两种类型
+	 * 
+	 * @param $interceptors
+	 * @return 
+	 */
+	public function addInterceptors($interceptors) {
+		if (is_array($interceptors))
+			$this->_interceptors += $interceptors;
+		else
+			$this->_interceptors[] = $interceptors;
+	}
+}
+?>
