0f71f6e280c2351e9a35c9f1d16af29755999156
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-02 05:42:50 +0000

    配置调整
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2287 18ba2127-5a84-46d4-baec-3457e417f034

--- docs/configtemplate/php/wind_config.php
+++ docs/configtemplate/php/wind_config.php
@@ -6,64 +6,69 @@
  * @license 
  */
 return array(
-	'web-apps' => array(
      /*配置应用项名为default*/
-		'default' => array(
-			'class' => 'windWebApp',
-			'root-path' => '',
-		    'factory' => array(
-				'class-definition' => 'components',
-				'class' => 'WIND:core.factory.WindComponentFactory',
-			),
-			'filters' => array(
-				'class' => 'WIND:core.filter.WindFilterChain',
-				'filter1' => array('class' => 'WIND:core.web.filter.WindLoggerFilter'),
-				'filter2' => array('class' => 'WIND:core.web.filter.WindUrlFilter'),
-			),
+	'default' => array(
+		'class' => 'windWebApp',
+		'filters' => array(
+			'class' => 'WIND:core.filter.WindFilterChain',
+			'filter1' => array('class' => 'WIND:core.web.filter.WindLoggerFilter'),
+			'filter2' => array('class' => 'WIND:core.web.filter.WindUrlFilter'),
+		),
             /*配置default应用的路由规则*/
-			'router' => array(
-				'class' => 'urlBasedRouter',
-                'config' => array(
-                    /*配置路径中module的规则*/
-                     'module' => array(
-                        'url-param' => 'm',
-                        'default-value' => 'default',
-                     ),
-                    /*配置路径中controller的规则*/
-                     'controller' => array(
-                        'url-param' => 'c',
-                        'default-value' => 'index',
-                     ),
-                    /*配置路径中action的规则*/
-                     'action' => array(
-                        'url-param' => 'a',
-                        'default-value' => 'run',
-                     ),
-                ),
-			),
+		'router' => array(
+			'class' => 'urlBasedRouter',
+            'config' => array(
+            /*配置路径中module的规则*/
+        	   	 'module' => array(
+	        	     'url-param' => 'm',
+	            	 'default-value' => 'default',
+             	 ),
+           /*配置路径中controller的规则*/
+                 'controller' => array(
+                     'url-param' => 'c',
+                     'default-value' => 'index',
+                  ),
+             /*配置路径中action的规则*/
+                  'action' => array(
+                     'url-param' => 'a',
+                     'default-value' => 'run',
+                  ),
+            ),
+		),
 			/*配置default应用的模块配置*/
-			'modules' => array(
+		'modules' => array(
 			    /*配置default模块*/
-				'default' => array(
-			        /*default模块的路径*/
-					'path' => 'controller',  
+			'default' => array(
+			       /*default模块的路径*/
+				'controller-path' => 'controller',  
 			        /*default模块的中controller的后缀*/
-					'controller-suffix' => array('value' => 'Controller',),
+				'controller-suffix' => 'Controller',
 					/*default模块中处理error的Action controller路径*/
-					'error-handler' => array('class' => 'WIND:core.web.WindErrorHandler',),
+				'error-handler' => 'WIND:core.web.WindErrorHandler',
 					/*default模块的试图配置*/
-					'view' => array(
-						'class' => 'windView',
-						'config' => array(
+				'view' => array(
+					'class' => 'windView',
+					'config' => array(
 					       /*模板路径*/
-							'template-dir' => array('value' => 'template',),
+						'template-dir' => 'template',
 					       /*模板后缀*/
-							'template-ext' => array('value' => 'htm',),
+						'template-ext' => 'htm',
 					       /*模板编译路径*/
-							'compile-dir' => array('value' => 'compile.template',),
-						),
+						'compile-dir' => 'compile.template',
 					),
 				),
+			),
+		),
+		/*db配置*/
+		'db' => array(
+		//   'resource' => 'db_config.php',
+			'conn1' => array(
+				'class' => 'COM:db.WindConnection',
+				'dsn' => 'mysql:host=localhost;dbname=test',
+				'user' => 'root',
+				'pwd' => 'root',
+				'charset' => 'utf8', 
+				'tablePrefix' => 'pw_'
 			),
 		),
 	),
