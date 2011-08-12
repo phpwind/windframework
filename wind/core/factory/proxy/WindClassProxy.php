683f218dfbbc3f5e76e656535d191a1ab5c0c076
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-12 08:00:15 +0000

    代码优化
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2319 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/factory/proxy/WindClassProxy.php
+++ wind/core/factory/proxy/WindClassProxy.php
@@ -8,9 +8,12 @@
  * @version $Id$
  * @package 
  */
-class WindClassProxy implements IWindClassProxy {
-	private $_interceptorChain = 'COM:filter.WindHandlerInterceptorChain';
-	private $_interceptorChainObj = null;
+class WindClassProxy {
+	const EVENT_TYPE_METHOD = 'method';
+	const EVENT_TYPE_SETTER = 'setter';
+	const EVENT_TYPE_GETTER = 'getter';
+	
+	private $_interceptorChain = 'WIND:core.filter.WindHandlerInterceptorChain';
 	protected $_attributes = array();
 	protected $_className = '';
 	protected $_classPath = '';
@@ -21,8 +24,34 @@ class WindClassProxy implements IWindClassProxy {
 	/**
 	 * @param string|object $targetObj
 	 */
-	public function __construct($targetObj = null, $args = array()) {
-		$this->initClassProxy($targetObj, $args);
+	public function __construct($targetObject = null, $args = array()) {
+		try {
+			if (is_object($targetObject)) {
+				$this->_setClassName(get_class($targetObject));
+				$this->_instance = $targetObject;
+			} elseif (is_string($targetObject) && !empty($targetObject)) {
+				$this->_setClassPath($targetObject);
+				$reflection = new ReflectionClass($this->_className);
+				if ($reflection->isAbstract() || $reflection->isInterface()) {
+					throw new WindException($this->_className, WindException::ERROR_CLASS_NOT_EXIST);
+				}
+				$this->_reflection = $reflection;
+				$this->_instance = call_user_func_array(array($this->_reflection, 'newInstance'), $args);
+			} else
+				throw new WindException($this->_className, WindException::ERROR_CLASS_NOT_EXIST);
+			
+			$types = array(self::EVENT_TYPE_METHOD, self::EVENT_TYPE_GETTER, self::EVENT_TYPE_SETTER);
+			foreach ($types as $type) {
+				$this->_listener[$type] = array();
+			}
+			
+			if ($this->_instance !== null) return;
+		
+		} catch (Exception $e) {
+			Wind::log('[core.factory.proxy.WindClassProxy.initClassProxy] Initialization proxy failed.' . $e->getMessage(), WindLogger::LEVEL_DEBUG, 'wind.core');
+		}
+	
+		//$this->initClassProxy($targetObj, $args);
 	}
 
 	/* (non-PHPdoc)
@@ -30,9 +59,7 @@ class WindClassProxy implements IWindClassProxy {
 	 */
 	public function registerEventListener($event, $listener, $type = self::EVENT_TYPE_METHOD) {
 		if (!in_array($type, array(self::EVENT_TYPE_METHOD, self::EVENT_TYPE_GETTER, self::EVENT_TYPE_SETTER))) {
-			throw new WindException(
-				'[core.factory.proxy.WindClassProxy.registerEventListener] Unsupport event type:' . $type, 
-				WindException::ERROR_PARAMETER_TYPE_ERROR);
+			throw new WindException('[core.factory.proxy.WindClassProxy.registerEventListener] Unsupport event type:' . $type, WindException::ERROR_PARAMETER_TYPE_ERROR);
 		}
 		!isset($this->_listener[$type][$event]) && $this->_listener[$type][$event] = array();
 		array_push($this->_listener[$type][$event], $listener);
@@ -101,8 +128,13 @@ class WindClassProxy implements IWindClassProxy {
 				$this->_setClassName(get_class($targetObject));
 				$this->_instance = $targetObject;
 			} elseif (is_string($targetObject) && !empty($targetObject)) {
-				$_className = Wind::import($targetObject);
-				$this->_setClassName($_className);
+				$this->_setClassPath($targetObject);
+				$reflection = new ReflectionClass($this->_className);
+				if ($reflection->isAbstract() || $reflection->isInterface()) {
+					throw new WindException($this->_className, WindException::ERROR_CLASS_NOT_EXIST);
+				}
+				$this->_reflection = $reflection;
+				$this->_instance = call_user_func_array(array($this->_reflection, 'newInstance'), $args);
 			} else
 				throw new WindException($this->_className, WindException::ERROR_CLASS_NOT_EXIST);
 			
@@ -110,17 +142,9 @@ class WindClassProxy implements IWindClassProxy {
 			foreach ($types as $type) {
 				$this->_listener[$type] = array();
 			}
-			$reflection = new ReflectionClass($this->_className);
-			if ($reflection->isAbstract() || $reflection->isInterface()) {
-				throw new WindException($this->_className, WindException::ERROR_CLASS_NOT_EXIST);
-			}
-			$this->_reflection = $reflection;
-			if ($this->_instance !== null) return;
-			$this->_instance = call_user_func_array(array($this->_reflection, 'newInstance'), $args);
+		
 		} catch (Exception $e) {
-			Wind::log(
-				'[core.factory.proxy.WindClassProxy.initClassProxy] Initialization proxy failed.' . $e->getMessage(), 
-				WindLogger::LEVEL_DEBUG, 'wind.core');
+			Wind::log('[core.factory.proxy.WindClassProxy.initClassProxy] Initialization proxy failed.' . $e->getMessage(), WindLogger::LEVEL_DEBUG, 'wind.core');
 		}
 	}
 
@@ -134,7 +158,8 @@ class WindClassProxy implements IWindClassProxy {
 			$interceptorChain = WindFactory::createInstance($chain);
 			if ($interceptorChain && $interceptorChain instanceof WindHandlerInterceptorChain) {
 				$this->_interceptorChainObj = $interceptorChain;
-			} else throw new WindException('unable to create interceptorChain.');
+			} else
+				throw new WindException('[core.factory.proxy.WindClassProxy._getInterceptorChain] Unable to create interceptorChain.');
 		}
 		$this->_interceptorChainObj->reset();
 		return $this->_interceptorChainObj;
@@ -159,17 +184,6 @@ class WindClassProxy implements IWindClassProxy {
 	 */
 	public function _getInstance() {
 		return $this->_instance;
-	}
-
-	/* (non-PHPdoc)
-	 * @see IWindClassProxy::_getReflection()
-	 */
-	public function _getReflection() {
-		if ($this->_reflection instanceof ReflectionClass)
-			return $this->_reflection;
-		else
-			throw new WindException(get_class($this) . '->_reflection, ' . gettype($this->_reflection), 
-				WindException::ERROR_CLASS_TYPE_ERROR);
 	}
 
 	/**
@@ -209,7 +223,6 @@ class WindClassProxy implements IWindClassProxy {
 	 */
 	public function _setProperty($propertyName, $value) {
 		$this->_getInstance()->$propertyName = $value;
-		return true;
 	}
 
 	/**
@@ -217,32 +230,6 @@ class WindClassProxy implements IWindClassProxy {
 	 */
 	public function _getProperty($propertyName) {
 		return $this->_getInstance()->$propertyName;
-	}
-
-	/**
-	 * 根据别名返回属性定义，别名为空时返回整个属性定义列表
-	 * 
-	 * @param string $alias
-	 * @return object | array
-	 */
-	public function _getAttribute($alias = '') {
-		if ($alias === '')
-			return $this->_attributes;
-		else
-			return isset($this->_attributes[$alias]) ? $this->_attributes[$alias] : null;
-	}
-
-	/**
-	 * 设置属性对象,设置的属性可以在listener中被访问到
-	 * 
-	 * @param string|array $alias
-	 * @param object $object
-	 */
-	public function _setAttribute($alias, $object = null) {
-		if (is_array($alias))
-			$this->_attributes += $alias;
-		elseif (is_string($alias))
-			$this->_attributes[$alias] = $object;
 	}
 }
 ?>
