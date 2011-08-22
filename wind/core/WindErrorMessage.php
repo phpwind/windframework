263d4d817594fe14a5f59c79f93e2617ddd171a9
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-22 06:59:14 +0000

    代码优化： 启动流程，路由策略机制，配置格式兼容components标签
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2407 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/core/WindErrorMessage.php
+++ wind/core/WindErrorMessage.php
@@ -73,3 +73,35 @@ class WindErrorMessage extends WindModule implements IWindErrorMessage {
 	}
 
 }
+
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author Qiong Wu <papa0924@gmail.com>
+ * @version $Id$
+ * @package 
+ */
+interface IWindErrorMessage {
+
+	/**
+	 * 添加错误信息
+	 * @param string $message
+	 * @param string $key
+	 */
+	public function addError($message, $key = '');
+
+	/**
+	 * 获得一条Error信息
+	 * @param string $key
+	 */
+	public function getError($key = '');
+
+	/**
+	 * 清空Error信息
+	 */
+	public function clearError();
+
+	/**
+	 * 发送错误信息
+	 */
+	public function sendError();
+}
