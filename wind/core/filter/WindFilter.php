f1d2912ee81ee41799cfb352a86bb8f17000cea4
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-22 04:25:27 +0000

    代码优化： 启动流程，路由策略机制，配置格式兼容components标签
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2406 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/core/filter/WindFilter.php
@@ -0,0 +1,25 @@
+<?php
+Wind::import('COM:fitler.WindHandlerInterceptor');
+/**
+ * the last known user to change this file in the repository  <$LastChangedBy$>
+ * @author xiaoxia xu <x_824@sina.com>
+ * @version $Id$
+ * @package
+ */
+class WindFilter extends WindHandlerInterceptor {
+
+	/* (non-PHPdoc)
+	 * @see WindHandlerInterceptor::preHandle()
+	 */
+	public function preHandle() {
+
+	}
+
+	/* (non-PHPdoc)
+	 * @see WindHandlerInterceptor::postHandle()
+	 */
+	public function postHandle() {
+
+	}
+
+}
