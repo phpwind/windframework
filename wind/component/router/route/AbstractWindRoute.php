3b3d9bfb9b11ea517921e259f97ed6d500675f8c
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-22 04:22:50 +0000

    代码优化： 启动流程，路由策略机制，配置格式兼容components标签
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2402 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/router/route/AbstractWindRoute.php
+++ wind/component/router/route/AbstractWindRoute.php
@@ -1,5 +1,4 @@
 <?php
-Wind::import('COM:filter.WindHandlerInterceptor');
 /**
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
