d8764d02244ade7e4e33026deeb4982fc3968b8d
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-22 04:18:31 +0000

    代码优化： 启动流程，路由策略机制，配置格式兼容components标签
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2398 18ba2127-5a84-46d4-baec-3457e417f034

--- _compile/compile.php
+++ _compile/compile.php
@@ -3,22 +3,19 @@
  * 动态编译脚本,开发环境执行此文件,动态生成过程文件
  * @author wuq
  **/
-define('_COMPILE_PATH', WIND_PATH . '_compile' . D_S);
-define('_COMPILE_LIBRARY_PATH', WIND_PATH . 'wind_basic.php');
+error_reporting(E_ALL);
+include '../wind/Wind.php';
+define('_COMPILE_PATH', dirname(__FILE__) . '/');
 Wind::clear();
 Wind::import('COM:log.WindLogger');
 Wind::import('WIND:core.*', true);
-Wind::import('COM:filter.*', true);
-Wind::import('COM:parser.WindConfigParser', true);
-Wind::import('COM:http.request.*', true);
-Wind::import('COM:http.response.*', true);
-Wind::import('COM:router.*', true);
 
 $imports = Wind::getImports();
 /* 载入需要的文件信息 */
 Wind::import('COM:utility.WindPack');
 Wind::import('COM:utility.WindFile');
 Wind::import('COM:utility.WindString');
+Wind::import('COM:parser.WindConfigParser');
 /* 打包 */
 $pack = new WindPack();
 $fileList = array();
@@ -28,16 +25,24 @@ foreach ($imports as $key => $value) {
 	$fileList[$_key] = array($key, $value);
 	$content[$value] = parseFilePath($key);
 }
-$pack->packFromFileList($fileList, _COMPILE_LIBRARY_PATH, WindPack::STRIP_PHP, true);
+$pack->packFromFileList($fileList, _COMPILE_PATH . 'wind_basic.php', WindPack::STRIP_PHP, true);
 /* import信息写入编译文件 */
-WindFile::write(WIND_PATH . 'wind_imports.php', 
+WindFile::write(_COMPILE_PATH . 'wind_imports.php', 
 	'<?php return ' . WindString::varToString($content) . ';');
 
 /* 编译配置文件信息 */
-$_systemConfig = Wind::getRealPath('WIND:components_config');
 $windConfigParser = new WindConfigParser();
-$result = $windConfigParser->parse(_COMPILE_PATH . 'components_config.xml');
-WindFile::write($_systemConfig, '<?php return ' . WindString::varToString($result) . ';');
+$dh = opendir(_COMPILE_PATH . 'config');
+while (($file = readdir($dh)) !== false) {
+	if (is_file(_COMPILE_PATH . 'config/' . $file) && $file !== '.' && $file !== '..') {
+		$result = $windConfigParser->parse(_COMPILE_PATH . 'config/' . $file);
+		$file = preg_replace('/\.(\w)*$/i', '', $file);
+		WindFile::write(_COMPILE_PATH . $file . '.php', 
+			'<?php return ' . WindString::varToString($result) . ';');
+	}
+}
+
+echo 'compile successful!';
 
 /* 清理所有缓存 */
 function parseFilePath($filePath) {
