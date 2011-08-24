b20f4107e0901a25a58f41e272de100969a721af
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-24 04:56:28 +0000

    代码优化
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2462 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/component/dao/WindDao.php
+++ wind/component/dao/WindDao.php
@@ -9,76 +9,20 @@
  */
 class WindDao extends WindModule {
 	/**
-	 * 链接配置文件或是配置数组
-	 * @var string|array
-	 */
-	protected $dbName = '';
-	/**
-	 * cache类型定义
-	 *
-	 * @var string
-	 */
-	protected $cacheClass = '';
-	/**
-	 * cache配置信息
-	 *
-	 * @var string
-	 */
-	protected $cacheConfig = '';
-	
-	/**
 	 * @var object
 	 */
 	protected $connection = null;
-	protected $cacheHandler = null;
-
-	/**
-	 * 获得需要缓存的处理的方法名称数组
-	 * array('methodName1'=>'WIND:component')
-	 * @return multitype:
-	 */
-	public function getCacheMethods() {
-		return array();
-	}
 
 	/**
 	 * 根据用户配置决定配置是采用配置链接管理
 	 * @return WindConnection
 	 */
 	public function getConnection($name = '') {
-		 $this->_getConnection();
-		 if ($this->connection instanceof WindConnectionManager) {
-		 	return $this->connection->getConnection($name);
-		 }
-		 return $this->connection;
-	}
-
-	/**
-	 * @return WindCacheDb
-	 */
-	public function getCacheHandler() {
-		return $this->_getCacheHandler();
-	}
-
-	/**
-	 * @return the $configName
-	 */
-	public function getDBName() {
-		return $this->dbName;
-	}
-
-	/**
-	 * @return the $cacheClass
-	 */
-	public function getCacheClass() {
-		return $this->cacheClass;
-	}
-
-	/**
-	 * @return the $cacheConfig
-	 */
-	public function getCacheConfig() {
-		return $this->cacheConfig;
+		$this->_getConnection();
+		if ($this->connection instanceof WindConnectionManager) {
+			return $this->connection->getConnection($name);
+		}
+		return $this->connection;
 	}
 
 	/**
@@ -87,13 +31,5 @@ class WindDao extends WindModule {
 	public function setConnection($connection) {
 		$this->connection = $connection;
 	}
-
-	/**
-	 * @param field_type $cacheHandler
-	 */
-	public function setCacheHandler($cacheHandler) {
-		$this->cacheHandler = $cacheHandler;
-	}
-
 }
 ?>
--- wind/component/dao/WindDaoFactory.php
+++ wind/component/dao/WindDaoFactory.php
@@ -15,14 +15,7 @@ Wind::import('COM:dao.exception.WindDaoException');
  */
 class WindDaoFactory extends WindModule {
 	/**
-	 * dao 实例数组
-	 *
-	 * @var array
-	 */
-	private $daos = array();
-	/**
 	 * dao路径信息
-	 *
 	 * @var string
 	 */
 	protected $daoResource = '';
@@ -38,19 +31,16 @@ class WindDaoFactory extends WindModule {
 	 */
 	public function getDao($className) {
 		try {
-			if (strpos($className, ":") === false && strpos($className, ".") === false) {
+			if (strpos($className, ":") === false)
 				$className = $this->getDaoResource() . '.' . $className;
-			}
-			if (isset($this->daos[$className])) return $this->daos[$className];
-			
-			$className = Wind::import($className);
-			$daoInstance = WindFactory::createInstance($className);
-			$this->createDbConnection($daoInstance);
-			$this->createCacheHandler($daoInstance);
+			Wind::getApp()->getWindFactory()->addClassDefinitions($className, 
+				array('path' => $className, 'scope' => 'application'));
+			$daoInstance = Wind::getApp()->getWindFactory()->getInstance($className);
+			$daoInstance->setDelayAttributes(array('connection' => array('ref' => 'db')));
 			return $daoInstance;
 		} catch (Exception $exception) {
 			throw new WindDaoException(
-				'[component.dao.WindDaoFactory] create dao ' . $className . ' fail. Error message:' . $exception->getMessage());
+				'[component.dao.WindDaoFactory] create dao ' . $className . ' fail.' . $exception->getMessage());
 		}
 	}
 
@@ -68,61 +58,6 @@ class WindDaoFactory extends WindModule {
 	 */
 	public function setDaoResource($daoResource) {
 		$this->daoResource = $daoResource;
-	}
-
-	/**
-	 * 注册Dao缓存监听
-	 * @param WindDao daoInstance
-	 */
-	private function registerCacheListener($daoInstance) {
-		$caches = (array) $daoInstance->getCacheMethods();
-		foreach ($caches as $classMethod => $classPath) {
-			if (!$classMethod) continue;
-			if ($classPath === 'default')
-				$_className = Wind::import('COM:dao.listener.WindDaoCacheListener');
-			else
-				$_className = Wind::import($classPath);
-			if (!$_className) continue;
-			$daoInstance->registerEventListener($classMethod, new $_className($daoInstance));
-		}
-	}
-
-	/**
-	 * 创建db链接句柄
-	 * 
-	 * @param WindDao $daoObject
-	 */
-	protected function createDbConnection($daoObject) {
-		$configName = $daoObject->getDBName();
-		$config = $this->getSystemConfig()->getDbConfig($configName);
-		if (!$config) throw new WindDbException(
-			'[component.dao.WindDaoFactory.createDbConnection] (' . $configName . ')', 
-			WindDbException::DB_CONN_NOT_EXIST);
-		
-		$path = $this->getConfig('class', '', 'COM:db.WindConnection', $config);
-		$alias = $configName ? $path . $configName : $path . get_class($this);
-		$definition = array('path' => $path, 'alias' => $alias, 'config' => $config, 'initMethod' => 'init', 'scope' => 'application');
-		$this->getSystemFactory()->addClassDefinitions($alias, $definition);
-		$daoObject->setDelayAttributes(array('connection' => array('ref' => $alias)));
-	}
-
-	/**
-	 * 创建cache句柄
-	 * 
-	 * @param WindDao $daoObject
-	 */
-	protected function createCacheHandler($daoObject) {
-		if (!($_className = $daoObject->getCacheClass())) return;
-		$_classConfig = $daoObject->getCacheConfig();
-		$_alias = $_className . '_' . md5((is_string($_classConfig) ? $_classConfig : serialize($_classConfig)));
-		if (!$this->getSystemFactory()->checkAlias($_alias)) {
-			$definition = array('path' => $_className, 'alias' => $_alias, 'initMethod' => 'init', 'scope' => 'singleton');
-			$definition['config'] = is_array($_classConfig) ? $_classConfig : array('resource' => $_classConfig);
-			$this->getSystemFactory()->addClassDefinitions($_alias, $definition);
-		}
-		$daoObject->setDelayAttributes(array('cacheHandler' => array('ref' => $_alias)));
-		$daoObject = new WindClassProxy($daoObject);
-		$this->registerCacheListener($daoObject);
 	}
 }
 ?>
