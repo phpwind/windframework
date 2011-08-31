9361de3a6500ee83699f91222bad4080cd06f2d4
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-08-31 10:19:26 +0000

    功能实现：dbmanager
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2532 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/dao/WindDao.php
+++ wind/dao/WindDao.php
@@ -17,12 +17,8 @@ class WindDao extends WindModule {
 	 * 根据用户配置决定配置是采用配置链接管理
 	 * @return WindConnection
 	 */
-	public function getConnection($name = '') {
-		$this->_getConnection();
-		if ($this->connection instanceof WindConnectionManager) {
-			return $this->connection->getConnection($name);
-		}
-		return $this->connection;
+	public function getConnection() {
+		return $this->_getConnection();
 	}
 
 	/**
