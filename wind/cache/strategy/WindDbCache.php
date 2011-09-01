9062f640ba415c6b02d7bb194c213172327a2905
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-01 06:42:12 +0000

    代码优化： db链接获取方式修改
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2537 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/cache/strategy/WindDbCache.php
+++ wind/cache/strategy/WindDbCache.php
@@ -8,31 +8,31 @@ Wind::import('WIND:cache.AbstractWindCache');
  * @version 2011-7-26  xiaoxiao
  */
 class WindDbCache extends AbstractWindCache {
-
+	
 	/**
 	 * 链接句柄
 	 * @var AbstractWindDbAdapter 
 	 */
-	private $connection;
-
+	protected $connection;
+	
 	/**
 	 * 缓存表
 	 * @var string 
 	 */
 	private $table = 'pw_cache';
-
+	
 	/**
 	 * 缓存表的键字段
 	 * @var string 
 	 */
 	private $keyField = 'key';
-
+	
 	/**
 	 * 缓存表的值字段
 	 * @var string 
 	 */
 	private $valueField = 'value';
-
+	
 	/**
 	 * 缓存表过期时间字段
 	 * @var string 
@@ -62,7 +62,7 @@ class WindDbCache extends AbstractWindCache {
 	 */
 	protected function getValue($key) {
 		$sql = 'SELECT * FROM ' . $this->getTableName() . ' WHERE `' . $this->keyField . '` =? AND (`' . $this->expireField . '`=0 OR `' . $this->expireField . '`>?)';
-		$data = $this->getConnection()->createStatement($sql)->getOne(array($key, time())); 
+		$data = $this->getConnection()->createStatement($sql)->getOne(array($key, time()));
 		return $data[$this->valueField];
 	}
 
@@ -74,7 +74,8 @@ class WindDbCache extends AbstractWindCache {
 			$keys[$key] = $this->buildSecurityKey($value);
 		}
 		list($sql, $result) = array('', array());
-		$sql = 'SELECT * FROM ' . $this->getTableName() . ' WHERE `' . $this->keyField . '` IN ' . $this->getConnection()->quoteArray($keys) . ' AND (`' . $this->expireField . '`=0 OR `' . $this->expireField . '`>?)';
+		$sql = 'SELECT * FROM ' . $this->getTableName() . ' WHERE `' . $this->keyField . '` IN ' . $this->getConnection()->quoteArray(
+			$keys) . ' AND (`' . $this->expireField . '`=0 OR `' . $this->expireField . '`>?)';
 		$data = $this->getConnection()->createStatement($sql)->queryAll(array(time()));
 		foreach ($data as $tmp) {
 			$result[] = $this->formatData(array_search($tmp[$this->keyField], $keys), $tmp[$this->valueField]);
@@ -97,7 +98,8 @@ class WindDbCache extends AbstractWindCache {
 		foreach ($keys as $key => $value) {
 			$keys[$key] = $this->buildSecurityKey($value);
 		}
-		$sql = 'DELETE FROM ' . $this->getTableName() . ' WHERE `' . $this->keyField . '` IN ' . $this->getConnection()->quoteArray($keys);
+		$sql = 'DELETE FROM ' . $this->getTableName() . ' WHERE `' . $this->keyField . '` IN ' . $this->getConnection()->quoteArray(
+			$keys);
 		return $this->getConnection()->execute($sql);
 	}
 
@@ -119,15 +121,16 @@ class WindDbCache extends AbstractWindCache {
 		$this->expireField = $this->getConfig('field-expire', '', 'expire', $config);
 		$this->dbConfigName = $this->getConfig('dbconfig-name', '', '', $config);
 	}
-	
+
 	/**
 	 * 设置链接对象
 	 * @param WindConnection $connection
 	 */
 	public function setConnection($connection) {
-		if ($connection instanceof WindConnection) $this->connection = $connection;
+		if ($connection instanceof WindConnection)
+			$this->connection = $connection;
 	}
-	
+
 	/**
 	 * 返回表名
 	 * @return string
@@ -135,7 +138,7 @@ class WindDbCache extends AbstractWindCache {
 	private function getTableName() {
 		return $this->table;
 	}
-	
+
 	/**
 	 * 存储数据
 	 * @param string $key
@@ -145,27 +148,17 @@ class WindDbCache extends AbstractWindCache {
 	 * @return boolean
 	 */
 	private function store($key, $value, $expires = 0) {
-		($expires > 0) ? $expires += time() : $expire=0;
+		($expires > 0) ? $expires += time() : $expire = 0;
 		$db = array($this->keyField => $key, $this->valueField => $value, $this->expireField => $expires);
-	    $sql = 'REPLACE INTO ' . $this->getTableName() . ' SET ' . $this->getConnection()->sqlSingle($db);
+		$sql = 'REPLACE INTO ' . $this->getTableName() . ' SET ' . $this->getConnection()->sqlSingle($db);
 		return $this->getConnection()->createStatement($sql)->update();
 	}
-	
+
 	/**
 	 * 获得链接对象
 	 * @return WindConnection 
 	 */
 	private function getConnection() {
-		if (null == $this->connection) {
-			$config = $this->getSystemConfig()->getDbConfig($this->dbConfigName);
-			$path = $this->getConfig('class', '', 'WIND:db.WindConnection', $config);
-			$alias = $this->dbConfigName ? $path . $this->dbConfigName : $path . get_class($this);
-		    if (!$this->getSystemFactory()->checkAlias($alias)) {
-				$definition = array('path' => $path, 'alias' => $alias, 'config' => $config, 'initMethod' => 'init', 'scope' => 'application');
-				$this->getSystemFactory()->addClassDefinitions($alias, $definition);
-			}
-			$this->connection = $this->getSystemFactory()->getInstance($alias);
-		}
-		return $this->connection;
+		return $this->_getConnection();
 	}
 }
