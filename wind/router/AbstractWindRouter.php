94f0695e0032741c1f468f24d15f68d0a4ed7803
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-05 04:22:48 +0000

    功能实现：createUrl
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2550 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/router/AbstractWindRouter.php
+++ wind/router/AbstractWindRouter.php
@@ -16,6 +16,7 @@ abstract class AbstractWindRouter extends WindHandlerInterceptorChain {
 	protected $module;
 	protected $controller = 'index';
 	protected $action = 'run';
+	protected $reverse = "%s?m=%s&c=%s&a=%s&";
 	/**
 	 * @var AbstractWindRoute
 	 */
