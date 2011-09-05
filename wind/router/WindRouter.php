6c4f54b3244a374768bbaf25c54a71cae65abf24
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-05 04:17:42 +0000

    功能实现：createUrl
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2548 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/router/WindRouter.php
+++ wind/router/WindRouter.php
@@ -20,8 +20,16 @@ class WindRouter extends AbstractWindRouter {
 	/* (non-PHPdoc)
 	 * @see AbstractWindRouter::assemble()
 	 */
-	public function assemble() {
-		// TODO Auto-generated method stub
+	public function assemble($action, $args = array(), $route = null) {
+		if ($route !== null)
+			return $route->build($action, $args);
+		if ($this->currentRoute !== null)
+			return $this->currentRoute->build($action, $args);
+		list($_a, $_c, $_m, $args) = WindUrlHelper::resolveAction($action, $args);
+		$_baseUrl = $this->getRequest()->getBaseUrl(true) . '/' . $this->getRequest()->getScript();
+		$_url = sprintf($_baseUrl . "?$this->moduleKey=%s&$this->controllerKey=%s&$this->actionKey=%s&%s", 
+			($_m ? $_m : $this->module), ($_c ? $_c : $this->controller), ($_a ? $_a : $this->action), $args);
+		return WindUrlHelper::checkUrl($_url);
 	}
 
 	/**
@@ -29,8 +37,7 @@ class WindRouter extends AbstractWindRouter {
 	 */
 	public function defaultRoute() {
 		$params[$this->actionKey] = $this->getRequest()->getRequest($this->actionKey, $this->action);
-		$params[$this->controllerKey] = $this->getRequest()->getRequest($this->controllerKey, 
-			$this->controller);
+		$params[$this->controllerKey] = $this->getRequest()->getRequest($this->controllerKey, $this->controller);
 		$params[$this->moduleKey] = $this->getRequest()->getRequest($this->moduleKey, $this->module);
 		return $params;
 	}
