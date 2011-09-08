1c55c5176a9c0ff37d510d9fb8cd874a3d573fbe
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-08 09:15:08 +0000

    session实现
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2598 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/http/session/AbstractWindSession.php
+++ wind/http/session/AbstractWindSession.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @author Qian Su <aoxue.1988.su.qian@163.com> 2010-12-17
- * @link http://www.phpwind.com
- * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
- */
 
 /**
  * 用户定义session存储机制
@@ -13,81 +7,106 @@
  * @version $Id: AbstractWindUserSession.php 1704 2011-03-08 10:40:17Z weihu $ 
  * @package 
  */
-abstract class  AbstractWindSession extends WindModule {
+abstract class AbstractWindSession extends WindModule {
+
 	protected $handler = null;
 	
 	/**
+	 * 构造函数
+	 * @param AbstractWindCache $handler
+	 */
+	public function __construct(AbstractWindCache $handler = null) {
+		$handler && $this->setHandler($handler);
+		register_shutdown_function('session_write_close');
+	}
+
+	/**
 	 * 打开会话存储机制
+	 * 在session_start()执行的时候执行。
+	 * 
 	 * @param string $savePath
 	 * @param string $sessionName
 	 * @return  bollean
 	 */
 	public abstract function open($savePath, $sessionName);
+
 	/**
 	 * 关闭会话存储存储机制
+	 * 在页面执行完的时候执行
+	 * 
 	 * @return  bollean
 	 */
 	public abstract function close();
+
 	/**
 	 * 将sessionID对应的数据写到存储
+	 * 在需要写入session数据的时候执行
+	 * 
 	 * @param string $name
 	 * @param mixed $value
 	 */
 	public abstract function write($sessId, $sessData);
+
 	/**
 	 * 从存储中装载session数据
+	 * 在执行session_start的时候执行在open之后
+	 * 
 	 * @param mixed $sessid
 	 */
 	public abstract function read($sessId);
+
 	/**
 	 * 对存储系统中的数据进行垃圾收集
+	 * 在执行session过期策略的时候执行，注意，session的过期并不是时时的，需要根据php.ini中的配置项：
+	 * session.gc_probability = 1
+	 * session.gc_divisor = 1000  
+	 * 执行的概率是gc_probability/gc_divisor .
+	 * session.gc_maxlifetime = 1440  设置的session的过期时间
+	 * 
 	 * @param mixed $maxlifetime
 	 */
 	public abstract function gc($maxlifetime);
+
 	/**
 	 * 破坏与指定的会话ID相关联的数据
+	 * 在执行session_destroy的时候执行。
+	 * 
 	 * @param mixed $name
 	 */
 	public abstract function destroy($sessId);
-	
+
 	/**
-	 * 数据序列化
-	 * @param mixed $sessData
-	 * @return string
+	 * 开启session
+	 * 
+	 * @param string $id
 	 */
-	protected function serializeData($sessData) {
-		return (is_array($sessData) || is_object($sessData)) ? serialize($sessData) : $sessData;
+	public function start() {
+		$this->getHandler() && $this->setSessionHandler();
+		if ('' === session_id() && '1' !== ini_get('session.auto_start')) {
+			session_start();
+		}
 	}
-	
+
 	/**
-	 * 数据反序列化
-	 * @param string $sessData
-	 * @return mixed
+	 * 设置session的回调函数
 	 */
-	protected function unserializeData($sessData) {
-		$data = unserialize($sessData);
-		return (is_array($data) || is_object($data)) ? $data : $sessData;
+	public function setSessionHandler() {
+		session_set_save_handler(array($this, 'open'), array($this, 'close'), array($this, 'read'), array($this, 
+			'write'), array($this, 'destroy'), array($this, 'gc'));
 	}
-	
-	/**
-	 * 设置监听接口
-	 */
-	public function setSessionHandler(){
-		session_set_save_handler(array($this,'open'),array($this,'close'),array($this,'read'),array($this,'write'),array($this,'destroy'),array($this,'gc'));
-	}
-	
 
 	/**
 	 * 设置链接对象
+	 * 
 	 * @param AbstractWindCache $handler
 	 */
 	public function setHandler($handler) {
-		if ($handler instanceof AbstractWindCache)
-			$this->handler = $handler;
+		if ($handler instanceof AbstractWindCache) $this->handler = $handler;
 	}
-	
+
 	/**
 	 * 获得链接对象
+	 * 
 	 * @return AbstractWindCache
 	 */
 	public function getHandler() {
--- wind/http/session/WindSession.php
+++ wind/http/session/WindSession.php
@@ -1,370 +1,95 @@
 <?php
+Wind::import('WIND:http.session.AbstractWindSession');
 /**
- * @author Qian Su <aoxue.1988.su.qian@163.com> 2010-12-17
- * @link http://www.phpwind.com
- * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
- */
-
-/**
- * Session会话操作
+ * 会话机制，依赖Cache机制实现，应用可以根据自己的需求配置需要的存储方式实现会话存储
+ * 【配置】支持组件配置格式:
+ * <pre>
+ * 'WindSession' => array(
+ *		'path' => 'WIND:http.session.WindSession',
+ *		'scope' => 'singleton',
+ *		'properties' => array(
+ *			'handler' => array(
+ *			    'ref' => 'sessionSave',//用户配置的缓存类型--缓存组件的配置格式参照缓存配置文件
+ *			),
+ *		),
+ *  )
+ * </pre>
+ * 【使用】调用时使用：
+ * <pre>
+ * $session = $this->getSystemFactory()->getInstance('WindSession');
+ * $session->start();
+ * 
+ * $_SESSION['name'] = 'test';
+ * echo $_SESSION['name'];
+ * </pre>
+ * 【使用原生】：
+ * 如果用户不需要配置自己其他存储方式的session，则不许要修改任何调用，只要在WindSession的配置中将properties配置项去掉即可。如下：
+ * <pre>
+ * 'WindSession' => array(
+ *		'path' => 'WIND:http.session.WindSession',
+ *		'scope' => 'singleton',
+ *  )
+ * </pre>
+ * 【扩展】
+ * 如果用户实现了自己的实现，需要调用自己的实现，则只需要更改path的值指定到自己的实现，即可。
+ * 
+ *
  * the last known user to change this file in the repository  <$LastChangedBy$>
- * @author Qian Su <aoxue.1988.su.qian@163.com>
- * @version $Id$ 
- * @package 
+ * @author xiaoxia.xu <xiaoxia.xuxx@aliyun-inc.com>
+ * @version $Id$
+ * @package
  */
-class WindSession implements IteratorAggregate, ArrayAccess, Countable {
-	/**
-	 * @var boolean 是否自动启动session
-	 */
-	public $autostart = false;
-	/**
-	 * @var int 没有启用cookie传用sessionid
-	 */
-	const COOKIE_MODE_NONE = 1;
-	/**
-	 * @var int 仅仅启用cookiew传递sessionid
-	 */
-	const COOKIE_MODE_ONLY = 2;
-	/**
-	 * @var int 启用cookie传用sessionid
-	 */
-	const COOKIE_MODE_ALLOW = 3;
+class WindSession extends AbstractWindSession {
 	
-	/**
-	 * @var string 以files格式将session在服务端的保存
+	/* (non-PHPdoc)
+	 * @see AbstractWindSession::open()
 	 */
-	const SESSION_SAVE_FILES = 'files';
-	/**
-	 * @var string  以user(用户自定义)格式将session在服务端的保存
-	 */
-	const SESSION_SAVE_USER = 'user';
-	
-	/**
-	 * @var array $read 只读session
-	 */
-	public static $read = array();
-	/**
-	 * @var array $write 只写session
-	 */
-	public static $write = array();
-	
-	public function __construct($autostart = false) {
-		$this->autostart = $autostart;
-	}
-	
-	public function start() {
-		if (!$this->isStart() && !$this->getAutoStart()) {
-			$this->autostart ? $this->setAutoStart(1) : session_start();
+	public function open($savePath, $sessionName) {
+		$handler = $this->getHandler();
+		$lifeTime = get_cfg_var("session.gc_maxlifetime");
+		if (($expire = $handler->getExpire()) == '0') {
+			$lifeTime = get_cfg_var("session.gc_maxlifetime");
+			$handler->setExpire($lifeTime ? $lifeTime : 0);
+		} else {
+			ini_set("session.gc_maxlifetime", $expire);
 		}
+		return true;
 	}
-	
-	/**
-	 * session是否开启
-	 * @return boolean
-	 */
-	public function isStart() {
-		return '' !== $this->getSessionId();
-	}
-	
-	/**
-	 * 写入和结束session
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindSession::close()
 	 */
 	public function close() {
-		if ($this->isStart()) {
-			session_write_close();
-		}
-	}
-	
-	/**
-	 * 获取session
-	 * @param string $name session名称
-	 * @return string
-	 */
-	public function get($name) {
-		return isset($_SESSION[$name]) ? $_SESSION[$name] : null;
-	}
-	
-	/**
-	 * 设置一个会话
-	 * @param string $name session名称
-	 * @param string $value $name对应的值
-	 * @return string
-	 */
-	public function set($name, $value) {
-		if (empty($name) && empty($value)) {
-			return false;
-		}
-		$_SESSION[$name] = $value;
+		session_write_close();
 		return true;
 	}
-	
-	/**
-	 * 删除一个会话
-	 * @param string $name  session名称
-	 * @return string
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindSession::write()
 	 */
-	public function remove($name) {
-		if (isset($_SESSION[$name])) {
-			$sessionValue = $_SESSION[$name];
-			unset($_SESSION[$name]);
-			return $sessionValue;
-		}
-		return null;
+	public function write($sessID, $sessData) {
+		return $this->getHandler()->set($sessID, $sessData);
 	}
-	
-	/**
-	 * 判断一个session是否存在
-	 * @param string $name session名称
-	 * @return string
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindSession::read()
 	 */
-	public function exist($name) {
-		return isset($_SESSION[$name]);
+	public function read($sessID) {
+		return $this->getHandler()->get($sessID);
 	}
-	
-	/**
-	 * 销毁当前所有会话
-	 * @return string
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindSession::gc()
 	 */
-	public function destroy() {
-		if (($name = $this->getSessionName()) && isset($_COOKIE[$name])) {
-			setcookie($name, '', time() - 3600);
-		}
-		session_unset();
-		session_destroy();
-		return true;
+	public function gc($maxlifetime) {
+		return $this->getHandler()->clear(true);
 	}
-	
-	/**
-	 * 获取当前会话名称
-	 * @return string
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindSession::destroy()
 	 */
-	public function getSessionName() {
-		return session_name();
-	}
-	/**
-	 * 设置当前会话名称
-	 * @return string $name
-	 */
-	public function setSessionName($name) {
-		return session_name($name);
-	}
-	
-	/**
-	 * 获取当前会话 id
-	 * @return string
-	 */
-	public function getSessionId() {
-		return session_id();
-	}
-	
-	/**
-	 * 设置当前会话 id
-	 * @param string $id
-	 * @return string
-	 */
-	public function setSessionId($id) {
-		return session_id($id);
-	}
-	
-	/**
-	 * 如果session在服务端的以files方式保存,获取session在服务器端存储路径
-	 * @return string
-	 */
-	public function getSavePath() {
-		return session_save_path();
-	}
-	
-	/**
-	 * 如果session在服务端的以files方式保存,设置session在服务器端存储路径
-	 * @param string $path 
-	 * @return string
-	 */
-	public function setSavePath($path) {
-		if (is_dir($path)) {
-			session_save_path($path);
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * 获取session在服务端的保存方式
-	 * @return string
-	 */
-	public function getSessionSaveMode() {
-		return session_module_name();
-	}
-	
-	/**
-	 * 定义session在服务端的保存方式，files意为把sesion保存到一个临时文件里，如果我们想自定义别的方式保存（比如用数据库），则需要把该项设置为user；
-	 * @param unknown_type $mode
-	 * @return string
-	 */
-	public function setSessionSaveMode($mode = self::SESSION_SAVE_FILES) {
-		return session_module_name($mode);
-	}
-	
-	/**
-	 * 取得session相关的cookie参数
-	 * @return array
-	 */
-	public function getCookieParams() {
-		return session_get_cookie_params();
-	}
-	
-	/**
-	 * 设置session相关的cookie参数
-	 * @param array $cookie
-	 * @return string
-	 */
-	public function setCookieParams($cookie = array()) {
-		extract($this->getCookieParams());
-		extract($cookie);
-		if (isset($httponly)) {
-			session_set_cookie_params($lifetime, $path, $domain, $secure, $httponly);
-		} else {
-			session_set_cookie_params($lifetime, $path, $domain, $secure);
-		}
-		return true;
-	}
-	
-	/**
-	 * 取得cookie传递sessionid的模式
-	 * @return number
-	 */
-	public function getCookieMode() {
-		if ('0' === ini_get('session.use_cookies')) {
-			self::COOKIE_MODE_NONE;
-		} else if ('0' === ini_get('session.use_only_cookies')) {
-			return self::COOKIE_MODE_ALLOW;
-		} else {
-			return self::COOKIE_MODE_ONLY;
-		}
-		return false;
-	}
-	
-	/**
-	 * 设置cookie传递sessionid的模式
-	 * @param int $mode
-	 * @return string
-	 */
-	public function setCookieMode($mode = self::COOKIE_MODE_ONLY) {
-		if (self::COOKIE_MODE_NONE === $mode) {
-			ini_set('session.use_cookies', '0');
-		} else if (self::COOKIE_MODE_ALLOW === $mode) {
-			ini_set('session.use_cookies', '1');
-			ini_set('session.use_only_cookies', '0');
-		} else if (self::COOKIE_MODE_ONLY === $mode) {
-			ini_set('session.use_cookies', '1');
-			ini_set('session.use_only_cookies', '1');
-		} else {
-			return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * 获取session进行清理的概率
-	 * @return number
-	 */
-	public function getGCProbability() {
-		return (int) ini_get('session.gc_probability');
-	}
-	
-	/**
-	 * 设置session进行清理的概率
-	 * @param int $probability 概率数
-	 * @return string|string
-	 */
-	public function setGCProbability($probability) {
-		if (!is_int($probability) || 0 >= $probability || 100 <= $probability) {
-			return false;
-		}
-		ini_set('session.gc_probability', $probability);
-		ini_set('session.gc_divisor', '100');
-		return true;
-	}
-	
-	/**
-	 * 是否允许sessionid通过url参数传递
-	 * @return boolean
-	 */
-	public function getTransSessionID() {
-		return '1' === ini_get('session.use_trans_sid');
-	}
-	
-	/**
-	 * 设置是否允许sessionid通过url参数传递
-	 * @param int $ifTrans
-	 * @return string
-	 */
-	public function setTransSessionID($ifTrans = 0) {
-		return ini_set('session.use_trans_sid', $ifTrans ? '1' : '0');
-	}
-	
-	/**
-	 * 获取session存活时间 
-	 * @return number
-	 */
-	public function getSessionLifeTime() {
-		return (int) ini_get('session.gc_maxlifetime');
-	}
-	
-	/**
-	 * 设置session存活时间
-	 * @param int $time
-	 * @return number
-	 */
-	public function setSessionLifeTime($time = 0) {
-		return (int) ini_set('session.gc_maxlifetime', (int) $time);
-	}
-	
-	/**
-	 * 是否自动启动session
-	 * @return boolean
-	 */
-	public function getAutoStart() {
-		return '1' === ini_get('session.auto_start');
-	}
-	
-	/**
-	 * 设置自动启动
-	 * @param boolean $autostart 是否自动启动
-	 * @return string
-	 */
-	public function setAutoStart($autostart) {
-		return ini_set('session.auto_start', $autostart ? '1' : '0');
-	}
-	
-	/**
-	 * 获取当前session的文件名
-	 * @return string
-	 */
-	public function getCurrentSessionFileName(){
-		return $this->getSavePath().'/sess_'.$this->getSessionId();
-	}
-	
-	public function offsetExists($offset) {
-		$this->exist($offset);
-	}
-	
-	public function offsetSet($offset, $value) {
-		$this->set($offset, $value);
-	}
-	
-	public function offsetGet($offset) {
-		$this->get($offset);
-	}
-	public function offsetUnset($offset) {
-		$this->remove($offset);
-	}
-	
-	public function getIterator($name = null) {
-		return new ArrayObject(($name && isset($_SESSION[$name])) ? $_SESSION[$name] : $_SESSION);
-	}
-	
-	public function count() {
-		return count($_SESSION);
+	public function destroy($sessID) {
+		return $this->getHandler()->delete($sessID);
 	}
 }
+
