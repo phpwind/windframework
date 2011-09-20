6e6e1dcd87ed0eb3b065505bacc9be625ae1799c
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-20 03:30:26 +0000

    docs: 注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2732 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/viewer/WindLayout.php
+++ wind/viewer/WindLayout.php
@@ -28,7 +28,7 @@
  *
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
- * @copyright Copyright &copy; 2003-2103 phpwind.com
+ * @copyright ©2003-2103 phpwind.com
  * @license http://www.windframework.com
  * @version $Id$
  * @package wind.viewer
--- wind/viewer/WindViewerResolver.php
+++ wind/viewer/WindViewerResolver.php
@@ -22,7 +22,7 @@ Wind::import('WIND:viewer.exception.WindViewException');
  * 
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
- * @copyright Copyright &copy; 2003-2103 phpwind.com
+ * @copyright ©2003-2103 phpwind.com
  * @license http://www.windframework.com
  * @version $Id$
  * @package wind.viewer
@@ -162,7 +162,7 @@ class WindViewerResolver extends WindModule implements IWindViewerResolver {
  * 
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
- * @copyright Copyright &copy; 2003-2103 phpwind.com
+ * @copyright ©2003-2103 phpwind.com
  * @license http://www.windframework.com
  * @version $Id$
  * @package wind.viewer
