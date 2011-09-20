ec2b793adf7e1b56d6e60d06e9b7755cd223ddd9
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-20 17:17:49 +0000

    docs: 注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2744 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/WindErrorMessage.php
+++ wind/base/WindErrorMessage.php
@@ -1,16 +1,30 @@
 <?php
 /**
+ * 错误消息类
+ * 
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
  * @version $Id$
  * @package
  */
 class WindErrorMessage extends WindModule implements IWindErrorMessage {
+	/**
+	 * 用于存储错误信息
+	 * 
+	 * @var array
+	 */
 	private $error = array();
+	/**
+	 * 用于处理当前错误的action操作
+	 * 
+	 * 用于处理当前错误的action操作,当该值为空时,系统自动调用默认错误处理类,可以通过配置改变默认的错误处理类
+	 * @var string
+	 */
 	private $errorAction;
 
 	/**
-	 * @param string $message
+	 * @param string $message 错误消息 默认为空
+	 * @param string $errorAction 用于处理当前错误的Action操作 默认为空
 	 */
 	public function __construct($message = '', $errorAction = '') {
 		$message !== '' && $this->addError($message);
@@ -21,8 +35,7 @@ class WindErrorMessage extends WindModule implements IWindErrorMessage {
 	 * @see IWindErrorMessage::sendError()
 	 */
 	public function sendError() {
-		if (empty($this->error))
-			return;
+		if (empty($this->error)) return;
 		throw new WindActionException($this);
 	}
 
@@ -37,8 +50,7 @@ class WindErrorMessage extends WindModule implements IWindErrorMessage {
 	 * @see IWindErrorMessage::getError()
 	 */
 	public function getError($key = '') {
-		if ($key === '')
-			return $this->error;
+		if ($key === '') return $this->error;
 		return isset($this->error[$key]) ? $this->error[$key] : '';
 	}
 
@@ -51,30 +63,35 @@ class WindErrorMessage extends WindModule implements IWindErrorMessage {
 				$this->error[] = $error;
 			elseif (is_object($error))
 				$error = get_object_vars($error);
-			if (is_array($error))
-				$this->error = array_merge($this->error, $error);
+			if (is_array($error)) $this->error = array_merge($this->error, $error);
 		} else
 			$this->error[$key] = $error;
 	}
 
 	/**
-	 * @return the $errorAction
+	 * 返回用于处理当前错误的action操作
+	 * 
+	 * @return string
 	 */
 	public function getErrorAction() {
 		return $this->errorAction;
 	}
 
 	/**
-	 * /module/controller/action/?a=b&c=a
+	 * 设置用于处理当前错误的action操作
+	 * 
+	 * <i>$errorAction</i>支持的输入格式<code>/module/controller/action/?args</code>
 	 * @param string $errorAction
+	 * @return void
 	 */
 	public function setErrorAction($errorAction) {
 		$this->errorAction = $errorAction;
 	}
-
 }
 
 /**
+ * 错误消息类接口定义
+ * 
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
  * @version $Id$
@@ -83,25 +100,35 @@ class WindErrorMessage extends WindModule implements IWindErrorMessage {
 interface IWindErrorMessage {
 
 	/**
-	 * 添加错误信息
-	 * @param string $message
-	 * @param string $key
+	 * 添加一条错误信息
+	 * 
+	 * 添加一条错误信息,可存储多条错误信息,以数组方式存储错误信息,当key值为空时,消息索引为自然索引
+	 * @param string $message 错误信息
+	 * @param string $key key值 默认为空
 	 */
 	public function addError($message, $key = '');
 
 	/**
-	 * 获得一条Error信息
+	 * 返回错误信息
+	 * 
+	 * 返回错误信息,当key为空时,返回全部的错误信息
 	 * @param string $key
+	 * @return string|array
 	 */
 	public function getError($key = '');
 
 	/**
-	 * 清空Error信息
+	 * 清空当前错误对象中的全部错误信息
+	 * 
+	 * @return void
 	 */
 	public function clearError();
 
 	/**
 	 * 发送错误信息
+	 * 
+	 * @return void
+	 * @throws WindActionException
 	 */
 	public function sendError();
 }
