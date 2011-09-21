96d94e4b71377f9953cd38ba3001acb1187936ce
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-21 05:30:28 +0000

    docs: 注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2753 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/IWindFactory.php
+++ wind/base/IWindFactory.php
@@ -2,29 +2,33 @@
 /**
  * 类工厂接口定义
  * 
- * the last known user to change this file in the repository  <$LastChangedBy$>
+ * 类工厂接口类主要有两个接口方法<i>getInstance,createInstance</i>.
+ * 'getInstance'创建并返回类的实例对象,'createInstance'静态方法,用于创建类对象.
  * @author Qiong Wu <papa0924@gmail.com>
- * @version $Id$ 
- * @package 
+ * @copyright ©2003-2103 phpwind.com
+ * @license http://www.windframework.com
+ * @version $Id$
+ * @package wind.base
  */
 interface IWindFactory {
 
 	/**
-	 * 类的实例对象
+	 * 创建并返回类的实例对象
 	 * 
-	 * 通过调用该方法,获取类的实力变量
-	 * @param string $classAlias
-	 * @return instance
+	 * 通过调用该方法,获取类的实例对象.当类的实例对象不存在时调用{@link createInstance}方法创建.
+	 * 通过该方法创建类对象,需要确定该类的组件定义已经被加载.如果未被加载则返回一个null.
+	 * @param string $classAlias 类别名 组件定义名称 必须填写
+	 * @return instance 返回类实例对象
 	 */
 	public function getInstance($classAlias);
 
 	/**
-	 * 根据类名称创建类对象
-	 * 返回一个类类型的实例对象，通过此方法创建类实例，并不能自动获取类路径信息
+	 * 创建并返回类对象
 	 * 
+	 * 返回一个类类型的实例对象，通过此方法创建类实例，并不能自动获取类路径信息
 	 * @param string $className 类名称
 	 * @param array $args 类参数信息
-	 * @return Object 返回的类类型的实例对象
+	 * @return Object 返回类的实例对象
 	 */
 	static public function createInstance($className, $args = array());
 }
