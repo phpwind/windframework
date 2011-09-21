b8e40d12c2b579c1c799f1559351f5a78fee1fda
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-21 03:54:11 +0000

    docs: 注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2746 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/IWindFactory.php
+++ wind/base/IWindFactory.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * 类工厂接口定义
+ * 
  * the last known user to change this file in the repository  <$LastChangedBy$>
  * @author Qiong Wu <papa0924@gmail.com>
  * @version $Id$ 
@@ -9,32 +10,21 @@
 interface IWindFactory {
 
 	/**
-	 * 描述：根据类的别名获得一个类实例变量
-	 * 返回：类的实例对象
+	 * 类的实例对象
 	 * 
-	 * 该方法首先通过类的别名到<b>类的配置文件中</b>找到类的相关配置信息，
-	 * 加载类的路径并创建类的依赖
-	 * 
+	 * 通过调用该方法,获取类的实力变量
 	 * @param string $classAlias
 	 * @return instance
 	 */
 	public function getInstance($classAlias);
 
 	/**
-	 * 根据类的别名返回一个类的实例变量的clone
-	 * 
-	 * @param string $classAlias
-	 * @return clone of instance
-	 */
-	public function getPrototype($classAlias);
-
-	/**
 	 * 根据类名称创建类对象
 	 * 返回一个类类型的实例对象，通过此方法创建类实例，并不能自动获取类路径信息
 	 * 
-	 * @param string $className | 类名称
-	 * @param array $args	| 类参数信息
-	 * @return Object | 返回的类类型的实例对象
+	 * @param string $className 类名称
+	 * @param array $args 类参数信息
+	 * @return Object 返回的类类型的实例对象
 	 */
 	static public function createInstance($className, $args = array());
 }
