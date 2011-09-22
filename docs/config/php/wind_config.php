659a5e967cc3ef0e19fa6725ff7548c1d2a10489
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-22 10:35:18 +0000

    配置文件更新
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2791 18ba2127-5a84-46d4-baec-3457e417f034

--- docs/config/php/wind_config.php
+++ docs/config/php/wind_config.php
@@ -1,74 +1,59 @@
 <?php
-/**
- * @author xiaoxia xu <xiaoxa.xuxx@aliyun-inc.com> 2011-3-8
- * @link http://www.phpwind.com
- * @copyright Copyright &copy; 2003-2110 phpwind.com
- * @license 
- */
 return array(
      /*配置应用项名为default*/
 	'default' => array(
 		'class' => 'windWebApp',
+
+		/*过滤链配置，可以配置通用的，同时也可以配置Module级别的，controller级别的，action级别的；*/
 		'filters' => array(
-			'class' => 'WIND:core.filter.WindFilterChain',
-			'filter1' => array('class' => 'WIND:core.web.filter.WindLoggerFilter'),
-			'filter2' => array('class' => 'WIND:core.web.filter.WindUrlFilter'),
+			/*过滤器的配置*/
+			'formfilter' => array(
+				/*过滤器实现*/
+				'class' => 'WIND:web.filter.WindFormFilter',
+				/*过滤器适用的范围：
+		    	 * *：则所有的请求都将会应用该过滤器
+		    	 * module*: 则所有module模块的请求都将会应用该过滤器
+		    	 * module_index*: 则module模块下的indexController下的所有Action请求都将会应用该过滤器
+		    	 * module_index_add*: 则module模块下的indexController下的addAction将会应用该过滤器
+		    	*/
+				'pattern' => '*',
+				/*使用框架提供的formFilter的时候配置给该filter指明需要使用的form*/
+				'form' => 'MyForm',
+				/*其他配置项：将会传递给配置的filter相同配置项的属性*/
+			)
 		),
-            /*配置default应用的路由规则*/
-		'router' => array(
-			'class' => 'urlBasedRouter',
-            'config' => array(
-            /*配置路径中module的规则*/
-        	   	 'module' => array(
-	        	     'url-param' => 'm',
-	            	 'default-value' => 'default',
-             	 ),
-           /*配置路径中controller的规则*/
-                 'controller' => array(
-                     'url-param' => 'c',
-                     'default-value' => 'index',
-                  ),
-             /*配置路径中action的规则*/
-                  'action' => array(
-                     'url-param' => 'a',
-                     'default-value' => 'run',
-                  ),
-            ),
-		),
-			/*配置default应用的模块配置*/
-		'modules' => array(
-			    /*配置default模块*/
-			'default' => array(
-			       /*default模块的路径*/
-				'controller-path' => 'controller',  
-			        /*default模块的中controller的后缀*/
-				'controller-suffix' => 'Controller',
-					/*default模块中处理error的Action controller路径*/
-				'error-handler' => 'WIND:core.web.WindErrorHandler',
-					/*default模块的试图配置*/
-				'view' => array(
-					'class' => 'windView',
-					'config' => array(
-					       /*模板路径*/
-						'template-dir' => 'template',
-					       /*模板后缀*/
-						'template-ext' => 'htm',
-					       /*模板编译路径*/
-						'compile-dir' => 'compile.template',
-					),
+		
+		/*组件配置：
+		 * 组件的配置将会覆盖框架提供的组件的默认配置行为*/
+		'components' => array(
+			/** 比如我配置db组件 **/
+			/*db相关配置，如果设置了resource则系统会默认找到resource指向的文件作为db配置信息.DB的配置config内容参照DOCS下的db_config配置*/
+			'db' => array(
+				'config' => array(
+					'resource' => 'db_config.ini'
 				),
 			),
+			/*可以根据自己的需求重新配置组件的相关项*/
 		),
-		/*db配置*/
-		'db' => array(
-		//   'resource' => 'db_config.php',
-			'conn1' => array(
-				'class' => 'COM:db.WindConnection',
-				'dsn' => 'mysql:host=localhost;dbname=test',
-				'user' => 'root',
-				'pwd' => 'root',
-				'charset' => 'utf8', 
-				'tablePrefix' => 'pw_'
+		
+		/*iscache属性控制windCache组件是否可用，如果关闭则windCache组件将不可用,通过Wind::getApp()->getComponent('windCache');*/
+		'iscache' => 1,
+		
+		/*模块配置： 可以通过设定多个module来设置多组模块配置，每组模块以name来相互区分*/
+		'modules' => array(
+			 /** 比如配置default模块 **/
+			/*模块名称为default*/
+			'default' => array(
+			    /*default模块的路径*/
+				'controller-path' => 'controller',  
+			    /*default模块的中controller的后缀*/
+				'controller-suffix' => 'Controller',
+				/*配置该模块的error处理的action controller类*/
+				'error-handler' => 'WIND:web.WindErrorHandler',
+				/*default模块的视图目录配置*/
+				'template-dir' => 'template',
+				/*default模块的编译目录配置*/
+				'compile-dir' => 'data.template',
 			),
 		),
 	),
