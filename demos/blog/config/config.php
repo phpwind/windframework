8d4ce0f6ba77a4b7cbc15b3a1ded1d28332a215d
long.shi <long.shi@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-23 08:52:26 +0000

    NEW - bug 157: demos:blog/config/db_config 修改为默认配置值 http://bugs.phpwind-inc.com/show_bug.cgi?id=157
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2817 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/blog/config/config.php
+++ demos/blog/config/config.php
@@ -3,21 +3,12 @@ return array(
 	'blog' => array(
 		'components' => array(
 			'db' => array(
-				'path' => 'WIND:db.WindConnection',
-				'scope' => 'singleton', 
 				'config' => array(
 					'resource' => 'config.db_config.php',
 				),
 			),
-			'windSession' => array(
-				'path' => 'WIND:http.session.WindSession',
-				'scope' => 'singleton',
-				'destroy' => 'commit',
-			),
 			'windView' => array(
 	 			'config' => array(
-					'template-dir' => 'template',
-					'template-ext' => 'htm',
 					'is-compile' => '1',
 					),
 			),
@@ -26,18 +17,18 @@ return array(
 			'default' => array(
 				'controller-path' => 'controller',
 				'controller-suffix' => 'Controller',
-				'error-handler' => 'WIND:web.WindErrorHandler',
-				'template-dir'=> 'template',
-				'compile-dir' => 'compile.template',
+				'error-handler' => 'BLOG:controller.Error',
+			),
+			'error' => array(
+				'controller-path' => 'controller',
+				'controller-suffix' => 'Controller',
 			),
 		),
 		'filters' => array(
-			'class' => 'WIND:core.filter.WindFilterChain',
-			'filter' => array(
-				'name' => 'login',
+			'user' => array(
 				'class' => 'WIND:web.filter.WindFormFilter',
-				'pattern' => 'default_Index_post',	
-				'form' => 'BLOG:model.LoginForm',
+				'pattern' => 'default_Index_login|default_Index_register',	
+				'form' => 'BLOG:model.UserForm',
 			),
 		),
 		'iscache' => false,
--- demos/blog/config/db_config.php
+++ demos/blog/config/db_config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-	'dsn' => 'mysql:host=localhost;dbname=p9_user',
+	'dsn' => 'mysql:host=localhost;dbname=test',
 	'user' => 'root',
-	'pwd' => 'phpwind.net',
+	'pwd' => 'root',
 	'charset' => 'utf8',
 );
