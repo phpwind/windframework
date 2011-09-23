52a8d44c842ba4de7726820c861fc9cab080c98c
long.shi <long.shi@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-23 09:22:23 +0000

    NEW - bug 160: demos:blog/controller 注意docs书写规范 http://bugs.phpwind-inc.com/show_bug.cgi?id=160
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2820 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/blog/controller/IndexController.php
+++ demos/blog/controller/IndexController.php
@@ -1,48 +1,63 @@
 <?php
 /**
- * controller test
+ * 默认的 controller
  *
  * @author Shi Long <long.shi@alibaba-inc.com>
  * @copyright ©2003-2103 phpwind.com
  * @license http://www.windframework.com
  * @version $Id$
- * @package demos.blog
+ * @package demos.blog.controller
  */
 class IndexController extends WindController {
 	
-	protected $viewer = array();
-	const LOGIN_SUCCESS = 1;
-  
 	/**
 	 * @see WindSimpleController::run()
 	 */
 	public function run() {
+		//获取输入数据
+		$isRegister = $this->getInput("register");
+		$param = $this->getFormParam($isRegister ? 'register' : 'login');
 		//指定模版
 		$this->setTemplate('index_run');
+		
+		//设置模版变量
+		$this->setOutput(
+			array('register' => $isRegister, 'param' => $param));
 	}
 
 	/**
-	 * postAction请求
+	 * loginAction请求
 	 */
-	public function postAction() {
+	public function loginAction() {
+		$info = '';
 		if ($this->getSession()->isRegistered('status')) {
-			$this->viewer['info'] = '已登录，请先注销！';
+			$info = '已登录，请先注销！';
+			$param = array();
 		} else {
 			//获得经过验证的表单
-			$loginForm = $this->getInput("loginForm");
+			$loginForm = $this->getInput("userForm");
 			$result = $this->login($loginForm->getUsername(), $loginForm->getPassword());
 			if (!$result) {
-				$this->viewer['info'] = '用户名、密码不正确！';
+				$info = '用户名、密码不正确！';
+				$param = $this->getFormParam('login');
 			} else {
-				$this->getSession()->set('status', self::LOGIN_SUCCESS);
+				$this->getSession()->set('status', 1);
 				$this->getSession()->set('username', $loginForm->getUsername());
+				$param = array();
 			}
 		}
+		
+		$this->setOutput(
+			array(
+				'info'      => $info, 
+				'username'  => $this->getSession()->get("username"), 
+				'status' 	=> $this->getSession()->get("status"), 
+				'param' 	=> $param));
 		$this->setTemplate('index_run');
 	}
 
 	/**
-	 * logOutAction请求
+	 * 接收logOutAction请求
 	 */
 	public function logOutAction() {
 		$this->getSession()->destroy();
@@ -50,47 +65,93 @@ class IndexController extends WindController {
 	}
 
 	/**
-	 * 处理每个Action请求前先调用的方法
-	 * @see WindSimpleController::beforeAction()
+	 * 接收registerAction请求
 	 */
-	public function beforeAction($handlerAdapter) {
-		$this->definedShareVars();
-	}
-
-	private function definedShareVars() {
-		$baseUrl = $this->getRequest()->getBaseUrl(true);
-		$this->viewer['baseUrl'] = $baseUrl;
-	}
-
-	/**
-	 * 完成每个Action请求后调用的方法
-	 * @see WindSimpleController::afterAction()
-	 */
-	public function afterAction($handler) {
-		parent::afterAction($handler);
-		$this->setOutput($this->viewer);
+	public function registerAction() {
+		$param = array();
+		$isRegister = 0;
+		if ($this->getSession()->isRegistered('status')) {
+			$info = '已登录，请先注销！';
+		} else {
+			//获得经过验证的表单，使用和登录同一个model，见config配置
+			$registerForm = $this->getInput("userForm");
+			list($result, $info) = $this->register($registerForm->getUsername(), $registerForm->getPassword());
+			if ($result) {
+				$this->getSession()->set('status', 1);
+				$this->getSession()->set('username', $registerForm->getUsername());
+			} else {
+				$param = $this->getFormParam('register');
+				$isRegister = 1;
+			}
+		}
+		
+		$this->setOutput(array(
+				'param'     => $param, 
+				'info' 		=> $info, 
+				'username' 	=> $this->getSession()->get("username"), 
+				'status' 	=> $this->getSession()->get("status"), 
+				'register' 	=> $isRegister,
+			));
+		$this->setTemplate('index_run');
 	}
 
 	/**
 	 * 获取windSession组件实例
-	 * @return
+	 * @return WindSession
 	 */
-	public function getSession() {
+	private function getSession() {
 		return $this->getSystemFactory()->getInstance('windSession');
 	}
-	
+
 	/**
-	 * 登录业务处理,获取db组件实例
+	 * 登录业务处理, 获取db组件实例
 	 *
 	 * @param string $username 用户名
 	 * @param string $password 密码
 	 * @return array
 	 */
-	public function login($username, $password) {
+	private function login($username, $password) {
 		$db = $this->getSystemFactory()->getInstance('db');
-		if (!$db instanceof WindConnection) $this->addMessage("database connect error");
-		$sql = "SELECT * FROM user WHERE username = " . $db->quote($username) . " AND password = " . $db->quote($password);
-		return $db->query($sql)->fetch();
+		$stmt = $db->createStatement('SELECT count(*) FROM user WHERE username=:username AND password =:password');
+		return $stmt->getValue(array('username' => $username, 'password' => $password));
+	}
+
+	/**
+	 * 注册业务处理
+	 * 
+	 * @param string $username
+	 * @param string $password
+	 * @return array
+	 */
+	private function register($username, $password) {
+		$db = $this->getSystemFactory()->getInstance('db');
+		$stmt = $db->createStatement('SELECT * FROM user WHERE username=:username');
+		if ($stmt->getOne(array(':username' => $username))) {
+			return array('0',"用户名已存在!");
+		}
+		$result = $db->execute(
+			"INSERT INTO user SET " . $db->sqlSingle(
+				array('username' => $username, 'password' => $password)));
+		return array($result, $result ? "注册成功！" : "注册失败！");
+	}
+	
+	/**
+	 * 获取模版表单的参数
+	 * 
+	 * @param string $type
+	 * @return array
+	 */
+	private function getFormParam($type) {
+		$params = array();
+		$params['login'] = array(
+			'formName'  => 'loginForm', 
+			'action' 	=> 'index.php?a=login', 
+			'button' 	=> '登录');
+		$params['register'] = array(
+			'formName'  => 'registerForm', 
+			'action' 	=> 'index.php?a=register', 
+			'button' 	=> '注册');
+	    return $params[$type];
 	}
 
 }
