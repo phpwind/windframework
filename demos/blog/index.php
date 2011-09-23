4101ed97826493e8df1c6d1c24a19af1bde475a0
long.shi <long.shi@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-23 03:30:20 +0000

    test svn keywords
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2807 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/blog/index.php
+++ demos/blog/index.php
@@ -2,4 +2,5 @@
 error_reporting(E_ALL);
 define('ROOT_PATH', dirname(dirname(dirname(__FILE__))) . '/');
 require_once ROOT_PATH . 'wind/Wind.php';
+echo 'test';
 Wind::application('blog', ROOT_PATH . 'demos/blog/config/config.php')->run();
