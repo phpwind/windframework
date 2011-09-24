af7f29d0ae4bf58730c82188689d3d7d9eab327d
xiaoxia.xuxx <xiaoxia.xuxx@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-24 05:50:25 +0000

    bug 169 cache多并发问题，denpendency策略实现 http://bugs.phpwind-inc.com/show_bug.cgi?id=169
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2826 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/cache/dependency/WindDbDependency.php
@@ -0,0 +1,49 @@
+<?php
+Wind::import('WIND:cache.IWindCacheDependency');
+/**
+ * db缓存方式的依赖实现
+ *
+ * @author xiaoxia.xu <xiaoxia.xuxx@aliyun-inc.com>
+ * @copyright ©2003-2103 phpwind.com
+ * @license http://www.windframework.com
+ * @version $Id: WindDbDependence.php 1532 2011-9-23下午05:42:00 xiaoxiao $
+ * @package wind.cache.dependence
+ */
+class WindDbDependency implements IWindCacheDependency {
+
+	private $sql = '';
+
+	private $timeOut = '';
+
+	private $key = '';
+
+	public function __construct($sql, $timeOut = 0) {
+		$this->sql = $sql;
+		$this->timeOut = (int)$timeOut + time();
+	}
+
+	/* (non-PHPdoc)
+	 * @see IWindCacheDependency::injectDependent()
+	 */
+	public function injectDependent($expires) {
+		if ($this->timeOut > 0) return;
+		$this->timeOut = $expires > 0 ? 0.8 * $expires + time() : 0;
+	}
+
+	/* (non-PHPdoc)
+	 * @see IWindCacheDependency::hasChanged()
+	 */
+	public function hasChanged($cache, $key, $expires) {
+		if (0 == $this->timeOut) return false;
+		if ($this->timeOut <= time()) {
+			$lock = $key . '_lock_';
+	        if ($cache->add($lock, 3 * 60 * 1000) == true) {
+	            $db = Wind::getApp()->getComponent('db');
+	            $data = $db->query($this->sql)->fetchAll();
+	            $cache->set($key, $data, $expires, $this);
+	            $cache->delete($lock);
+	        }
+	    }
+	    return false;
+	}
+}
