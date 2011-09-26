35df1e3733f33bb592097b72100b4a99ef55dd88
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-09-26 11:02:10 +0000

    NEW - bug 169: PHP Task: TODO Cache 需要考虑多并发问题//考虑用dependence实现 http://bugs.phpwind-inc.com/show_bug.cgi?id=169
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2862 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/cache/dependency/WindResolvedCrashDependency.php
+++ wind/cache/dependency/WindResolvedCrashDependency.php
@@ -1,7 +1,12 @@
 <?php
 Wind::import('WIND:cache.IWindCacheDependency');
 /**
- * db缓存方式的依赖实现
+ * cache依赖实现,通过该实现解决大并发时cache(memcached)的值突然失效
+ * 
+ * 在value内部设置1个超时值(timeout1), timeout1比实际的cache(memcached)timeout(timeout2)小.
+ * 当从cache读取到timeout1发现它已经过期时候,马上延长timeout1并重新设置到cache.
+ * 然后再从数据库加载数据并设置到cache中.
+ * <note><b>注意:</b>该方法用回调的方式来获得当前缓存的数据值.</note>
  *
  * @author xiaoxia.xu <xiaoxia.xuxx@aliyun-inc.com>
  * @copyright ©2003-2103 phpwind.com
@@ -10,15 +15,20 @@ Wind::import('WIND:cache.IWindCacheDependency');
  * @package wind.cache.dependency
  */
 class WindResolvedCrashDependency implements IWindCacheDependency {
-
+	
 	private $timeOut = '';
 	
 	private $callBack = array();
 	
 	private $args = array();
-	
-	public function __construct($callBack, $args  = array(), $timeOut = 0) {
-		$this->timeOut = (int)$timeOut + time();
+
+	/**
+	 * @param array $callBack 回调方法,通过该方法返回当前数据
+	 * @param array $args 回调方法参数
+	 * @param int $timeOut 超时时间
+	 */
+	public function __construct($callBack, $args = array(), $timeOut = 0) {
+		$this->timeOut = (int) $timeOut + time();
 		$this->callBack = serialize($callBack);
 		$this->args = serialize($args);
 	}
@@ -38,13 +48,13 @@ class WindResolvedCrashDependency implements IWindCacheDependency {
 		if (0 == $this->timeOut) return false;
 		if ($this->timeOut <= time()) {
 			$lock = $key . '_lock_';
-	        if ($cache->add($lock, 3 * 60 * 1000) == true) {
-	        	$callBack = unserialize($this->callBack);
-	        	$data = call_user_func_array($callBack, unserialize($this->args));
-	            $cache->set($key, $data, $expires, $this);
-	            $cache->delete($lock);
-	        }
-	    }
-	    return false;
+			if ($cache->add($lock, 3 * 60 * 1000) == true) {
+				$callBack = unserialize($this->callBack);
+				$data = call_user_func_array($callBack, unserialize($this->args));
+				$cache->set($key, $data, $expires, $this);
+				$cache->delete($lock);
+			}
+		}
+		return false;
 	}
 }
