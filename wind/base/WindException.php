cec47b5957cd6d9ada3ad00568ef9483c0121112
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-10-08 08:00:59 +0000

    docs注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2905 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/base/WindException.php
+++ wind/base/WindException.php
@@ -6,7 +6,8 @@
  * @copyright ©2003-2103 phpwind.com
  * @license http://www.windframework.com
  * @version $Id$
- * @package wind.base
+ * @package wind
+ * @subpackage base
  */
 class WindException extends Exception {
 	/* 系统错误 */
