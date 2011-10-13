077bd026fa26db6307acc8d2c5078ad0c6dc600f
long.shi <long.shi@18ba2127-5a84-46d4-baec-3457e417f034>
2011-10-13 06:42:31 +0000

    fix WindHelperTest
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2948 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/base/WindHelperTest.php
+++ _tests/base/WindHelperTest.php
@@ -24,14 +24,36 @@ class WindHelperTest extends BaseTestCase {
 		//set_error_handler("WindHelper::errorHandle");
 		//$str = '111';
 		//$a = unserialize($str);
+		//Unable to test
 	}
 
 	/**
 	 * Tests WindHelper::exceptionHandle()
 	 */
 	public function testExceptionHandle() {
-		
+		//Unable to Test
+	}
+	
+	/**
+	 * @dataProvider dataForGetErrorName
+	 */
+	public function testGetErrorName($errorNumber, $expected){
+		$this->assertEquals($expected, ForWindHelper::getErrorName($errorNumber));
+	}
+	
+	public function dataForGetErrorName(){
+		$args = array();
+		$args[] = array(E_ALL, "E_ALL");
+		$args[] = array(E_STRICT, "E_STRICT");
+		$args[] = array(3, "E_UNKNOWN");
+		return $args;
 	}
 
+}
+
+class ForWindHelper extends WindHelper{
+	public static function getErrorName($errorNumber) {
+		return parent::getErrorName($errorNumber);
+	}
 }
 
