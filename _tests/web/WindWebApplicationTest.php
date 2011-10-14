d5a738b815333afe7e9748509f70d60862c21128
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-10-14 06:28:03 +0000

    docs注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2963 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/web/WindWebApplicationTest.php
+++ _tests/web/WindWebApplicationTest.php
@@ -1,6 +1,12 @@
 <?php
 /**
  * WindWebApplication test case.
+ *
+ * @author Qiong Wu <papa0924@gmail.com> 2011-10-14
+ * @copyright ©2003-2103 phpwind.com
+ * @license http://www.windframework.com
+ * @version $Id$
+ * @package web
  */
 class WindWebApplicationTest extends BaseTestCase {
 	
