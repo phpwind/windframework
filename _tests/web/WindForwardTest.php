d66e03ab0609640750e57d9de1f89bbe6d4363d3
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-10-14 03:12:27 +0000

    docs注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2959 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/web/WindForwardTest.php
+++ _tests/web/WindForwardTest.php
@@ -1,6 +1,12 @@
 <?php
 /**
- * WindForward test case.
+ * WindForward test case
+ *
+ * @author Qiong Wu <papa0924@gmail.com> 2011-10-14
+ * @copyright ©2003-2103 phpwind.com
+ * @license http://www.windframework.com
+ * @version $Id$
+ * @package web
  */
 class WindForwardTest extends BaseTestCase {
 	
@@ -32,27 +38,35 @@ class WindForwardTest extends BaseTestCase {
 	 * @dataProvider dataForForwardAction
 	 */
 	public function testForwardAction($action, $args, $isRedirect, $immediately) {
-		if ($immediately){
+		if ($immediately) {
 			try {
 				$this->WindForward->forwardAction($action, $args, $isRedirect, $immediately);
 			} catch (WindForwardException $e) {
 				$this->assertEquals(array($action, $args, $isRedirect, true), 
-					array($e->getForward()->getAction(), $e->getForward()->getArgs(), $e->getForward()->getIsRedirect(), $e->getForward()->getIsReAction()));
+					array(
+						$e->getForward()->getAction(), 
+						$e->getForward()->getArgs(), 
+						$e->getForward()->getIsRedirect(), 
+						$e->getForward()->getIsReAction()));
 				return;
 			}
 			$this->fail("ForwardAction Test Error!");
-		}else {
+		} else {
 			$this->WindForward->forwardAction($action, $args, $isRedirect, $immediately);
 			$this->assertEquals(array($action, $args, $isRedirect, true), 
-					array($this->WindForward->getAction(), $this->WindForward->getArgs(), $this->WindForward->getIsRedirect(), $this->WindForward->getIsReAction()));
+				array(
+					$this->WindForward->getAction(), 
+					$this->WindForward->getArgs(), 
+					$this->WindForward->getIsRedirect(), 
+					$this->WindForward->getIsReAction()));
 		}
 	}
-	
-	public function dataForForwardAction(){
+
+	public function dataForForwardAction() {
 		$args = array();
 		$args[] = array("/default/long/shi", array('shi' => 'long'), true, false);
-		$args[] = array("/hello/long/shi", array(),false, true);
-		$args[] = array("/hello/shi/long",array(), true, true);
+		$args[] = array("/hello/long/shi", array(), false, true);
+		$args[] = array("/hello/shi/long", array(), true, true);
 		return $args;
 	}
 
@@ -63,12 +77,13 @@ class WindForwardTest extends BaseTestCase {
 		try {
 			$this->WindForward->forwardRedirect("index.php");
 		} catch (WindForwardException $e) {
-			$this->assertEquals(array("index.php", true), array($e->getForward()->getUrl(), $e->getForward()->getIsRedirect()));
-			return ;
+			$this->assertEquals(array("index.php", true), 
+				array($e->getForward()->getUrl(), $e->getForward()->getIsRedirect()));
+			return;
 		}
 		$this->fail("ForwardRedirect Test Error!");
 	}
-	
+
 	/**
 	 * Tests WindForward->setVars()
 	 * @dataProvider dataForSetVars
@@ -77,11 +92,11 @@ class WindForwardTest extends BaseTestCase {
 		$this->WindForward->setVars($vars, $key);
 		$this->assertEquals("long", $this->WindForward->getVars("shi"));
 	}
-	
-	public function dataForSetVars(){
+
+	public function dataForSetVars() {
 		$args = array();
 		$object = new stdClass();
-		$object->shi ='long';
+		$object->shi = 'long';
 		$args[] = array($object);
 		$args[] = array(array('shi' => 'long'));
 		$args[] = array('long', 'shi');
