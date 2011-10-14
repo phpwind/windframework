48d7f6fce5da2f1ed89ca34eee47d2a3cb4cffaa
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-10-14 06:26:31 +0000

    docs注释完善
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@2962 18ba2127-5a84-46d4-baec-3457e417f034

--- _tests/web/AllWebTest.php
+++ _tests/web/AllWebTest.php
@@ -1,13 +1,19 @@
 <?php
-/**
- * Static test suite.
- */
 require_once 'web/WindWebApplicationTest.php';
 require_once 'web/WindUrlHelperTest.php';
 require_once 'web/WindForwardTest.php';
 require_once 'web/WindErrorHandlerTest.php';
 require_once 'web/WindDispatcherTest.php';
 require_once 'web/WindControllerTest.php';
+/**
+ * Static test suite.
+ *
+ * @author Qiong Wu <papa0924@gmail.com> 2011-10-14
+ * @copyright ©2003-2103 phpwind.com
+ * @license http://www.windframework.com
+ * @version $Id$
+ * @package web
+ */
 class AllWebTest extends PHPUnit_Framework_TestSuite {
 
 	public static function main() {
