eef9c90cc9a5f1d5c3b3c95dac866bd12e519e47
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-11-17 08:49:29 +0000

    支持 acceptType 类型判断
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@3141 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/viewer/WindView.php
+++ wind/viewer/WindView.php
@@ -120,7 +120,13 @@ class WindView extends WindModule implements IWindView {
 	 */
 	public function render($display = false) {
 		if (!$this->templateName) return;
-		$_type = $this->getRequest()->getIsAjaxRequest() ? 'json' : $this->getResponse()->getResponseType();
+		//$_type = $this->getRequest()->getIsAjaxRequest() ? 'json' : $this->getResponse()->getResponseType();
+		$_type = $this->getResponse()->getResponseType();
+		if (!$_type) {
+			list($acceptTypes) = explode(',', $this->getRequest()->getAcceptTypes(), 2);
+			Wind::import('WIND:http.mime.WindMimeType');
+			$_type = WindMimeType::getType($acceptTypes);
+		}
 		switch (strtolower($_type)) {
 			case 'json':
 				$this->renderWithJson();
