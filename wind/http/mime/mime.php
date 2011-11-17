29627dbb4028c9edc6db82dd48d6e4f6f05264cc
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-11-17 08:51:12 +0000

    支持 acceptType 类型判断
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@3142 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/http/mime/mime.php
+++ wind/http/mime/mime.php
@@ -66,6 +66,7 @@ return array(
 	'jpeg' => 'image/jpeg', 
 	'jpg' => 'image/jpeg', 
 	'js' => 'application/x-javascript', 
+	'json' => 'application/json',
 	'kar' => 'audio/midi', 
 	'latex' => 'application/x-latex', 
 	'lha' => 'application/octet-stream', 
