d2024105b0c55b1ec1be2f661790a6ecf1572653
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-11-30 05:24:32 +0000

    RESOLVED - bug 227: feature// view themes http://bugs.phpwind-inc.com/show_bug.cgi?id=227
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@3207 18ba2127-5a84-46d4-baec-3457e417f034

--- demos/blog/index.php
+++ demos/blog/index.php
@@ -3,6 +3,5 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 define('WIND_DEBUG', 1);
 require_once '../../wind/Wind.php';
 
-Wind::register('template', 'TPL');
 Wind::application('blog', 'config/config.php')->run();
 
