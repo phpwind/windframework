679b687a12eb084900c92649b9a682cecc343dc9
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2011-12-02 09:18:54 +0000

    Merged revision(s) 3229 from laboratory/trunk: bugs :  修复windcookie 接口 问题 ........
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@3230 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/utility/WindCookie.php
+++ wind/utility/WindCookie.php
@@ -22,7 +22,6 @@ class WindCookie {
 	 * 
 	 * @param string $name cookie名称
 	 * @param string $value cookie值,默认为null
-	 * @param string $prefix cookie前缀,默认为null即没有前缀
 	 * @param boolean $encode 是否使用 MIME base64 对数据进行编码,默认是false即不进行编码
 	 * @param string|int $expires 过期时间,默认为null即会话cookie,随着会话结束将会销毁
 	 * @param string $path cookie保存的路径,默认为null即采用默认
@@ -31,12 +30,10 @@ class WindCookie {
 	 * @param boolean $httponly 是否可通过客户端脚本访问,默认为false即客户端脚本可以访问cookie
 	 * @return boolean 设置成功返回true,失败返回false
 	 */
-	public static function set($name, $value = null, $prefix = null, $encode = false, $expires = null, $path = null, $domain = null, $secure = false, $httponly = false) {
+	public static function set($name, $value = null, $encode = false, $expires = null, $path = null, $domain = null, $secure = false, $httponly = false) {
 		if (empty($name)) return false;
 		$value && $value = serialize($value);
 		$encode && $value = base64_encode($value);
-		$prefix && $name = $prefix . $name;
-		$expires && $expires = time() + intval($expires);
 		$path = $path ? $path : '/';
 		setcookie($name, $value, $expires, $path, $domain, $secure, $httponly);
 		return true;
@@ -46,11 +43,9 @@ class WindCookie {
 	 * 根据cookie的名字删除cookie
 	 * 
 	 * @param string $name cookie名称
-	 * @param string $prefix cookie前缀,默认为null,即没有前缀
 	 * @return boolean 删除成功返回true
 	 */
-	public static function delete($name, $prefix = null) {
-		$prefix && $name = $prefix . $name;
+	public static function delete($name) {
 		if (self::exist($name)) {
 			self::set($name, '');
 			unset($_COOKIE[$name]);
@@ -62,12 +57,10 @@ class WindCookie {
 	 * 取得指定名称的cookie值
 	 * 
 	 * @param string $name cookie名称
-	 * @param string $prefix cookie前缀,默认为null即没有前缀
 	 * @param boolean $dencode 是否对cookie值进行过解码,默认为false即不用解码
 	 * @return mixed 获取成功将返回保存的cookie值,获取失败将返回false
 	 */
-	public static function get($name, $prefix = null, $dencode = false) {
-		$prefix && $name = $prefix . $name;
+	public static function get($name, $dencode = false) {
 		if (self::exist($name)) {
 			$value = $_COOKIE[$name];
 			$value && $dencode && $value = base64_decode($value);
@@ -90,10 +83,9 @@ class WindCookie {
 	 * 判断cookie是否存在
 	 * 
 	 * @param string $name cookie名称
-	 * @param string $prefix cookie前缀,默认为null即没有前缀
 	 * @return boolean 如果不存在则返回false,否则返回true
 	 */
-	public static function exist($name, $prefix = null) {
-		return isset($_COOKIE[$prefix ? $prefix . $name : $name]);
+	public static function exist($name) {
+		return isset($_COOKIE[$name]);
 	}
 }
