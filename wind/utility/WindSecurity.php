5d85abaf688805258015568aae68808f26f87c41
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2012-01-05 09:38:43 +0000

    NEW - bug 248: refactory//第三方团队反馈问题集12年月4日异常机制问题&mcrypt_cbc问题 http://bugs.phpwind-inc.com/show_bug.cgi?id=248
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@3293 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/utility/WindSecurity.php
+++ wind/utility/WindSecurity.php
@@ -44,16 +44,17 @@ class WindSecurity {
 	 * @param string $key 密钥
 	 * @return string 加密后的结果
 	 */
-	public static function encrypt($str, $key) {
+	public static function encrypt($str, $key, $iv = '') {
 		if (!$key || !is_string($key)) throw new WindException(
 			'[utility.WindSecurity.encrypt] security key is required. ', WindException::ERROR_PARAMETER_TYPE_ERROR);
 		if (!$str || !is_string($str)) throw new WindException(
 			'[utility.WindSecurity.encrypt] security string is required.', WindException::ERROR_PARAMETER_TYPE_ERROR);
 		
 		$size = mcrypt_get_block_size(MCRYPT_DES, MCRYPT_MODE_CBC);
+		$iv = substr(md5($iv ? $iv : $key), -$size);
 		$pad = $size - (strlen($str) % $size);
 		$str .= str_repeat(chr($pad), $pad);
-		@$data = mcrypt_cbc(MCRYPT_DES, $key, $str, MCRYPT_ENCRYPT);
+		@$data = mcrypt_cbc(MCRYPT_DES, $key, $str, MCRYPT_ENCRYPT, $iv);
 		return base64_encode($data);
 	}
 
@@ -64,14 +65,16 @@ class WindSecurity {
 	 * @param string $key 密钥
 	 * @return string 解密后的结果
 	 */
-	public static function decrypt($str, $key) {
+	public static function decrypt($str, $key, $iv = '') {
 		if (!$str || !is_string($str)) throw new WindException(
 			'[utility.WindSecurity.dncrypt] security string is required.', WindException::ERROR_PARAMETER_TYPE_ERROR);
 		if (!$key || !is_string($key)) throw new WindException('[utlity.WindSecurity.decrypt] security key is required.', 
 			WindException::ERROR_PARAMETER_TYPE_ERROR);
 		
+		$size = mcrypt_get_block_size(MCRYPT_DES, MCRYPT_MODE_CBC);
+		$iv = substr(md5($iv ? $iv : $key), -$size);
 		$str = base64_decode($str);
-		@$str = mcrypt_cbc(MCRYPT_DES, $key, $str, MCRYPT_DECRYPT);
+		@$str = mcrypt_cbc(MCRYPT_DES, $key, $str, MCRYPT_DECRYPT, $iv);
 		$pad = ord($str{strlen($str) - 1});
 		if ($pad > strlen($str)) return false;
 		if (strspn($str, chr($pad), strlen($str) - $pad) != $pad) return false;
