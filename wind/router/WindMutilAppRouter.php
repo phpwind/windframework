87b15027ec07dd36049192ce545a2f5d5feb5782
long.shi <long.shi@18ba2127-5a84-46d4-baec-3457e417f034>
2012-01-12 03:27:12 +0000

    RESOLVED - bug 254: refactor// WindWebApplication.run 优化run状态设置 http://bugs.phpwind-inc.com/show_bug.cgi?id=254
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@3308 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/router/WindMutilAppRouter.php
+++ wind/router/WindMutilAppRouter.php
@@ -9,7 +9,96 @@
  * @package router
  */
 class WindMutilAppRouter extends WindRouter {
+	protected $appKey = 'p';
+	protected $_app;
+	protected $app = 'default';
 
+	/* (non-PHPdoc)
+	 * @see WindRouter::route()
+	 */
+	public function route($request, $response) {
+		$this->_app = $this->app;
+		parent::route($request, $response);
+	}
+	
+	public function assemble($action, $args = array(), $route = null) {
+		$route || $route = $this->defaultRoute;
+		if ($route && (null !== $route = $this->getRoute($route))) {
+			$_url = $route->build($this, $action, $args);
+		} else {
+			list($_a, $_c, $_m, $_p, $args) = WindUrlHelper::resolveAction($action, $args);
+			$_p || $_p = $this->getApp();
+			if ($_p && $_p !== $this->getDefaultApp()) $args[$this->appKey] = $_p;
+			if ($_m && $_m !== $this->getDefaultModule()) $args[$this->moduleKey] = $_m;
+			if ($_c && $_c !== $this->getDefaultController()) $args[$this->controllerKey] = $_c;
+			if ($_a && $_a !== $this->getDefaultAction()) $args[$this->actionKey] = $_a;
+			$_url = $this->request->getScript() . '?' . WindUrlHelper::argsToUrl($args);
+		}
+		return $_url;
+	}
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindRouter::setParams()
+	 */
+	protected function setParams($params, $request) {
+		parent::setParams($params, $request);
+		$app = isset($params[$this->appKey]) ? $params[$this->appKey] : $request->getRequest(
+			$this->appKey);
+		$app && $this->setApp($app);
+	}
+
+	/* (non-PHPdoc)
+	 * @see AbstractWindRouter::setConfig()
+	 */
+	public function setConfig($config) {
+		parent::setConfig($config);
+		$this->app = $this->getConfig('app', 'default-value', $this->app);
+		$this->appKey = $this->getConfig('app', 'url-param', $this->appKey);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getApp() {
+		return $this->app;
+	}
+
+	/**
+	 * 设置当前要访问的appname
+	 *
+	 * @param string $appName
+	 */
+	public function setApp($appName) {
+		$this->app = $appName;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAppKey() {
+		return $this->appKey;
+	}
+
+	/**
+	 * @param string $appKey
+	 */
+	public function setAppKey($appKey) {
+		$this->appKey = $appKey;
+	}
+
+	/**
+	 * @param string $app
+	 */
+	public function setDefaultApp($app) {
+		$this->_app = $app;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDefaultApp() {
+		return $this->_app;
+	}
 
 }
 
