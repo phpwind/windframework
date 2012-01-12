b198a263e15852e96938a89334fac56d155b8d8c
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2012-01-12 02:39:05 +0000

    RESOLVED - bug 254: refactor// WindWebApplication.run 优化run状态设置 http://bugs.phpwind-inc.com/show_bug.cgi?id=254
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/laboratory/trunk@3307 18ba2127-5a84-46d4-baec-3457e417f034

---
+++ wind/router/WindMutilAppRouter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 多应用支持路由协议解析器
+ *
+ * @author Qiong Wu <papa0924@gmail.com> 2012-1-12
+ * @copyright ©2003-2103 phpwind.com
+ * @license http://www.windframework.com
+ * @version $Id$
+ * @package router
+ */
+class WindMutilAppRouter extends WindRouter {
+
+
+}
+
+?>
