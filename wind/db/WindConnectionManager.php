52ff7d36e894cbdb1a628a86dceb90b62d2f9b89
yishuo <yishuo@18ba2127-5a84-46d4-baec-3457e417f034>
2012-03-14 08:33:48 +0000

    每次链接用完重置一下初始值
    git-svn-id: http://svn.simba.taobao.com/svn/PW_WindFramework/trunk@3396 18ba2127-5a84-46d4-baec-3457e417f034

--- wind/db/WindConnectionManager.php
+++ wind/db/WindConnectionManager.php
@@ -2,13 +2,14 @@
 Wind::import("WIND:db.WindConnection");
 /**
  * 数据库链接管理
- * 
+ *
  * 配置说明：<code>
  * 1. 当没有任何策略部署时 ，默认返回当前配置中的第一个链接句柄
  * 2. 当没有任何策略部署时，如果在sql语句中有链接句柄指定时则返回指定的链接句柄
  * 例如：'{db1:tableName}'返回db1指定的链接句柄
  * 3. 如果当前有策略部署时，则按照策略部署规则返回
- * 4. createStatement($sql = null, $forceMaster = false) 当$forceMaster为true时则强制主链接
+ * 4. createStatement($sql = null, $forceMaster = false)
+ * 当$forceMaster为true时则强制主链接
  * 配置格式如下：
  * <connections except='*:db1;user*,tablename2:db1|db2;'>
  * <connection name='db1'>
@@ -36,60 +37,60 @@ Wind::import("WIND:db.WindConnection");
 class WindConnectionManager extends WindConnection {
 	/**
 	 * 通配符设置
-	 * 
+	 *
 	 * @var string
 	 */
 	private $wildcard = '*';
 	/**
 	 * 数据链接池,临时保存当前所有数据库连接句柄
-	 * 
+	 *
 	 * @var array
 	 */
 	private $pool = array();
 	/**
 	 * 当前数据表名称
-	 * 
+	 *
 	 * @var string
 	 */
 	private $tableName;
 	/**
 	 * 当前的sql语句查询类型
-	 * 
+	 *
 	 * @var string
 	 */
 	private $sqlType;
 	/**
 	 * 数据库连接池策略部署配置信息
-	 * 
+	 *
 	 * @var array
 	 */
 	private $except = array('_current' => '', '_default' => array(), '_except' => array(), '_db' => array());
 	
 	/**
 	 * 是否强制主链接,默认为false
-	 * 
+	 *
 	 * @var boolean
 	 */
 	private $forceMaster = false;
-
-	/* (non-PHPdoc)
-	 * @see WindConnection::createStatement()
+	
+	/*
+	 * (non-PHPdoc) @see WindConnection::createStatement()
 	 */
 	public function createStatement($sql = null, $forceMaster = false) {
 		$this->forceMaster = $forceMaster;
 		return parent::createStatement($sql);
 	}
-
-	/* (non-PHPdoc)
-	 * @see WindConnection::getDbHandle()
+	
+	/*
+	 * (non-PHPdoc) @see WindConnection::getDbHandle()
 	 */
 	public function getDbHandle() {
 		$this->init();
 		return $this->_dbHandle;
 	}
-
-	/* (non-PHPdoc)
-	 * @see WindConnection::init()
+	
+	/*
+	 * (non-PHPdoc) @see WindConnection::init()
 	 */
 	public function init() {
 		try {
@@ -111,9 +112,9 @@ class WindConnectionManager extends WindConnection {
 			throw new WindDbException($e->getMessage());
 		}
 	}
-
-	/* (non-PHPdoc)
-	 * @see WindConnection::parseQueryString()
+	
+	/*
+	 * (non-PHPdoc) @see WindConnection::parseQueryString()
 	 */
 	protected function parseQueryString($sql) {
 		$sql = preg_replace_callback(
@@ -145,8 +146,8 @@ class WindConnectionManager extends WindConnection {
 
 	/**
 	 * 根据当前sql语句类型,从一条数据连接配置中解析出主从信息,并设置当前db链接名
-	 * 
-	 * @param array $_c
+	 *
+	 * @param array $_c        	
 	 * @return void
 	 */
 	private function _resolveCurrentDb($_c) {
@@ -170,7 +171,7 @@ class WindConnectionManager extends WindConnection {
 
 	/**
 	 * 解析sql语句
-	 * 
+	 *
 	 * 解析sql语句,在sql语句中提取数据库连接信息,返回解析后的sql语句<code>
 	 * Array(
 	 * [0] => SELECT * FROM {db1:database.members} WHERE uid<=:uid	| 匹配到的完整str
@@ -180,8 +181,8 @@ class WindConnectionManager extends WindConnection {
 	 * [4] => database.												| 当前database
 	 * [5] => members												| 当前表名
 	 * )</code>
-	 * 
-	 * @param array $matchs
+	 *
+	 * @param array $matchs        	
 	 * @return string
 	 */
 	private function _pregQueryString($matchs) {
@@ -190,12 +191,13 @@ class WindConnectionManager extends WindConnection {
 			$this->tableName = $matchs[5];
 			$this->except['_current'] = trim($matchs[3], ':');
 			$matchs[0] = str_replace($matchs[3] . $matchs[4], '', $matchs[0]);
-		}
+		} else
+			$this->except['_current'] = $this->tableName = '';
 		return $matchs[0];
 	}
-
-	/* (non-PHPdoc)
-	 * @see WindConnection::_initConfig()
+	
+	/*
+	 * (non-PHPdoc) @see WindConnection::_initConfig()
 	 */
 	protected function _initConfig() {
 		$_except = $this->getConfig('connections', 'except');
@@ -212,8 +214,8 @@ class WindConnectionManager extends WindConnection {
 
 	/**
 	 * 重置链接信息
-	 * 
-	 * @param string $db
+	 *
+	 * @param string $db        	
 	 * @return void
 	 */
 	private function _resetConnection($db) {
@@ -225,14 +227,14 @@ class WindConnectionManager extends WindConnection {
 
 	/**
 	 * 解析链接管理链接策略
-	 * 
+	 *
 	 * db链接管理配置信息如下:<code>except='*:(db1);user*,tablename2:(db1|db2);'</code>
 	 * 一组'*:(db1);'代表一组数据配置,格式如:'tableName1,tableName2,tableName3:(db1|db2,db3,db4)'.
 	 * 数据表名称与策略用':'隔开,':'前面表示一组数据表,可以是多个也可以是一个,多个表名用','隔开.
 	 * ':'后面'()'中表示一组数据策略,例子中策略的意思是'db1'是主链接(master),'db2,db3,db4'为从属链接(slaves).
 	 * 该方法解析这段数据库链接配置,并将解析结果存储到 'except' 属性中.
-	 * 
-	 * @param array $matchs
+	 *
+	 * @param array $matchs        	
 	 * @return void
 	 */
 	private function _pregExcept($matchs) {
